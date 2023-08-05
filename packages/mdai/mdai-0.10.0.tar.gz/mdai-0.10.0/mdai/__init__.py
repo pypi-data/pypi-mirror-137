243b1831e0d3378e73888545416839b2471c1257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:50 +0000

    Add package mdai/0.10.0/mdai-0.10.0.tar.gz

---
+++ packages/mdai/mdai-0.10.0.tar.gz/mdai-0.10.0/mdai/__init__.py
@@ -0,0 +1,15 @@
+"""MD.ai Python client library."""
+
+__version__ = "0.10.0"
+
+from . import preprocess
+from .client import Client
+from .utils import common_utils
+from .utils import transforms
+
+try:
+    CAN_VISUALIZE = True
+    from . import visualize
+except ImportError:
+    # matplotlib backend missing or cannot be loaded
+    CAN_VISUALIZE = False
