4bfa167ef4e6b7995f902ee85c05bec5c027f756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:35 +0000

    Add package yt-idefix/0.11.0/yt_idefix-0.11.0-py3-none-any.whl

---
+++ packages/yt-idefix/yt_idefix-0.11.0-py3-none-any.whl/yt_idefix/_io/commons.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+from enum import IntEnum
+from typing import Any, Dict, Literal, NamedTuple, Tuple
+
+import numpy as np
+
+Prec = Literal["d", "f", "i"]
+Dim = Literal[1, 2, 3]
+
+
+class Shape(NamedTuple):
+    n1: int = 1
+    n2: int = 1
+    n3: int = 1
+
+    @property
+    def size(self):
+        return self.n1 * self.n2 * self.n3
+
+    def to_cell_centered(self) -> Shape:
+        vals: list[int] = [1, 1, 1]
+        for i, attr in enumerate(self):
+            vals[i] = max(1, attr - 1)
+        return Shape(*vals)
+
+
+class Coordinates(NamedTuple):
+    x1: np.ndarray
+    x2: np.ndarray
+    x3: np.ndarray
+
+    @property
+    def shape(self) -> Shape:
+        return Shape(len(self.x1), len(self.x2), len(self.x3))
+
+    def padded(self) -> Coordinates:
+        arrs = list(self)
+        for i, arr in enumerate(arrs):
+            if arr.size == 1:
+                arrs[i] = np.array((arr[0], arr[0] + 1))
+        return Coordinates(*arrs)
+
+
+# map field name to numpy array init data:
+# precision (-> datatype), dimensionality, [nx, ny, nz]
+# the np.ndarray is assumed to contain *dim* elements
+IdefixFieldProperties = Dict[str, Tuple[Prec, Dim, np.ndarray]]
+
+# Map various str keys to scalars and arrays
+IdefixMetadata = Dict[str, Any]
+
+
+class ByteSize(IntEnum):
+    """Constant byte size for data types"""
+
+    CHAR = 1
+    INT = 4
+    FLOAT = 4
+    DOUBLE = 8
