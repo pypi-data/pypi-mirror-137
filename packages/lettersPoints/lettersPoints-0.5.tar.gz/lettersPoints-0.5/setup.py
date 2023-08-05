e6e22ad3468beab3152eea19a041bd7aa4491ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:48 +0000

    Add package lettersPoints/0.5/lettersPoints-0.5.tar.gz

---
+++ packages/lettersPoints/lettersPoints-0.5.tar.gz/lettersPoints-0.5/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'lettersPoints',
+  packages = ['lettersPoints'],
+  version = '0.5',
+  license = 'MIT',
+  description = 'This library can be used to generate points that, when plotted, form letters. The height and spacing can be set.',
+  author = 'Jean-Paul Mitra',
+  author_email = 'jeanmitra77@gmail.com',
+  url = 'https://github.com/MitraMitraMitra/letters-plot',
+  download_url = 'https://github.com/MitraMitraMitra/letters-plot/archive/refs/tags/v_05.tar.gz',  
+  keywords = ['plotting', 'letters'],
+  install_requires = [
+           
+      ],
+  classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
