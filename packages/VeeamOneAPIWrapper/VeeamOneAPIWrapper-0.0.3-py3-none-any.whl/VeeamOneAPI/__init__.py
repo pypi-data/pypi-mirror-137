d6c621163c1f677e4294a0f0deb0582aeed1a333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:29 +0000

    Add package VeeamOneAPIWrapper/0.0.3/VeeamOneAPIWrapper-0.0.3-py3-none-any.whl

---
+++ packages/VeeamOneAPIWrapper/VeeamOneAPIWrapper-0.0.3-py3-none-any.whl/VeeamOneAPI/__init__.py
@@ -0,0 +1,6 @@
+from .Functions import veeamOneAuthToken
+from .Functions import invokeVeeamOne
+from .Functions import veeamOneHyperVVMs
+from .Functions import veeamOnevSphereVMs
+from .Functions import getPoolName
+from .Functions import veeamOnevSphereResourcePools
