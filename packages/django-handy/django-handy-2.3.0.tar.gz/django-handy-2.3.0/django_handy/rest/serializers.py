47194027f960cda63157c9732d0d8b1fbf7a59e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:37 +0000

    Add package django-handy/2.3.0/django-handy-2.3.0.tar.gz

---
+++ packages/django-handy/django-handy-2.3.0.tar.gz/django-handy-2.3.0/django_handy/rest/serializers.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from django.db import models
+
+from rest_framework import serializers
+
+
+class SelectObjectsSerializer(serializers.Serializer):
+    ids = serializers.ListField(child=serializers.IntegerField(min_value=0))
+
+    @property
+    def selected_ids(self) -> List[int]:
+        return self.validated_data.get('ids', [])
+
+    @property
+    def selected_objects(self) -> models.QuerySet:
+        objects = self.context['view'].get_queryset()
+        objects = objects.filter(id__in=self.selected_ids)
+        return objects
