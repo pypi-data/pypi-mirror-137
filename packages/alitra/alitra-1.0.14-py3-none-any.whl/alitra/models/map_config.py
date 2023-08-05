7ac404b41d92cc92924cb51642754e17ccb49e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:43 +0000

    Add package alitra/1.0.14/alitra-1.0.14-py3-none-any.whl

---
+++ packages/alitra/alitra-1.0.14-py3-none-any.whl/alitra/models/map_config.py
@@ -0,0 +1,24 @@
+import json
+from dataclasses import dataclass
+from pathlib import Path
+
+from dacite import from_dict
+
+from alitra.frame_dataclasses import PointList
+
+
+@dataclass
+class MapConfig:
+    map_name: str
+    robot_reference_points: PointList
+    asset_reference_points: PointList
+
+
+def load_map_config(map_config_path: Path) -> MapConfig:
+    with open(map_config_path) as json_file:
+        map_config_dict = json.load(json_file)
+
+    return from_dict(
+        data_class=MapConfig,
+        data=map_config_dict,
+    )
