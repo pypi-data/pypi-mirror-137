bbb2ef6596994a9d94608948fa14d6ed8305645d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:12 +0000

    Add package viewsource/3.12.0/viewsource-3.12.0.tar.gz

---
+++ packages/viewsource/viewsource-3.12.0.tar.gz/viewsource-3.12.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '3.12.0' 
+DESCRIPTION = 'View and print the HTML of websites!'
+LONG_DESCRIPTION = 'Useful for webscraping. This module allows you to view the html from pages, in a single line!'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="viewsource", 
+        version=VERSION,
+        author="Daniel Duchouquette",
+        author_email="<danpersonguything@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['bs4'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'Simple', 'HTML', 'view', 'webscraping'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
