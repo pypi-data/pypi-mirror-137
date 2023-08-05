903b5a2c2df3f4566ece0cad56ab538d4c5d9876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:38 +0000

    Add package mmselfsup/0.6.0/mmselfsup-0.6.0.tar.gz

---
+++ packages/mmselfsup/mmselfsup-0.6.0.tar.gz/mmselfsup-0.6.0/mmselfsup/models/algorithms/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .base import BaseModel
+from .byol import BYOL
+from .classification import Classification
+from .deepcluster import DeepCluster
+from .densecl import DenseCL
+from .moco import MoCo
+from .mocov3 import MoCoV3
+from .npid import NPID
+from .odc import ODC
+from .relative_loc import RelativeLoc
+from .rotation_pred import RotationPred
+from .simclr import SimCLR
+from .simsiam import SimSiam
+from .swav import SwAV
+
+__all__ = [
+    'BaseModel', 'BYOL', 'Classification', 'DeepCluster', 'DenseCL', 'MoCo',
+    'MoCoV3', 'NPID', 'ODC', 'RelativeLoc', 'RotationPred', 'SimCLR',
+    'SimSiam', 'SwAV'
+]
