01d652fe3068a86a80589f585c88f0e8dde61cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:04 +0000

    Add package dorado/2.0.2.dev42/dorado-2.0.2.dev42.tar.gz

---
+++ packages/dorado/dorado-2.0.2.dev42.tar.gz/dorado-2.0.2.dev42/dorado/ceres/ceresClass.py
@@ -0,0 +1,113 @@
+import warnings
+warnings.filterwarnings('ignore')
+# import sys
+# import os
+
+import numpy as np
+import ccdprocx
+from astropy.time import Time
+from astropy.table import QTable, Table
+import astroalign as aa
+from astropy.wcs import WCS
+# from astropy.utils.console import ProgressBar, ProgressBarOrSpinner
+from tqdm import tqdm
+# from astropy.coordinates import SkyCoord as acoord
+# import astropy.units as un
+from astropy.io import fits
+
+from astropy.nddata.ccddata import CCDData
+
+# photometry imports
+# from photutils.psf import IntegratedGaussianPRF, DAOGroup
+# from photutils.background import MMMBackground, MADStdBackgroundRMS
+# from astropy.modeling.fitting import LevMarLSQFitter
+# from astropy.stats import gaussian_sigma_to_fwhm
+# from photutils.psf import IterativelySubtractedPSFPhotometry
+from photutils.aperture import CircularAperture, aperture_photometry, CircularAnnulus
+# from photutils import DAOStarFinder
+# from astropy.stats import mad_std
+# from astroquery.simbad import Simbad
+
+from astropy.visualization import SqrtStretch
+from astropy.visualization.mpl_normalize import ImageNormalize
+from astropy.stats import SigmaClip
+from photutils import Background2D, MedianBackground
+
+
+from ..timeseries import timeSeries
+
+'''
+Ceres is the handler of observational image series in Dorado.
+'''
+
+__all__ = ['Ceres']
+
+class Ceres:
+    '''
+        The Ceres class encapsulates a set of astronomical data from a single night of observation.
+        Ceres can handle multiple stacks of data in different filters and perform a variety of
+        actions on them in an orgainized fashion.
+
+        Attributes
+        ----------
+
+        filters: dictionary
+
+        data: Stack array
+
+        bias: CCDdata
+
+        time: 'astropy.Time' 
+
+        datestr: str
+
+        
+    '''
+    def __init__(self, filters = {}, data = [], bias = None, time = None, datestr = None):
+        # metadata
+        self.filters = filters
+        self.data = data
+        self.bias = bias
+        self.time = time
+        self.datestr = datestr
+        
+        # location, weather, timezone, camera, observer array
+
+        # refering instance of clippy to call and save later?
+        # or call clippy directly and feed it a ceres object
+        # TODO figure out how to clean this up
+        try:
+            self.date = Time(int(self.time.mjd), format = 'mjd')
+        except:
+            self.date = None
+        
+        if datestr == None:
+            try:
+                day = str(self.date.ymdhms['day'])
+                day2 = str(self.date.ymdhms['day'] + 1)
+                month = str(self.date.ymdhms['month'])
+
+                if self.date.ymdhms['day'] < 10:
+                    day = '0' + str(self.date.ymdhms['day'])
+                    if self.date.ymdhms['day'] < 9:
+                        day2 = '0' + str(self.date.ymdhms['day'] + 1)
+                        
+                if self.date.ymdhms['month'] < 10:
+                    month = '0' + str(self.date.ymdhms['month'])
+
+                self.datestr = str(self.date.ymdhms['year']) + '-' + month + '-' + day + '+' + day2
+            except:
+                self.datestr = datestr
+        
+    def add_stack(self, stack):
+        # eventually stacks themelves should have some metadata 
+        # to denote stuff like calibration status
+        # TODO do I need to add times?
+        self.filters[stack.filter] = len(self.data)
+        self.data.append(stack)
+        # done
+
+    def rem_stack(self, filter):
+        # TODO delete time strings
+        del self.data[self.filters[filter]]
+        # done
