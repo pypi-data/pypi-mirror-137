2af7d20abe995af9f2d1181b8873a6512b433813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:55 +0000

    Add package df-fuzzy-merge/0.0.1/df_fuzzy_merge-0.0.1.tar.gz

---
+++ packages/df-fuzzy-merge/df_fuzzy_merge-0.0.1.tar.gz/df_fuzzy_merge-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='df_fuzzy_merge',
+      version='0.0.1',
+      description='fuzzy merges two dataframes',
+      url='https://lamplightlab.com',
+      author='Matthew McElhaney',
+      author_email='matt@lamplightlab.com',
+      license='MIT',
+      packages=['df_fuzzy_merge'],
+      install_requires=['nltk', 'fuzzywuzzy', 'pandas', 'python-Levenshtein'],
+      zip_safe=False)
