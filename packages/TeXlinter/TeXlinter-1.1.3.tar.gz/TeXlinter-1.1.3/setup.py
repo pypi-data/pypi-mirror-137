7d9c07dd453a7246de48038e6d0ed1fcea9e7553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:11 +0000

    Add package TeXlinter/1.1.3/TeXlinter-1.1.3.tar.gz

---
+++ packages/TeXlinter/TeXlinter-1.1.3.tar.gz/TeXlinter-1.1.3/setup.py
@@ -0,0 +1,25 @@
+import imp
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+
+setuptools.setup(
+    name='TeXlinter',
+    version='1.1.3',    
+    description='A small linter for LaTex',
+    long_description=long_description,
+    author='Marcus Björnbäck',
+    author_email='smacke123b@gmail.com',
+    license='BSD 2-clause',
+    scripts=['TeXlinter', 'rules.JSON'],
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+        'License :: OSI Approved :: BSD License',  
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+    ],
+)
