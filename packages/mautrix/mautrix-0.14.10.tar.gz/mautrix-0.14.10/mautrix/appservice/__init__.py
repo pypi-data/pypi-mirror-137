c833e74046930b910a8b5af5644539b00859049e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:49 +0000

    Add package mautrix/0.14.10/mautrix-0.14.10.tar.gz

---
+++ packages/mautrix/mautrix-0.14.10.tar.gz/mautrix-0.14.10/mautrix/appservice/__init__.py
@@ -0,0 +1,14 @@
+from .api import DOUBLE_PUPPET_SOURCE_KEY, AppServiceAPI, ChildAppServiceAPI, IntentAPI
+from .appservice import AppService
+from .as_handler import AppServiceServerMixin
+from .state_store import ASStateStore
+
+__all__ = [
+    "AppService",
+    "AppServiceAPI",
+    "ChildAppServiceAPI",
+    "IntentAPI",
+    "ASStateStore",
+    "AppServiceServerMixin",
+    "DOUBLE_PUPPET_SOURCE_KEY",
+]
---
+++ packages/mautrix/mautrix-0.14.10.tar.gz/mautrix-0.14.10/mautrix/client/state_store/__init__.py
@@ -0,0 +1,4 @@
+from .abstract import StateStore
+from .file import FileStateStore
+from .memory import MemoryStateStore
+from .sync import MemorySyncStore, SyncStore
