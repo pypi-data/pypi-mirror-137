eea9ba196d9133acd90404002e07dc3b7ec07e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:11 +0000

    Add package seedphotos/0.1.1/seedphotos-0.1.1.tar.gz

---
+++ packages/seedphotos/seedphotos-0.1.1.tar.gz/seedphotos-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['seedphotos']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.27.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'seedphotos',
+    'version': '0.1.1',
+    'description': 'A lib that create image through words',
+    'long_description': None,
+    'author': 'Vinicius Barbosa de Aguiar',
+    'author_email': 'vb9763662@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
