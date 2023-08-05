a45037a06fce87e35929578b2c79682c23d258c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:52 +0000

    Add package pyobs-core/0.17.0a1/pyobs-core-0.17.0a1.tar.gz

---
+++ packages/pyobs-core/pyobs-core-0.17.0a1.tar.gz/pyobs-core-0.17.0a1/pyobs/robotic/lco/__init__.py
@@ -0,0 +1,5 @@
+from .task import LcoTask
+from .taskarchive import LcoTaskArchive
+from .taskschedule import LcoTaskSchedule
+from .taskrunner import LcoTaskRunner
+from .dummytaskschedule import LcoDummyTaskSchedule
