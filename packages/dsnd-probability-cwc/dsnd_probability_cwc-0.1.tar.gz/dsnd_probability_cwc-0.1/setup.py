0df8f414a45ba0691ff2ca9ebe33328d98ace599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:14 +0000

    Add package dsnd-probability-cwc/0.1/dsnd_probability_cwc-0.1.tar.gz

---
+++ packages/dsnd-probability-cwc/dsnd_probability_cwc-0.1.tar.gz/dsnd_probability_cwc-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_cwc',
+      version='0.1',
+      description='Gaussian abd Binomial Distribution',
+      packages=['dsnd_probability_cwc'],
+      author='Chin-Wen Chang',
+      zip_safe=False)
