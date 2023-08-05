b6a0c01a7777e213549e7ac0636abdd156a08e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:02 +0000

    Add package spalb/1.2.0/spalb-1.2.0-py3-none-any.whl

---
+++ packages/spalb/spalb-1.2.0-py3-none-any.whl/spalb/__init__.py
@@ -0,0 +1,4 @@
+from spalb.straight import comsoal, lcr, rpw
+#from drogba.U_GA import readfile, ShowAns, ShowAns_Pa, Fitness, Fitness_Pa, Efficiency, Efficiency_Pa, sm_index, sm_index_Pa, WeightMappingCrossOver, Crossover, Crossover_selection, SwapMutation, Mutation, Mutation_selection
+from spalb.U_GA import u_ga, parallel_ga
+
