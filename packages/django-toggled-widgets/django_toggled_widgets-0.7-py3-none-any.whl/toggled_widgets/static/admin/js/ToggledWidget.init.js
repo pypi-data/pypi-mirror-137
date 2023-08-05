f75eb7a64a2dfe496163854c939236a066a40714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:40 +0000

    Add package django-toggled-widgets/0.7/django_toggled_widgets-0.7-py3-none-any.whl

---
+++ packages/django-toggled-widgets/django_toggled_widgets-0.7-py3-none-any.whl/toggled_widgets/static/admin/js/ToggledWidget.init.js
@@ -0,0 +1,18 @@
+(function($) {
+    $(function() {
+        $('.toggled-widget:not([name*="__prefix__"])').each(function() {
+            new ToggledWidget($, this);
+        });
+        $('.toggle-metafield:not([name*="__prefix__"])').each(function() {
+            initializeMetafield(this);
+        });
+        $(document).on('formset:added', function(e, $row) {
+            $row.find('.toggled-widget').each(function() {
+                new ToggledWidget($, this);
+            });
+            $row.find('.toggle-metafield').each(function() {
+                initializeMetafield(this);
+            });
+        });
+    });
+})(django.jQuery);
