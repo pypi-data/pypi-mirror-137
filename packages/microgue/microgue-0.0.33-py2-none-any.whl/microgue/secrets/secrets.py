e35a7209c09c87189464c61d01fdd957ba5a6779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:22 +0000

    Add package microgue/0.0.33/microgue-0.0.33-py2-none-any.whl

---
+++ packages/microgue/microgue-0.0.33-py2-none-any.whl/microgue/secrets/secrets.py
@@ -0,0 +1,27 @@
+import boto3
+import json
+
+
+class GetSecretFailed(Exception):
+    pass
+
+
+class SecretsConnectionFailed(Exception):
+    pass
+
+
+class Secrets:
+    def __init__(self, *args, **kwargs):
+        try:
+            self.secrets = boto3.client('secretsmanager')
+        except Exception as e:
+            raise SecretsConnectionFailed(str(e))
+
+    def get_secret(self, secret_name):
+        try:
+            get_secret_value_response = self.secrets.get_secret_value(
+                SecretId=secret_name
+            )
+        except Exception as e:
+            raise GetSecretFailed(str(e))
+        return json.loads(get_secret_value_response['SecretString'])
