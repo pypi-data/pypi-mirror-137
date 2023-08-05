8d8d29287a347f4bd6cdc5ff31f4335a74b1a66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:20 +0000

    Add package aiopytesseract/0.2.0/aiopytesseract-0.2.0-py2.py3-none-any.whl

---
+++ packages/aiopytesseract/aiopytesseract-0.2.0-py2.py3-none-any.whl/aiopytesseract/__init__.py
@@ -0,0 +1,33 @@
+from .commands import (
+    confidence,
+    deskew,
+    get_languages,
+    get_tesseract_version,
+    image_to_boxes,
+    image_to_data,
+    image_to_hocr,
+    image_to_osd,
+    image_to_pdf,
+    image_to_string,
+    languages,
+    run,
+    tesseract_version,
+)
+
+__version__ = "0.2.0"
+__all__ = [
+    "__version__",
+    "confidence",
+    "deskew",
+    "get_languages",
+    "get_tesseract_version",
+    "image_to_boxes",
+    "image_to_data",
+    "image_to_hocr",
+    "image_to_osd",
+    "image_to_pdf",
+    "image_to_string",
+    "languages",
+    "run",
+    "tesseract_version",
+]
