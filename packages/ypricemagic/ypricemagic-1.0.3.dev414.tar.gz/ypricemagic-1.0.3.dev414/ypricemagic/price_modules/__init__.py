ef257338b73b02a98a87f87f2aa70b0b6871d91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:51 +0000

    Add package ypricemagic/1.0.3.dev414/ypricemagic-1.0.3.dev414.tar.gz

---
+++ packages/ypricemagic/ypricemagic-1.0.3.dev414.tar.gz/ypricemagic-1.0.3.dev414/ypricemagic/price_modules/__init__.py
@@ -0,0 +1,20 @@
+from ypricemagic.price_modules import (belt, cream, ellipsis, froyo, gelato,
+                                       ib, mooniswap, mstablefeederpool,
+                                       piedao, saddle, tokensets, wsteth,
+                                       yearn)
+
+__all__ = [
+    'belt',
+    'cream',
+    'ellipsis',
+    'froyo',
+    'gelato',
+    'ib',
+    'mooniswap',
+    'mstablefeederpool',
+    'piedao',
+    'saddle',
+    'tokensets',
+    'wsteth',
+    'yearn',
+]
