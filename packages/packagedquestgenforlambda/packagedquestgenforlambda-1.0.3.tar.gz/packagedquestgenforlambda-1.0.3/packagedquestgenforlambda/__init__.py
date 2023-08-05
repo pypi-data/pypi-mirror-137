ebb0965a7e49ed4c54420fd5ade9882665f95acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:54 +0000

    Add package packagedquestgenforlambda/1.0.3/packagedquestgenforlambda-1.0.3.tar.gz

---
+++ packages/packagedquestgenforlambda/packagedquestgenforlambda-1.0.3.tar.gz/packagedquestgenforlambda-1.0.3/packagedquestgenforlambda/__init__.py
@@ -0,0 +1,5 @@
+# Constructor for packagedquestgenforlambda
+from __future__ import absolute_import
+from packagedquestgenforlambda.encoding import encoding
+from packagedquestgenforlambda.mcq import mcq
+from packagedquestgenforlambda.main import QGen, BoolQGen, AnswerPredictor
---
+++ packages/packagedquestgenforlambda/packagedquestgenforlambda-1.0.3.tar.gz/packagedquestgenforlambda-1.0.3/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+setup(name='packagedquestgenforlambda',
+      version='1.0.3',
+      description='Question generator from any text',
+      author='packagedquestgenforlambda contributors',
+      author_email='vaibhavtiwarifu@gmail.com',
+      packages=['packagedquestgenforlambda', 'packagedquestgenforlambda.encoding', 'packagedquestgenforlambda.mcq'],
+      url="https://github.com/ramsrigouthamg/packagedquestgenforlambda.ai",
+      install_requires=[
+         
+           'torch==1.9.0',
+           'transformers==3.0.2',
+           'pytorch_lightning==0.8.1',
+           'sense2vec==1.0.3',
+           'strsim==0.0.3',
+           'six==1.15.0',
+           'networkx==2.4.0',
+           'numpy',
+           'scipy',
+           'scikit-learn',
+           'unidecode==1.1.1',
+           'future==0.18.2',
+           'joblib==0.14.1',
+           'spacy==2.2.4',
+           'pytz==2020.1',
+           'python-dateutil==2.8.1',
+           'boto3==1.14.40',
+           'flashtext==2.7',
+           'pandas',
+      ],
+      package_data={'packagedquestgenforlambda': ['packagedquestgenforlambda.py', 'mcq.py', 'train_gpu.py', 'encoding.py']}
+      )
