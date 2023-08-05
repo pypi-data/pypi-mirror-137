0eb0e0d9efed2453b30cd8c06b493fdfca175df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:14 +0000

    Add package epcpm/2022.1.5/epcpm-2022.1.5-py3-none-any.whl

---
+++ packages/epcpm/epcpm-2022.1.5-py3-none-any.whl/epcpm/__build.py
@@ -0,0 +1,9 @@
+# This file has been generated
+
+
+build_system = 'GitHub Actions'
+build_id = '1780116635'
+build_number = '255'
+build_version = None
+job_id = '1780116635_255'
+job_url = 'https://github.com/epcpower/pm/actions/runs/1780116635'
