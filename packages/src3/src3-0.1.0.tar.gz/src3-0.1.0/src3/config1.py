231dfec78c740bea3a2d3fb1ab73a9f21fe39440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:20 +0000

    Add package src3/0.1.0/src3-0.1.0.tar.gz

---
+++ packages/src3/src3-0.1.0.tar.gz/src3-0.1.0/src3/config1.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class mytransformer(BaseEstimator, TransformerMixin):
+    
+	def __init__(self):
+		print('hello')
+        
+	def fit(self, X, y = None):
+		print("fit!")
+		return self
+
+
+	def transform(self, X, y = None):
+		print("transform")
+        df = X
+		return df.to_numpy()
