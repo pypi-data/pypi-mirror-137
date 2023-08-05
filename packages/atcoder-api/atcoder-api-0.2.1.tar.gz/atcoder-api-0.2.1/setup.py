0a385b98cc1764320b9b33ddc9270d5b5c1f90e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:15 +0000

    Add package atcoder-api/0.2.1/atcoder-api-0.2.1.tar.gz

---
+++ packages/atcoder-api/atcoder-api-0.2.1.tar.gz/atcoder-api-0.2.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['atcoder', 'atcoder.core', 'atcoder.core.crawl', 'atcoder.core.scrape']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4', 'boto3', 'lxml', 'pandas', 'requests', 'selenium', 'tqdm']
+
+setup_kwargs = {
+    'name': 'atcoder-api',
+    'version': '0.2.1',
+    'description': 'AtCoder API.',
+    'long_description': '# atcoder-api',
+    'author': 'Hiroshi Tsuyuki',
+    'author_email': 'kagemeka1@gmail.com',
+    'maintainer': 'Hiroshi Tsuyuki',
+    'maintainer_email': 'kagemeka1@gmail.com',
+    'url': 'https://github.com/competitive-programming-helper/atcoder-api',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
