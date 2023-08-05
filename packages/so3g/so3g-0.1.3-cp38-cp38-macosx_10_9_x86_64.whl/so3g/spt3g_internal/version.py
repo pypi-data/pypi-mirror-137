9358e593174fd61595242152f92e15234abbf420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:09 +0000

    Add package so3g/0.1.3/so3g-0.1.3-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/so3g/so3g-0.1.3-cp38-cp38-macosx_10_9_x86_64.whl/so3g/spt3g_internal/version.py
@@ -0,0 +1,12 @@
+# AUTO-GENERATED FILE: DO NOT EDIT
+
+# Contains version control information (revision IDs, paths, etc.)
+# as of the last time make was run.
+
+upstream_url="UNKNOWN VCS"
+upstream_branch="UNKNOWN VCS"
+revision="959caff19ccc37ee269f1aa37c7ee80740ac6ff5"
+gitrevision="959caff19ccc37ee269f1aa37c7ee80740ac6ff5"
+localdiffs=True
+versionname=""
+fullversion="0.3-105-g959caff-dirty"
