05249435bcdd23a3b97d9aaaa96639bebe91c38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:44 +0000

    Add package ThemeTkinter/0.0.3/ThemeTkinter-0.0.3-py3-none-any.whl

---
+++ packages/ThemeTkinter/ThemeTkinter-0.0.3-py3-none-any.whl/ThemeTkinter/__init__.py
@@ -0,0 +1,30 @@
+"""
+Copyright (c) 2021 The Python Packaging Authority
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+__Version__ = '0.0.3'
+__Title__ = 'ThemeTkinter'
+__Author__ = 'Xiaocaicai'
+__Author_Email__ = 'xiaocaicai_email@sina.com'
+__Description__ = 'With beautiful theme tkinter package.'
+__Url__ = 'https://github.com/xiaocaicai-github/ThemeTkinter/'
+
+from ThemeTkinter.Core import Window
