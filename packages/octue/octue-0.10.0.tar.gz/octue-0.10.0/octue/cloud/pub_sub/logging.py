7e285e7c2fa30b93d4ba4d3c7f14474d455fcc72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:23 +0000

    Add package octue/0.10.0/octue-0.10.0.tar.gz

---
+++ packages/octue/octue-0.10.0.tar.gz/octue-0.10.0/octue/cloud/pub_sub/logging.py
@@ -0,0 +1,44 @@
+import json
+from logging import Handler
+
+
+class GooglePubSubHandler(Handler):
+    """A log handler that publishes log records to a Google Cloud Pub/Sub topic.
+
+    :param google.cloud.pubsub_v1.PublisherClient publisher: pub/sub publisher to use to publish the log records
+    :param octue.cloud.pub_sub.topic.Topic topic: topic to publish log records to
+    :param str analysis_id: the UUID of the analysis the instance is handling the log records for
+    :param float timeout: timeout in seconds for attempting to publish each log record
+    :return None:
+    """
+
+    def __init__(self, publisher, topic, analysis_id, timeout=60, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.topic = topic
+        self.analysis_id = analysis_id
+        self.timeout = timeout
+        self._publisher = publisher
+
+    def emit(self, record):
+        """Serialise the log record as a dictionary and publish it to the topic.
+
+        :param logging.LogRecord record:
+        :return None:
+        """
+        try:
+            self._publisher.publish(
+                topic=self.topic.path,
+                data=json.dumps(
+                    {
+                        "type": "log_record",
+                        "log_record": vars(record),
+                        "analysis_id": self.analysis_id,
+                        "message_number": str(self.topic.messages_published),
+                    }
+                ).encode(),
+            )
+
+            self.topic.messages_published += 1
+
+        except Exception:  # noqa
+            self.handleError(record)
