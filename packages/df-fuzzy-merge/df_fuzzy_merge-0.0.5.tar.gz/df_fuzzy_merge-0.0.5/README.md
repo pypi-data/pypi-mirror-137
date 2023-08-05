796d13da269291d60f87b85900b8eaf186af7d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:37 +0000

    Add package df-fuzzy-merge/0.0.5/df_fuzzy_merge-0.0.5.tar.gz

---
+++ packages/df-fuzzy-merge/df_fuzzy_merge-0.0.5.tar.gz/df_fuzzy_merge-0.0.5/README.md
@@ -0,0 +1,26 @@
+# df_fuzzy_merge
+
+fuzzy merge for pandas dataframe package
+
+## local install:
+
+pip install git+https://github.com/mmcelhan/df_fuzzy_merge.git#egg=df_fuzzy_merge
+
+## to test:
+
+import df_fuzzy_merge as df
+
+import os
+
+import pandas as pd
+
+first_df = pd.read_csv(os.path.join("testing_files", "data_1.csv"))
+second_df = pd.read_csv(os.path.join("testing_files", "data_2.csv"))
+
+merged_df = df.df_fuzzy_merge(first_df, second_df, left_on=['last_name', 'first_name', 'school'], right_on=['first_name', 'school', 'last_name'])
+
+print(merged_df)
+
+## source code available here:
+
+https://github.com/mmcelhan/dataframe_fuzzy_merge_source
---
+++ packages/df-fuzzy-merge/df_fuzzy_merge-0.0.5.tar.gz/df_fuzzy_merge-0.0.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="df_fuzzy_merge",
+    version="0.0.5",
+    install_requires=['ntlk', 'pandas', 'python-Levenshtein', 'fuzzywuzzy'],
+    author="Matthew McElhaney",
+    author_email="matt@lamplightlab.com",
+    description="Package that fuzzy merges two dataframes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mmcelhan/df_fuzzy_merge",
+    project_urls={
+        "blog post": "https://lamplightlab.com/2022/01/12/dataframe-fuzzy-matching/",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
