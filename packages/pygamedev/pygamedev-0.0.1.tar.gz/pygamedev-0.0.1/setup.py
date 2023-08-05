e67eadf07a95f12c376c5bd4f1cdc02e41ccea4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:16 +0000

    Add package pygamedev/0.0.1/pygamedev-0.0.1.tar.gz

---
+++ packages/pygamedev/pygamedev-0.0.1.tar.gz/pygamedev-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import pygame
+
+VERSION = '0.0.1'
+DESCRIPTION = 'a package to make games'
+LONG_DESCRIPTION = 'a package to make games using the pygame package but simplifeid (because i barely know how to use it)'
+
+setup(
+    name='pygamedev',
+    version=VERSION,
+    author='jortpower2009 (Jort Vlaming)',
+    author_email="<vlamingjort@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pygame'],
+    keywords=['python', 'game', 'dev', 'gamedev']
+)
