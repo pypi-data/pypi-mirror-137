d845c9fffe34bc0fdac4ce760724614b425577f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:33 +0000

    Add package create-dash-app/0.0.2/create_dash_app-0.0.2-py3-none-any.whl

---
+++ packages/create-dash-app/create_dash_app-0.0.2-py3-none-any.whl/templates/root.py
@@ -0,0 +1,49 @@
+import templates.keys as keys
+
+APP_TEMPLATE = f'''
+from {{{keys.BASE}}}.components.index import Layout
+from {{{keys.BASE}}}.server import app
+
+# In order for callbacks to work, they must be imported where Dash is instantiated 
+# noinspection PyUnresolvedReferences
+import {{{keys.BASE}}}.callbacks.index
+
+server = app.server
+app.layout = Layout
+
+
+if __name__ == '__main__':
+    app.runserver(debug=True)
+    
+'''
+
+SERVER_TEMPLATE = f'''
+import dash
+import dash_bootstrap_components as dbc
+
+
+app = dash.Dash(
+    __name__,
+    external_stylesheets=[
+        dbc.themes.CYBORG, 
+    ],
+    # Dash throws exceptions about callbacks from components that might be not loaded yet if
+    # elements like Tabs are used. These alerts are annoying so suppress them.
+    suppress_callback_exceptions=True,
+    title={{{keys.TITLE}}},
+)
+cache = Cache(
+    app.server,
+    config={{{{
+        'CACHE_TYPE': 'filesystem',
+        'CACHE_DIR': os.path.join({{{keys.CACHE_PATH}}}, 'dash-cache'),
+    }}}}
+)
+
+'''
+
+WSGI_TEMPLATE = f'''
+# noinspection PyUnresolvedReferences
+from {{{keys.BASE}}}.app import server as application
+
+'''
