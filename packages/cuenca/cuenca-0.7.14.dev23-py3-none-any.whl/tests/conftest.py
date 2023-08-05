8344af93fbcc3ff686a6ca5e60d180b81593de20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:20 +0000

    Add package cuenca/0.7.14.dev23/cuenca-0.7.14.dev23-py3-none-any.whl

---
+++ packages/cuenca/cuenca-0.7.14.dev23-py3-none-any.whl/tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+import cuenca
+
+cuenca.configure(sandbox=True)
+
+
+@pytest.fixture(scope='module')
+def vcr_config():
+    config = dict()
+    config['filter_headers'] = [
+        ('Authorization', 'DUMMY'),
+        ('X-Cuenca-Token', 'DUMMY'),
+    ]
+    return config
+
+
+@pytest.fixture
+def transfer():
+    yield cuenca.Transfer.create(
+        account_number='646180157034181180',
+        amount=10000,
+        descriptor='first transfer',
+        recipient_name='Frida Kahlo',
+    )
