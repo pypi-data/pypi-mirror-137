7cb94ab340323783a37a68a0095237e77121b4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:57 +0000

    Add package pte-stats/0.1.0/pte-stats-0.1.0.tar.gz

---
+++ packages/pte-stats/pte-stats-0.1.0.tar.gz/pte-stats-0.1.0/pte_stats/__init__.py
@@ -0,0 +1,16 @@
+"""Package for statistics."""
+
+__version__ = "0.1.0"
+
+from .cluster import (
+    cluster_2d,
+    clusters_from_pvals,
+    clusterwise_pval_numba,
+    get_clusters,
+)
+from .permutation import (
+    permutation_2d,
+    permutation_onesample,
+    permutation_twosample,
+)
+from .timeseries import correct_pvals, timeseries_pvals
