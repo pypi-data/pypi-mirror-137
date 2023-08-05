aa9c1d8cd8524ef41dcb50106f3bc4941f6a61e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:32 +0000

    Add package aioinject/0.1.1/aioinject-0.1.1.tar.gz

---
+++ packages/aioinject/aioinject-0.1.1.tar.gz/aioinject-0.1.1/aioinject/context.py
@@ -0,0 +1,104 @@
+from __future__ import annotations
+
+import contextlib
+from contextvars import ContextVar
+from typing import TYPE_CHECKING, Any, Optional, Type, TypeVar, Union
+
+if TYPE_CHECKING:
+    from .containers import Container
+
+_T = TypeVar("_T")
+
+_AnyCtx = Union["InjectionContext", "SyncInjectionContext"]
+context_var: ContextVar[_AnyCtx] = ContextVar("aioinject_context")
+
+
+class _BaseInjectionContext:
+    def __init__(self, container: Container):
+        self.container = container
+        self.cache: dict[tuple[Type[_T], Optional[Any]], _T] = {}
+        self._token = None
+
+
+class InjectionContext(_BaseInjectionContext):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.exit_stack = contextlib.AsyncExitStack()
+
+    async def resolve(
+        self,
+        type_: Type[_T],
+        impl: Optional[Type] = None,
+        use_cache: bool = True,
+    ) -> _T:
+        if use_cache and (type_, impl) in self.cache:
+            return self.cache[type_, impl]
+        provider = self.container.get_provider(type_, impl)
+        dependencies = {
+            dep.name: await self.resolve(
+                type_=dep.type,
+                impl=dep.implementation,
+                use_cache=dep.use_cache,
+            )
+            for dep in provider.dependencies
+        }
+
+        resolved = await provider.provide(**dependencies)
+        if isinstance(resolved, contextlib.ContextDecorator):
+            resolved = self.exit_stack.enter_context(resolved)
+
+        if isinstance(resolved, contextlib.AsyncContextDecorator):
+            resolved = await self.exit_stack.enter_async_context(resolved)
+
+        if use_cache:
+            self.cache[type_, impl] = resolved
+        return resolved
+
+    async def __aenter__(self) -> InjectionContext:
+        self._token = context_var.set(self)
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        context_var.reset(self._token)
+        await self.exit_stack.__aexit__(exc_type, exc_val, exc_tb)
+
+
+class SyncInjectionContext(_BaseInjectionContext):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.exit_stack = contextlib.ExitStack()
+
+    def resolve(
+        self,
+        type_: Type[_T],
+        impl: Optional[Any] = None,
+        use_cache: bool = True,
+    ) -> _T:
+        if use_cache and (type_, impl) in self.cache:
+            return self.cache[type_, impl]
+
+        provider = self.container.get_provider(type_, impl)
+        dependencies = {
+            dep.name: self.resolve(
+                type_=dep.type,
+                impl=dep.implementation,
+                use_cache=dep.use_cache,
+            )
+            for dep in provider.dependencies
+        }
+
+        resolved = provider.provide_sync(**dependencies)
+        if isinstance(resolved, contextlib.ContextDecorator):
+            resolved = self.exit_stack.enter_context(resolved)
+
+        if use_cache:
+            self.cache[type_, impl] = resolved
+        return resolved
+
+    def __enter__(self) -> SyncInjectionContext:
+        self._token = context_var.set(self)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        context_var.reset(self._token)
+        self.exit_stack.__exit__(exc_type, exc_val, exc_tb)
---
+++ packages/aioinject/aioinject-0.1.1.tar.gz/aioinject-0.1.1/aioinject/decorators.py
@@ -0,0 +1,58 @@
+import functools
+import inspect
+import itertools
+import typing
+from typing import Any, Type
+
+from aioinject.context import context_var
+from aioinject.providers import collect_dependencies
+
+
+def _missing_kwargs(
+    type_hints: dict[str, Type],
+    signature: inspect.Signature,
+    *args: Any,
+    **kwargs: Any,
+) -> dict[str, Type]:
+    already_provided_args: set[str] = set(
+        itertools.islice(signature.parameters, len(args))
+    )
+    already_provided_args.update(kwargs)
+
+    need_to_provide = {
+        k: v for k, v in type_hints.items() if k not in already_provided_args
+    }
+    return need_to_provide
+
+
+def _wrap_sync(func):
+    type_hints = typing.get_type_hints(func, include_extras=True)
+    signature = inspect.signature(func)
+    dependencies = list(collect_dependencies(type_hints=type_hints))
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        missing_dependencies = _missing_kwargs(type_hints, signature, *args, **kwargs)
+        try:
+            ctx = context_var.get()
+        except LookupError:
+            return func(*args, **kwargs)
+        return func(
+            *args,
+            **kwargs,
+            **{
+                dep.name: ctx.resolve(
+                    type_=dep.type,
+                    impl=dep.implementation,
+                    use_cache=dep.use_cache,
+                )
+                for dep in dependencies
+                if dep.name in missing_dependencies
+            },
+        )
+
+    return wrapper
+
+
+def inject(func):
+    return _wrap_sync(func)
