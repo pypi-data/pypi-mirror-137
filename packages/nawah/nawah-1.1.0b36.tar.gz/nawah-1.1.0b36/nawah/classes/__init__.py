3a5f035bd6681e39637d3d0e424a2dd1de6583cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:55 +0000

    Add package nawah/1.1.0b36/nawah-1.1.0b36.tar.gz

---
+++ packages/nawah/nawah-1.1.0b36.tar.gz/nawah-1.1.0b36/nawah/classes/__init__.py
@@ -0,0 +1,42 @@
+from ._attr import (
+    ATTR,
+    SPECIAL_ATTRS,
+    ATTRS_TYPES_ARGS,
+)
+from ._base_model import BaseModel
+from ._dictobj import DictObj
+from ._json_encoder import JSONEncoder
+from ._module import (
+    PERM,
+    EXTN,
+    METHOD,
+    CACHE,
+    CACHED_QUERY,
+    CACHE_CONDITION,
+    ANALYTIC,
+    PRE_HANDLER_RETURN,
+    ON_HANDLER_RETURN,
+)
+from ._package import (
+    CLIENT_APP,
+    ANALYTICS_EVENTS,
+    SYS_DOC,
+    L10N,
+    APP_CONFIG,
+    PACKAGE_CONFIG,
+    USER_SETTING,
+    JOB,
+)
+from ._query import Query
+from ._types import (
+    NAWAH_DOC,
+    NAWAH_ENV,
+    NAWAH_EVENTS,
+    NAWAH_QUERY,
+    NAWAH_QUERY_SPECIAL,
+    NAWAH_QUERY_SPECIAL_GROUP,
+    IP_QUOTA,
+    WATCH_TASK,
+)
+
+from ._exceptions import *
