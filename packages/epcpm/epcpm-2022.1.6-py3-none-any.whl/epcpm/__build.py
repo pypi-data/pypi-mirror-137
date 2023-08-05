aa95463edbe0093ffc4f3ce6381a1c34650b8323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:28 +0000

    Add package epcpm/2022.1.6/epcpm-2022.1.6-py3-none-any.whl

---
+++ packages/epcpm/epcpm-2022.1.6-py3-none-any.whl/epcpm/__build.py
@@ -0,0 +1,9 @@
+# This file has been generated
+
+
+build_system = 'GitHub Actions'
+build_id = '1780240375'
+build_number = '257'
+build_version = None
+job_id = '1780240375_257'
+job_url = 'https://github.com/epcpower/pm/actions/runs/1780240375'
---
+++ packages/epcpm/epcpm-2022.1.6-py3-none-any.whl/epcpm/__init__.py
@@ -0,0 +1,14 @@
+import epcpm._build
+import subprocess
+
+
+def get_git_revision_hash() -> str:
+    return subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
+
+
+__version__ = "2022.1.6"
+__sha__ = get_git_revision_hash()
+# del get_versions
+
+__version_tag__ = "v{}".format(__version__)
+__build_tag__ = epcpm._build.job_id
