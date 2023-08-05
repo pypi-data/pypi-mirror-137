dd74677cca101b3e13b2bbe67e0e43c6356bb239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:35 +0000

    Add package levatas-alira/2.0.8/levatas-alira-2.0.8.tar.gz

---
+++ packages/levatas-alira/levatas-alira-2.0.8.tar.gz/levatas-alira-2.0.8/alira/modules/__init__.py
@@ -0,0 +1,5 @@
+from alira.modules.map import Map
+from alira.modules.s3 import S3
+from alira.modules.selection import Selection
+from alira.modules.flagging import Flagging, CostSensitiveFlagging
+from alira.modules.rest import Rest, RestAuthentication
