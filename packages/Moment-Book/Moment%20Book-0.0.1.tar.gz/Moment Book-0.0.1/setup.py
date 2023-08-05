3d43cb33c2694833f7d898926ff60d162a0d20a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:43 +0000

    Add package Moment-Book/0.0.1/Moment%20Book-0.0.1.tar.gz

---
+++ packages/Moment-Book/Moment%20Book-0.0.1.tar.gz/Moment Book-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A Terminal based digital diary'
+
+# Setting up
+setup(
+    name="Moment Book",
+    version=VERSION,
+    author="PsyQuake (Vishesh Sharma)",
+    author_email="<sharmavishesh022@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['mysql.connector','datetime','tabulate'],
+    keywords=['python', 'book', 'diary', 'notepad', 'cli', 'mysql'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
