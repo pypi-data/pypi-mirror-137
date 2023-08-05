e3d010222ea2957d7bc03dbf8b2aa1f65a58885b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:34 +0000

    Add package st-card-component/0.0.7/st_card_component-0.0.7.tar.gz

---
+++ packages/st-card-component/st_card_component-0.0.7.tar.gz/st_card_component-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="st_card_component",
+    version="0.0.7",
+    author="James Briggs",
+    author_email="",
+    description="Card component for Streamlit",
+    long_description="Card component for Streamlit",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+)
