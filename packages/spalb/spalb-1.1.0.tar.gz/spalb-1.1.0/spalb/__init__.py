5dff59ee3303b2abadc8a511eea42223a0491603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:00 +0000

    Add package spalb/1.1.0/spalb-1.1.0.tar.gz

---
+++ packages/spalb/spalb-1.1.0.tar.gz/spalb-1.1.0/spalb/__init__.py
@@ -0,0 +1,4 @@
+from drogba.straight import comsoal, lcr, rpw
+#from drogba.U_GA import readfile, ShowAns, ShowAns_Pa, Fitness, Fitness_Pa, Efficiency, Efficiency_Pa, sm_index, sm_index_Pa, WeightMappingCrossOver, Crossover, Crossover_selection, SwapMutation, Mutation, Mutation_selection
+from drogba.U_GA import u_ga, parallel_ga
+
