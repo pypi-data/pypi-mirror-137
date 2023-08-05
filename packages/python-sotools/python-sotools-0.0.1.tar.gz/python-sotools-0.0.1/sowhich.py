7108dd63bdae47f54080a05f1a7b6414062987b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:11 +0000

    Add package python-sotools/0.0.1/python-sotools-0.0.1.tar.gz

---
+++ packages/python-sotools/python-sotools-0.0.1.tar.gz/python-sotools-0.0.1/sowhich.py
@@ -0,0 +1,15 @@
+#!/bin/env python3
+
+import sys
+
+from sotools.linker import resolve
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: %s <libmpi.so.x>" % sys.argv[0], file=sys.stderr)
+        sys.exit(1)
+
+    if path := resolve(sys.argv[1])
+        print(path)
+        sys.exit(0)
+    sys.exit(1)
