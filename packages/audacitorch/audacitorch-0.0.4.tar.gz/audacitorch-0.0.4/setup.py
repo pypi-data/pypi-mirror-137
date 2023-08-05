0ae56e396c867a6d77c056899afed21095982970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:12 +0000

    Add package audacitorch/0.0.4/audacitorch-0.0.4.tar.gz

---
+++ packages/audacitorch/audacitorch-0.0.4.tar.gz/audacitorch-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+with open('README.md') as file:
+    long_description = file.read()
+
+
+setup(
+    name='audacitorch',
+    description='',
+    version='0.0.4',
+    author='Hugo Flores Garcia',
+    author_email='hf01049@georgiasouthern.edu',
+    url='https://github.com/hugofloresgarcia/audacitorch',
+    install_requires=[
+        'torch==1.9.0',
+        'jsonschema'
+    ],
+    packages=['audacitorch'],
+    package_data={'audacitorch': ['assets/*']},
+    include_package_data=False,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=[],
+    classifiers=['License :: OSI Approved :: MIT License'],
+    license='MIT')
