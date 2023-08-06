96413932ff9fc406f4f6b60c0ee9f41b4c50ad08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:28 +0000

    Add package idom/0.36.2/idom-0.36.2-py2.py3-none-any.whl

---
+++ packages/idom/idom-0.36.2-py2.py3-none-any.whl/idom/client/_snowpack/pkg/idom-client-react.js
@@ -0,0 +1,5 @@
+import './common/index-6ed86a98.js';
+import './common/index-21e68f69.js';
+export { L as LayoutServerInfo, m as mountWithLayoutServer } from './common/server-a6f83155.js';
+import './common/htm.module-dd7abb54.js';
+import './common/index-6ddd8323.js';
