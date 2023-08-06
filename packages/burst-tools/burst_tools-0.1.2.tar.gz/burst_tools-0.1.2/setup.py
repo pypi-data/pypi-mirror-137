dec89700f599ab4305f50d58579a1d99f7cd842a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:48 +0000

    Add package burst-tools/0.1.2/burst_tools-0.1.2.tar.gz

---
+++ packages/burst-tools/burst_tools-0.1.2.tar.gz/burst_tools-0.1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="burst_tools",
+    version="0.1.2",
+    packages=find_packages(),
+    python_requires='>=3',
+    include_package_data=True
+)
