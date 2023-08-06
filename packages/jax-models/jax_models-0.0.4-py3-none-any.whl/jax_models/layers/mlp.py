c7d106df2024cda9dae993e8e43c5cbde84f2a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:59 +0000

    Add package jax-models/0.0.4/jax_models-0.0.4-py3-none-any.whl

---
+++ packages/jax-models/jax_models-0.0.4-py3-none-any.whl/jax_models/layers/mlp.py
@@ -0,0 +1,35 @@
+import jax.numpy as jnp
+import flax.linen as nn
+
+from ..layers import DepthwiseConv2D
+from typing import Optional, Callable
+
+
+class TransformerMLP(nn.Module):
+    dim: int = 256
+    out_dim: int = 256
+    dropout: float = 0.2
+    use_dwconv: bool = False
+    kernel_init: Callable = nn.initializers.xavier_uniform()
+    deterministic: Optional[bool] = None
+
+    @nn.compact
+    def __call__(self, inputs, deterministic=None):
+        deterministic = nn.merge_param(
+            "deterministic", self.deterministic, deterministic
+        )
+        x = nn.Dense(self.dim, kernel_init=self.kernel_init, name="fc1")(inputs)
+
+        if self.use_dwconv:
+            batch, n, channels = x.shape
+            width = height = int(n ** 0.5)
+            x = jnp.reshape(x, (batch, height, width, channels))
+            x = DepthwiseConv2D((3, 3))(x)
+            x = jnp.reshape(x, (batch, -1, channels))
+
+        x = nn.gelu(x)
+        x = nn.Dropout(self.dropout)(x, deterministic)
+        x = nn.Dense(self.out_dim, kernel_init=self.kernel_init, name="fc2")(x)
+        x = nn.Dropout(self.dropout)(x, deterministic)
+
+        return x
