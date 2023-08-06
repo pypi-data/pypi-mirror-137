a5f86f5f01f83ddb21db372d257bc1cf094eeca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:48 +0000

    Add package dscript/0.1.8/dscript-0.1.8-py3.7.egg

---
+++ packages/dscript/dscript-0.1.8-py3.7.egg/dscript/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.1.8"
+__citation__ = "Sledzieski, Singh, Cowen, Berger. Sequence-based prediction of protein-protein interactions: a structure-aware interpretable deep learning model. Cell Systems, 2021."
+import dscript.models
+import dscript.commands
+import dscript.alphabets
+import dscript.fasta
+import dscript.language_model
+import dscript.pretrained
+import dscript.utils
+
+__all__ = [
+    "models",
+    "commands",
+    "alphabets",
+    "fasta",
+    "language_model",
+    "pretrained",
+    "utils",
+]
