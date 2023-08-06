e8ff51739f1b820864b2ba039b001e1385087788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:35 +0000

    Add package dtwhaclustering/1.0.14/dtwhaclustering-1.0.14-py3-none-any.whl

---
+++ packages/dtwhaclustering/dtwhaclustering-1.0.14-py3-none-any.whl/dtwhaclustering/__init__.py
@@ -0,0 +1,16 @@
+'''
+Dynamic Time Warping based Hierarchical Agglomerative Clustering
+================================================================
+Codes to perform Dynamic Time Warping Based Hierarchical Agglomerative Clustering of GPS data
+
+:author: Utpal Kumar
+:date: 2021/08
+:copyright: 2021, Institute of Earth Sciences, Academia Sinica.
+'''
+
+
+# version
+__version__ = "1.0.14"
+__author__ = "Utpal Kumar"
+__copyright__ = "Copyright 2021 Utpal Kumar"
+__license__ = "Apache License, Version 2.0"
