f9b7abf78d7a271738ca9c0d164c3d98afb0cba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:52 +0000

    Add package intellifire4py/0.5.1/intellifire4py-0.5.1-py3-none-any.whl

---
+++ packages/intellifire4py/intellifire4py-0.5.1-py3-none-any.whl/intellifire4py/model.py
@@ -0,0 +1,46 @@
+
+from typing import List
+
+from pydantic import BaseModel, Field
+
+class IntellifirePollData(BaseModel):
+    name: str
+    serial: str
+    temperature_c: int = Field(alias="temperature")
+    battery: int
+    pilot_on: bool = Field(alias="pilot")
+    light_on: bool = Field(alias="light")
+    flameheight: int = Field(alias="height")
+    fanspeed: int
+    is_hot: bool = Field(alias="hot")
+    is_on: bool = Field(alias="power")
+    thermostat_on: bool = Field(alias="thermostat")
+    raw_thermostat_setpoint: int = Field(alias="setpoint")
+    timer_on: bool = Field(alias="timer")
+    timeremaining_s: int = Field(alias="timeremaining")
+    prepurge: int
+    has_light: bool = Field(alias="feature_light")
+    has_thermostat: bool = Field(alias="feature_thermostat")
+    has_power_vent: bool = Field(alias="power_vent")
+    has_fan: bool = Field(alias="feature_fan")
+    errors: List[int]
+    fw_version: str
+    fw_ver_str: str
+    downtime: int
+    uptime: int
+    connection_quality: int
+    ecm_latency: int
+    ipv4_address: str
+
+    @property
+    def temperature_f(self) -> float:
+        return (self.temperature_c * 9 / 5) + 32
+
+    @property
+    def thermostat_setpoint_c(self) -> int:
+        return self.raw_thermostat_setpoint / 100
+
+    @property
+    def thermostat_setpoint_f(self) -> int:
+        return (self.raw_thermostat_setpoint / 100 * 9 / 5) + 32
+
