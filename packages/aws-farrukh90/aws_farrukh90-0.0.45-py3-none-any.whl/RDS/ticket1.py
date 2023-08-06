7970251745339eb496466161abccb2a6c800d31a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:23 +0000

    Add package aws-farrukh90/0.0.45/aws_farrukh90-0.0.45-py3-none-any.whl

---
+++ packages/aws-farrukh90/aws_farrukh90-0.0.45-py3-none-any.whl/RDS/ticket1.py
@@ -0,0 +1,39 @@
+import os
+import re 
+import sys 
+import time 
+import boto3
+import json 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+def accountverifier():
+    result = boto3.client('sts').get_caller_identity().get('Account')
+    if result == "713287746880":
+        print(bcolors.FAIL + "You are running in a wrong account, please configure your account by running 'aws configure'"  + bcolors.ENDC)
+        sys.exit(1)
+
+    # print(bcolors.OKGREEN + "Account is verified %s " % result + bcolors.ENDC)
+def taskchecker():
+    REGION="us-east-1"
+    DBNAME="django"
+    try:
+        client = boto3.client('rds', region_name=REGION)
+        response = client.describe_db_instances(
+            DBInstanceIdentifier=DBNAME
+            )
+        time.sleep(0.5)
+        print(bcolors.OKGREEN + "RDS is created %s " % REGION + bcolors.ENDC)
+    except Exception as e:
+        print(bcolors.FAIL + "RDS is not created %s " % REGION + bcolors.ENDC)
+        sys.exit(1)
+        time.sleep(0.5)
