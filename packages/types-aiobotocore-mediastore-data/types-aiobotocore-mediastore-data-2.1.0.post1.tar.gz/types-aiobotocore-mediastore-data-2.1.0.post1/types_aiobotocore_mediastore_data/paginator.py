c915c61603051c79331b184ffef461dc6b1ee43b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:21 +0000

    Add package types-aiobotocore-mediastore-data/2.1.0.post1/types-aiobotocore-mediastore-data-2.1.0.post1.tar.gz

---
+++ packages/types-aiobotocore-mediastore-data/types-aiobotocore-mediastore-data-2.1.0.post1.tar.gz/types-aiobotocore-mediastore-data-2.1.0.post1/types_aiobotocore_mediastore_data/paginator.py
@@ -0,0 +1,55 @@
+"""
+Type annotations for mediastore-data service client paginators.
+
+[Open documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/paginators.html)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_mediastore_data.client import MediaStoreDataClient
+    from types_aiobotocore_mediastore_data.paginator import (
+        ListItemsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("mediastore-data") as client:
+        client: MediaStoreDataClient
+
+        list_items_paginator: ListItemsPaginator = client.get_paginator("list_items")
+    ```
+"""
+from typing import Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import ListItemsResponseTypeDef, PaginatorConfigTypeDef
+
+__all__ = ("ListItemsPaginator",)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListItemsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data.html#MediaStoreData.Paginator.ListItems)
+    [Show types-aiobotocore documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/paginators.html#listitemspaginator)
+    """
+
+    async def paginate(
+        self, *, Path: str = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListItemsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data.html#MediaStoreData.Paginator.ListItems.paginate)
+        [Show types-aiobotocore documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/paginators.html#listitemspaginator)
+        """
