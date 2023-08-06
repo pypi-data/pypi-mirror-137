5a4bcaefaf3c564fc8911fb40a0c80ad0c0c3734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:40 +0000

    Add package gtrending/0.3.6/gtrending-0.3.6-py3-none-any.whl

---
+++ packages/gtrending/gtrending-0.3.6-py3-none-any.whl/gtrending/__init__.py
@@ -0,0 +1,11 @@
+from .fetch import (
+    fetch_repos,
+    fetch_developers,
+    languages_list,
+    spoken_languages_list,
+    check_language,
+    check_spoken_language,
+    check_since,
+)
+
+__version__ = "0.3.6"
