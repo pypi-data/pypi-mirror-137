988fcc3a6684b635fdf3a03afa4b228ebf8b942f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:47 +0000

    Add package sine-properties-estimation/0.1.1/sine_properties_estimation-0.1.1-py3-none-any.whl

---
+++ packages/sine-properties-estimation/sine_properties_estimation-0.1.1-py3-none-any.whl/sine_properties_estimation/__init__.py
@@ -0,0 +1,33 @@
+"""
+TODO
+    add docstrings.
+    validate inputs, add exceptions.
+    write tests
+        random sine, and check output dist and for max abs error.
+        input invalid values, and test exceptions
+"""
+from sine_properties_estimation.estimation import SineProperties
+
+
+def calc_sine_properties(raw_data_1d, total_seconds):
+    return SineProperties(raw_data_1d, total_seconds)
+
+
+if __name__ == "__main__":
+    import numpy as np
+
+    # sine with random noise
+    samples = 1000
+    seconds = 10
+    amp_mv = 340
+    phase_rad = 2.2
+    offset_mv = 60
+    freq_ghz = 1.834
+
+    t = np.linspace(0, seconds, samples)
+    signal = amp_mv * np.sin(2 * np.pi * freq_ghz * t + phase_rad) + offset_mv + np.random.normal(0, 50, samples)
+
+    estimation = calc_sine_properties(signal, seconds)
+    print(f'estimate freq is {estimation.est_freq} Hz')
+    print(f'estimated sine amp is {estimation.est_sine_amp}')
+    estimation.print_estimation()
