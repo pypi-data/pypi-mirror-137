1a7c7e740830395514bb8fb35d9c27a3913be541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:15 +0000

    Add package junipertest/0.0.19/junipertest-0.0.19.tar.gz

---
+++ packages/junipertest/junipertest-0.0.19.tar.gz/junipertest-0.0.19/junipercs/system_parser/__init__.py
@@ -0,0 +1,7 @@
+# -------------------------------
+# Juniper Command System
+# '__init__.py'
+# Author: Juan Carlos Juárez.
+# Licensed under MPL 2.0.
+# All rights reserved.
+# -------------------------------
