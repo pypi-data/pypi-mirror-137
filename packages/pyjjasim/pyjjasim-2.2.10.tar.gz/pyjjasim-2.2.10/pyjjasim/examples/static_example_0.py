49ad0f5b843443f2c8af319303680fea3f557259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:35 +0000

    Add package pyjjasim/2.2.10/pyjjasim-2.2.10.tar.gz

---
+++ packages/pyjjasim/pyjjasim-2.2.10.tar.gz/pyjjasim-2.2.10/pyjjasim/examples/static_example_0.py
@@ -0,0 +1,27 @@
+
+from pyjjasim import *
+
+import matplotlib.pyplot as plt
+
+"""
+Static Example 0: Single vortex
+
+Compute a single vortex at the centre of a small square array.
+
+"""
+
+if __name__ == "__main__":
+
+    N = 4
+    sq_array = SquareArray(N, N)
+    f = 0.1
+    n = [0, 0, 0, 0, 1, 0, 0, 0, 0]
+    problem = StaticProblem(sq_array, frustration=f, vortex_configuration=n)
+    config, _, _ = problem.compute()
+    handles = config.plot()
+    plt.show()
+
+
+
+
+
