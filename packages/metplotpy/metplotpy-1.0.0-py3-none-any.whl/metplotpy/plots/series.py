e43b2897e1bb8d8f3b22f266d0f73cdf4ea11f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:55 +0000

    Add package metplotpy/1.0.0/metplotpy-1.0.0-py3-none-any.whl

---
+++ packages/metplotpy/metplotpy-1.0.0-py3-none-any.whl/metplotpy/plots/series.py
@@ -0,0 +1,67 @@
+"""
+Class Name: series.py
+ """
+__author__ = 'Minna Win'
+
+import itertools
+import metcalcpy.util.utils as utils
+
+
+class Series:
+    """
+        Represents a series object of data points and their plotting style
+        elements (line colors, markers, linestyles, etc.)
+        Each series is depicted by a single line in the performance diagram.
+
+    """
+
+    def __init__(self, config, idx: int, input_data, y_axis: int = 1):
+        self.config = config
+        self.idx = idx
+        self.input_data = input_data
+        self.y_axis = y_axis
+        self.plot_disp = config.plot_disp[idx]
+        self.plot_stat = config.plot_stat
+        self.color = config.colors_list[idx]
+        self.marker = config.marker_list[idx]
+        self.linewidth = config.linewidth_list[idx]
+        self.linestyle = config.linestyles_list[idx]
+        self.user_legends = config.user_legends[idx]
+        self.series_order = config.series_ordering_zb[idx]
+
+        # Variables used for subsetting the input dataframe
+        # series variable names defined in the series_val_1 setting
+        self.series_vals_1 = config.series_vals_1
+        self.series_vals_2 = config.series_vals_2
+        self.all_series_vals = self.series_vals_1 + self.series_vals_2
+
+        # Forecast variable names defined in the fcst_var_val setting
+        # Renamed fcst_vars_1 and fcst_vars_2 in config file
+        # self.fcst_vars_1 = config.fcst_vars_1
+        # self.fcst_vars_2 = config.fcst_vars_2
+        # to this:
+        self.fcst_vars_1 = config.fcst_var_val_1
+        self.fcst_vars_2 = config.fcst_var_val_2
+
+        # Column names corresponding to the series variable names
+        self.series_val_names = config.series_val_names
+
+        self.all_fields_values_no_indy = self._create_all_fields_values_no_indy()
+
+        # Subset the data for this series object
+        self.series_points = self._create_series_points()
+
+    def _create_series_points(self):
+        """
+        Abstract method that create series points.
+
+        """
+        raise NotImplementedError
+
+    def _create_all_fields_values_no_indy(self) -> dict:
+        """
+        Creates a dictionary with two keys that represents each axis
+        values - dictionaries of field values pairs of all series variables (without indy variable)
+        :return: dictionary with field-values pairs for each axis
+        """
+        return {}
---
+++ packages/metplotpy/metplotpy-1.0.0-py3-none-any.whl/metplotpy/plots/util.py
@@ -0,0 +1,67 @@
+import argparse
+
+"""
+    Collection of utility functions used by multiple plotting classes
+"""
+__author__ = 'Minna Win'
+
+import matplotlib
+import numpy as np
+
+
+def read_config_from_command_line():
+    """
+        Read the "custom" config file from the command line
+
+        Args:
+
+        Returns:
+            The full path to the config file
+    """
+    # Create Parser
+    parser = argparse.ArgumentParser(description='Generates a performance diagram')
+
+    # Add arguments
+    parser.add_argument('Path', metavar='path', type=str,
+                        help='the full path to config file')
+
+    # Execute the parse_args() method
+    args = parser.parse_args()
+    return args.Path
+
+
+def alpha_blending(hex_color: str, alpha: float) -> str:
+    """ Alpha color blending as if on the white background.
+        Useful for gridlines
+
+    Args:
+        @param hex_color - the color in hex
+        @param alpha - Alpha value between 0 and 1
+    Returns: blended hex color
+    """
+    foreground_tuple = matplotlib.colors.hex2color(hex_color)
+    foreground_arr = np.array(foreground_tuple)
+    final = tuple((1. - alpha) + foreground_arr * alpha)
+    return matplotlib.colors.rgb2hex(final)
+
+
+def apply_weight_style(text: str, weight: int) -> str:
+    """
+    Applied HTML style weight to text:
+    1 - none
+    2 - bold
+    3 - italic
+    4 - bold italic
+
+    :param text: text to style
+    :param weight: - int representation of the style
+    :return: styled text
+    """
+    if len(text) > 0:
+        if weight == 2:
+            return '<b>' + text + '</b>'
+        if weight == 3:
+            return '<i>' + text + '</b>'
+        if weight == 4:
+            return '<b><i>' + text + '</i></b>'
+    return text
