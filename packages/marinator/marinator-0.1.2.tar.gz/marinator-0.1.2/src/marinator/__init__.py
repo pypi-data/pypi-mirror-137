5d6f407314f7805c847be75c2cd0f9a330e6c49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:10 +0000

    Add package marinator/0.1.2/marinator-0.1.2.tar.gz

---
+++ packages/marinator/marinator-0.1.2.tar.gz/marinator-0.1.2/src/marinator/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from . import base
+
+from .base import Marinator
+
+if __name__ == "__main__":
+    marinator = Marinator()
+    marinator.run()
+else:
+    __path__ = []
