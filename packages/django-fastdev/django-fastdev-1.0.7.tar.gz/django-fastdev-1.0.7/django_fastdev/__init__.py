d36469e33be19245bd1c2896538a14ef75763a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:19 +0000

    Add package django-fastdev/1.0.7/django-fastdev-1.0.7.tar.gz

---
+++ packages/django-fastdev/django-fastdev-1.0.7.tar.gz/django-fastdev-1.0.7/django_fastdev/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '1.0.7'
+
+
+default_app_config = 'django_fastdev.django_app.FastDevConfig'
+
+
+from django.core.management.commands.runserver import Command
+Command.check = lambda *_, **__: print('Skipped model validations')
+Command.check_migrations = lambda *_, **__: print('Skipped migrations checks')
