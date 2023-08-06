4a16c27afb6877ec446c145232d41a54b66b28b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:39 +0000

    Add package throttled/0.1.16a19/throttled-0.1.16a19-py3-none-any.whl

---
+++ packages/throttled/throttled-0.1.16a19-py3-none-any.whl/throttled/strategy/base.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+from throttled.exceptions import RateLimitExceeded
+from throttled.models import Hit, Rate
+from throttled.storage.base import Storage
+
+
+class StorageFactory(ABC):
+    @abstractmethod
+    def get_storage_for_strategy(self, strategy: "Strategy") -> Storage:
+        ...
+
+
+class Strategy:
+    __slots__ = ("__limit", "__storage")
+
+    def __init__(self, limit: Rate, storage_factory: StorageFactory, limiter):
+        self.__limit = limit
+        self.__storage = storage_factory.get_storage_for_strategy(self)
+
+    @property
+    def limit(self) -> Rate:
+        return self.__limit
+
+    def maybe_block(self, hit: Hit):
+        """
+        :param hit: The hit to be tested
+        :raises: RateLimitExceeded
+        """
+        window = self.__storage.get_current_window(hit)
+        if window.incr(hit.cost) > self.limit.hits:
+            raise RateLimitExceeded(
+                hit.key,
+                retry_after=window.get_remaining_seconds(),
+            )
