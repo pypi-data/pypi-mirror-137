079d9c0e3e6d382b96db7be145814ee306070bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:27 +0000

    Add package easycm/0.0.5/easycm-0.0.5.tar.gz

---
+++ packages/easycm/easycm-0.0.5.tar.gz/easycm-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name='easycm',
+    version='0.0.5',
+    author='Victor Henrique Alves Ribeiro',
+    description='A simple python package to plot confusion matrices',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['easycm'],
+    license='BSD-3',
+    url='https://github.com/vhrique/easycm',
+    project_urls={
+        'Source': 'https://github.com/vhrique/easycm',
+        'Tracker': 'https://github.com/vhrique/easycm/issues',
+    }
+)
