81e216ca12b7f0ec267fafb507fc60261d35318e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:49 +0000

    Add package pacote-coupa/0.0.1/pacote_coupa-0.0.1.tar.gz

---
+++ packages/pacote-coupa/pacote_coupa-0.0.1.tar.gz/pacote_coupa-0.0.1/pacote_coupa/pegar_salvar_arquivo.py
@@ -0,0 +1,32 @@
+﻿#------------------------------------------------------------------------------------------------------
+# imports
+import PySimpleGUI as gui
+
+#------------------------------------------------------------------------------------------------------
+gui.theme('dark grey 9')
+
+#------------------------------------------------------------------------------------------------------
+# Funções
+def pegar_arquivo():
+    pegando_arquivo: str = gui.popup_get_file(
+        "Pegando Arquivo",
+        default_path = "",
+        save_as=False,
+        no_window=True,
+    )
+    
+    return pegando_arquivo
+
+#------------------------------------------------------------------------------------------------------
+def salvar_arquivo_csv():
+    salvarCSV: str = gui.popup_get_file(
+        "Salvar Como",
+        save_as=True,
+        no_window=True,
+        default_extension=".csv",
+        file_types=(("CSV", ".csv"),),
+    )
+    
+    return salvarCSV
+
+#------------------------------------------------------------------------------------------------------
