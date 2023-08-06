71d5868446a56c4e042b59fbc787845870306c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:59 +0000

    Add package diarium-cli/0.1.1/diarium-cli-0.1.1.tar.gz

---
+++ packages/diarium-cli/diarium-cli-0.1.1.tar.gz/diarium-cli-0.1.1/README.md
@@ -0,0 +1,12 @@
+# Diarium-CLI
+Command-line interface providing random functions for the journaling app [Diarium](https://timopartl.com/).
+
+
+## Install and launch
+1. `git clone https://github.com/YelovSK/Diarium-CLI.git`
+2. `cd Diarium-CLI`
+3. `pip install .`
+4. `diarium-cli` to run
+
+## Commands
+Type `help` or `help [command]` after launching the application.
---
+++ packages/diarium-cli/diarium-cli-0.1.1.tar.gz/diarium-cli-0.1.1/setup.py
@@ -0,0 +1,18 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="diarium-cli",
+    version="0.1.1",
+    description="CLI tool for journaling app Diarium",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Yelov",
+    packages=find_packages(),
+    include_package_date=True,
+    install_requires=open("requirements.txt").read().splitlines(),
+    license_files=("LICENSE", ),
+    entry_points="""
+        [console_scripts]
+        diarium-cli=src.main:cli
+    """,
+)
