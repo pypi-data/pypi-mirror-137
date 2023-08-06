40ad041c4d32dfeb1599a880b80369cf8ade1ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:36:44 +0000

    Add package st-card-component/0.0.10/st_card_component-0.0.10-py3-none-any.whl

---
+++ packages/st-card-component/st_card_component-0.0.10-py3-none-any.whl/st_card_component/frontend/build/precache-manifest.1f866a8115f6dd83e0c8d1eb755ea353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6b043b0d16aaf627c948f737380584",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be5cb160731a41a76b92",
+    "url": "./static/js/2.2e142390.chunk.js"
+  },
+  {
+    "revision": "fdc04156fe2caf5da02c72a4c7efb329",
+    "url": "./static/js/2.2e142390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43d64769521ebf775119",
+    "url": "./static/js/main.4195b895.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
