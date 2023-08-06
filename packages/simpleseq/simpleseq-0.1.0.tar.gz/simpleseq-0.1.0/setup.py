1f86a572ea78d61eedbbe68c32e5bc2a27aec32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:18 +0000

    Add package simpleseq/0.1.0/simpleseq-0.1.0.tar.gz

---
+++ packages/simpleseq/simpleseq-0.1.0.tar.gz/simpleseq-0.1.0/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(
+    name='simpleseq',
+    version='0.1.0',
+    description='Python package to convert image sequences to padding sequence',
+    author='Ramesh Babu',
+    author_email='rameshkanna95@gmail.com',
+    url='https://github.com/rameshbabuvfx/simpleseq',
+    keywords='simpleseq'
+)
