ab67da986edceebde6c7ad352441a05260723960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:06 +0000

    Add package gcp-airflow-foundations-dev/0.3.7/gcp-airflow-foundations-dev-0.3.7.tar.gz

---
+++ packages/gcp-airflow-foundations-dev/gcp-airflow-foundations-dev-0.3.7.tar.gz/gcp-airflow-foundations-dev-0.3.7/gcp_airflow_foundations/base_class/file_table_config.py
@@ -0,0 +1,20 @@
+from pydantic import validator
+from pydantic.dataclasses import dataclass
+
+from typing import List, Optional
+
+@dataclass
+class FileTableConfig:
+    """
+    Attributes:
+        directory_prefix: optional directory prefix override per table
+        metadata_file: the relative path to the metadata file within the GSC bucket
+        schema_file: the (optional) relative path to the schema file for the table
+        flag_file_path: the path to the optional success file
+        bq_upload_option: either "BASH" or "GCS"
+    """
+    directory_prefix: Optional[str]
+    metadata_file: Optional[str]
+    schema_file: Optional[str]
+    flag_file_path: Optional[str]
+    parquet_upload_option: Optional[str]
