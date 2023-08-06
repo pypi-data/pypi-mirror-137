1dd4d223d7b8db0d6f3ac8fa8ee45f0e129d4849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:20 +0000

    Add package scirisweb/0.17.1/scirisweb-0.17.1.tar.gz

---
+++ packages/scirisweb/scirisweb-0.17.1.tar.gz/scirisweb-0.17.1/scirisweb/sw_version.py
@@ -0,0 +1,5 @@
+__all__ = ['__version__', '__versiondate__', '__license__']
+
+__version__      = '0.17.1'
+__versiondate__  = '2022-02-02'
+__license__      = 'ScirisWeb %s (%s) -- (c) Sciris.org' % (__version__, __versiondate__)
