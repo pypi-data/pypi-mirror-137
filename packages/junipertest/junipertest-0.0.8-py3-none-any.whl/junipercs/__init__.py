9476dee464d596a0487c0424753d8b4ecfe4b9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:22 +0000

    Add package junipertest/0.0.8/junipertest-0.0.8-py3-none-any.whl

---
+++ packages/junipertest/junipertest-0.0.8-py3-none-any.whl/junipercs/__init__.py
@@ -0,0 +1,11 @@
+# -------------------------------
+# Juniper Command System
+# '__init__.py'
+# Author: Juan Carlos Juárez.
+# Licensed under MPL 2.0.
+# All rights reserved.
+# -------------------------------
+
+from .juniper_source import *
+from .ply.lex import *
+from .ply.yacc import *
