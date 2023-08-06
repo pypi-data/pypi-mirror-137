3db1f7502fde70429dbe95b89dc44efbae9e2fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:04 +0000

    Add package kronus/2.3/kronus-2.3-py3-none-any.whl

---
+++ packages/kronus/kronus-2.3-py3-none-any.whl/kronus/__init__.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#+ Autor:  	Ran#
+#+ Creado: 	2022/02/05 21:16:06.895461
+#+ Editado:	2022/02/05 21:27:46.602859
+# ------------------------------------------------------------------------------
+
+'''
+info e variable de kronus
+'''
+
+# ------------------------------------------------------------------------------
+
+import os
+
+from .kronus import agora, separar
+
+# ------------------------------------------------------------------------------
+
+RAIZ = os.path.dirname(os.path.abspath(__file__))
+
+# ------------------------------------------------------------------------------
+
+__nome__ = 'kronus'
+__version__ = '2.3'
+__autor__ = 'Ran#'
+__correo_ = 'ran-n@tutanota.com'
+__creditos__ = 'Ran#'
+__licenza__ = 'GPLv3'
+__raiz__ = RAIZ
+__descricion__ = 'Timestamp module'
+__ligazon__ = 'https://wwww.github.com/Ran-n/kronus'
+__problemas__ = 'https://wwww.github.com/Ran-n/kronus/issues'
+
+# ------------------------------------------------------------------------------
