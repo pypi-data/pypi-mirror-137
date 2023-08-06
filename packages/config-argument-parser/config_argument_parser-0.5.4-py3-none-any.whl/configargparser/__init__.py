c3a254afbedc99657451a1daed568be4bb7dc1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:00 +0000

    Add package config-argument-parser/0.5.4/config_argument_parser-0.5.4-py3-none-any.whl

---
+++ packages/config-argument-parser/config_argument_parser-0.5.4-py3-none-any.whl/configargparser/__init__.py
@@ -0,0 +1,7 @@
+"""A package help automatically create command-line interface from configuration or code."""
+
+__version__ = "0.5.4"
+
+from .cap import ConfigArgumentParser
+
+__all__ = ["ConfigArgumentParser"]
