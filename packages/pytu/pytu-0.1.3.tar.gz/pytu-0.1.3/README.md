be0ee2a0401bfb95f7c8619c25a39588bce13850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:22 +0000

    Add package pytu/0.1.3/pytu-0.1.3.tar.gz

---
+++ packages/pytu/pytu-0.1.3.tar.gz/pytu-0.1.3/README.md
@@ -0,0 +1,56 @@
+# PyTu
+My personal Python Tuuls :-) 
+
+## Install & init
+```
+> pip install pytu
+```
+
+```py
+from pytu.tools import Tools  # general python tools
+from pytu.Matt import Matt    # GUI automation tool based on PyAutoGUI
+```
+
+## Logging
+```py
+Tools.log('Hello world')  # prints "[2022-02-07 16:31:14] Hello world"
+
+Tools.log_path = "log/{year}-{month}-{day}_{hour}.log"  # from now log messages are also saved in text file
+```
+
+## Type conversions
+```py
+Tools.str(datetime.now())                      # "2022-02-07 16:31:14"
+Tools.str("5.1234000")                         # "5.123"
+Tools.str(ValueError('something went wrong'))  # "ValueError: something went wrong"
+
+Tools.datetime('7.2.2022')    # datetime(2022, 2, 7)
+Tools.datetime('2/7/2022')    # datetime(2022, 2, 7)
+Tools.datetime('2022-07-02')  # datetime(2022, 2, 7)
+```
+
+## Automation 
+```py
+# Init
+matt = Matt(
+	cache_file = 'temp/cache/matt.json',
+	logger = Tools,
+)
+matt.set_ui({                     # recommended, but not mandatory
+	'btn_ok' => 'ui/btn_ok.png',
+	'btn_home' => 'ui/btn_home.png',
+	'msg_ok' => 'ui/msg_ok.png',
+	'msg_error' => 'ui/msg_error.png',
+	'homescreen' => 'ui/homescreen.png',
+})
+
+# Automate
+matt.click('btn_ok')
+
+found = matt.which(['msg_ok', 'msg_error'])  # waits for one of the messages to show up
+if found == 'msg_error':
+	print('Error message found')
+elif found == 'msg_ok':
+	matt.click('btn_home')
+	matt.wait('homepage')
+```
---
+++ packages/pytu/pytu-0.1.3.tar.gz/pytu-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pytu']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pytu',
+    'version': '0.1.3',
+    'description': 'My personal Python Tuuls :-)',
+    'long_description': '# PyTu\nMy personal Python Tuuls :-) \n\n## Install & init\n```\n> pip install pytu\n```\n\n```py\nfrom pytu.tools import Tools  # general python tools\nfrom pytu.Matt import Matt    # GUI automation tool based on PyAutoGUI\n```\n\n## Logging\n```py\nTools.log(\'Hello world\')  # prints "[2022-02-07 16:31:14] Hello world"\n\nTools.log_path = "log/{year}-{month}-{day}_{hour}.log"  # from now log messages are also saved in text file\n```\n\n## Type conversions\n```py\nTools.str(datetime.now())                      # "2022-02-07 16:31:14"\nTools.str("5.1234000")                         # "5.123"\nTools.str(ValueError(\'something went wrong\'))  # "ValueError: something went wrong"\n\nTools.datetime(\'7.2.2022\')    # datetime(2022, 2, 7)\nTools.datetime(\'2/7/2022\')    # datetime(2022, 2, 7)\nTools.datetime(\'2022-07-02\')  # datetime(2022, 2, 7)\n```\n\n## Automation \n```py\n# Init\nmatt = Matt(\n\tcache_file = \'temp/cache/matt.json\',\n\tlogger = Tools,\n)\nmatt.set_ui({                     # recommended, but not mandatory\n\t\'btn_ok\' => \'ui/btn_ok.png\',\n\t\'btn_home\' => \'ui/btn_home.png\',\n\t\'msg_ok\' => \'ui/msg_ok.png\',\n\t\'msg_error\' => \'ui/msg_error.png\',\n\t\'homescreen\' => \'ui/homescreen.png\',\n})\n\n# Automate\nmatt.click(\'btn_ok\')\n\nfound = matt.which([\'msg_ok\', \'msg_error\'])  # waits for one of the messages to show up\nif found == \'msg_error\':\n\tprint(\'Error message found\')\nelif found == \'msg_ok\':\n\tmatt.click(\'btn_home\')\n\tmatt.wait(\'homepage\')\n```\n',
+    'author': 'Michal Mikolas',
+    'author_email': 'nanuqcz@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
