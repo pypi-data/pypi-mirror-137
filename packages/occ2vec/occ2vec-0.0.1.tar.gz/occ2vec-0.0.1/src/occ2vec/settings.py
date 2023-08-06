8b43193034983f9b3d7136845876f7f089259782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:17 +0000

    Add package occ2vec/0.0.1/occ2vec-0.0.1.tar.gz

---
+++ packages/occ2vec/occ2vec-0.0.1.tar.gz/occ2vec-0.0.1/src/occ2vec/settings.py
@@ -0,0 +1,42 @@
+""" SETTINGS
+
+This scripts defines standard settings NOT to be changed
+
+Date: February 1, 2022
+"""
+#------------------------------------------------------------------------------
+# LIBRARIES
+#------------------------------------------------------------------------------
+import pandas as pd
+
+#------------------------------------------------------------------------------
+# GLOBAL PARAMETERS
+#------------------------------------------------------------------------------
+class Globals():
+    """ Global variables across scripts
+        
+    """
+    # --------------------
+    # Constructor function
+    # --------------------
+    def __init__(self):
+        
+        ## Set parameters
+        
+        # Date format
+        self.DATE_FORMAT = "%Y-%m-%d"
+        
+        # Dateparser
+        self.DATE_PARSER  = lambda x: pd.to_datetime(x, format = self.DATE_FORMAT)
+
+        # Important columns 
+        self.DB_COL = "DB"
+        self.DATE_COL = "Date"
+        self.ONET_COL = "O*NET-SOC Code"
+        
+    
+        
+        
+        
+        
+        
