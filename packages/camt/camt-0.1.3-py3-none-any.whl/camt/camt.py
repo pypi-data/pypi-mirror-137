d3cba8f6bf9291dd4adb7a47a55866b26e385990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:27:11 +0000

    Add package camt/0.1.3/camt-0.1.3-py3-none-any.whl

---
+++ packages/camt/camt-0.1.3-py3-none-any.whl/camt/camt.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+import os
+from shutil import copy2
+from shutil import make_archive
+from typing import Tuple, Iterable
+
+
+def get_labeled_files(path: str) -> list:
+    labeled_files = []
+
+    for root, _, filenames in os.walk(path, topdown=True):
+        filenames.sort()
+
+        for index, filename in enumerate(filenames):
+            if filename.endswith(".xml"):
+                img = os.path.join(root, filenames[index - 1])
+                xml = os.path.join(root, filename)
+                labeled_files.append((img, xml))
+
+    return labeled_files
+
+
+def _copy(sources: Iterable[Tuple[str]], dst: str):
+    for src1, src2 in sources:
+        copy2(src1, dst)
+        copy2(src2, dst)
+
+
+def archive(dest_dir: str, src: str, _format: str = 'zip') -> None:
+    """
+    Creates a compressed file in the format of zip, tar, etc.
+    :param dest_dir:
+    :param src:
+    :param _format:
+    :return:
+    """
+    make_archive(dest_dir, _format, src)
