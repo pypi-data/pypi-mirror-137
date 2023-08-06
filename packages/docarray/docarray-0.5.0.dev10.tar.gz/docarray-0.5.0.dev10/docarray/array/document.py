5bd14dd18420be2682be121da5554a214bccf996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:40:45 +0000

    Add package docarray/0.5.0.dev10/docarray-0.5.0.dev10.tar.gz

---
+++ packages/docarray/docarray-0.5.0.dev10.tar.gz/docarray-0.5.0.dev10/docarray/array/document.py
@@ -0,0 +1,52 @@
+from typing import Optional, overload, TYPE_CHECKING, Dict, Union
+
+from .base import BaseDocumentArray
+from .mixins import AllMixins
+
+if TYPE_CHECKING:
+    from ..types import (
+        DocumentArraySourceType,
+        DocumentArrayLike,
+        DocumentArraySqlite,
+        DocumentArrayInMemory,
+    )
+    from .storage.sqlite import SqliteConfig
+
+
+class DocumentArray(AllMixins, BaseDocumentArray):
+    @overload
+    def __new__(
+        cls, _docs: Optional['DocumentArraySourceType'] = None, copy: bool = False
+    ) -> 'DocumentArrayInMemory':
+        """Create an in-memory DocumentArray object."""
+        ...
+
+    @overload
+    def __new__(
+        cls,
+        _docs: Optional['DocumentArraySourceType'] = None,
+        storage: str = 'sqlite',
+        config: Optional[Union['SqliteConfig', Dict]] = None,
+    ) -> 'DocumentArraySqlite':
+        """Create a SQLite-powered DocumentArray object."""
+        ...
+
+    def __new__(cls, *args, storage: str = 'memory', **kwargs) -> 'DocumentArrayLike':
+        if cls is DocumentArray:
+            if storage == 'memory':
+                from .memory import DocumentArrayInMemory
+
+                instance = super().__new__(DocumentArrayInMemory)
+            elif storage == 'sqlite':
+                from .sqlite import DocumentArraySqlite
+
+                instance = super().__new__(DocumentArraySqlite)
+            elif storage == 'weaviate':
+                from .weaviate import DocumentArrayWeaviate
+
+                instance = super().__new__(DocumentArrayWeaviate)
+            else:
+                raise ValueError(f'storage=`{storage}` is not supported.')
+        else:
+            instance = super().__new__(cls)
+        return instance
