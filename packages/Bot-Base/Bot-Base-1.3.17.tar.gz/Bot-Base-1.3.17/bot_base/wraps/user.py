b581d0706a5b27eb7c8e89100cfd8b18fda61fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:42 +0000

    Add package Bot-Base/1.3.17/Bot-Base-1.3.17.tar.gz

---
+++ packages/Bot-Base/Bot-Base-1.3.17.tar.gz/Bot-Base-1.3.17/bot_base/wraps/user.py
@@ -0,0 +1,18 @@
+import nextcord
+from nextcord.ext import commands
+
+from bot_base.wraps.meta import Meta
+
+
+class WrappedUser(Meta, nextcord.User):
+    """Wraps discord.user for ease of stuff"""
+
+    @classmethod
+    async def convert(cls, ctx, argument: str) -> "WrappedUser":
+        user: nextcord.User = await commands.UserConverter().convert(
+            ctx=ctx, argument=argument
+        )
+        return cls(user, ctx.bot)
+
+    def __getattr__(self, item):
+        return getattr(self._wrapped_item, item)
