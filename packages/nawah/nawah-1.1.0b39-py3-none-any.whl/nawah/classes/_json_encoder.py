496e495bdc1b65abc000014989711851022a62c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:53 +0000

    Add package nawah/1.1.0b39/nawah-1.1.0b39-py3-none-any.whl

---
+++ packages/nawah/nawah-1.1.0b39-py3-none-any.whl/nawah/classes/_json_encoder.py
@@ -0,0 +1,18 @@
+import datetime
+import json
+
+from bson import ObjectId
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        elif type(o) == datetime.datetime:
+            return o.isoformat()
+        elif type(o) == bytes:
+            return True
+        try:
+            return json.JSONEncoder.default(self, o)
+        except TypeError:
+            return str(o)
