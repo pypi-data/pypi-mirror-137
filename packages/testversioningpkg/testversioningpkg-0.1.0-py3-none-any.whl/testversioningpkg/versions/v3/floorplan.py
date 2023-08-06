fcb64aa018edff572577546083ad82911f65d686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:24 +0000

    Add package testversioningpkg/0.1.0/testversioningpkg-0.1.0-py3-none-any.whl

---
+++ packages/testversioningpkg/testversioningpkg-0.1.0-py3-none-any.whl/testversioningpkg/versions/v3/floorplan.py
@@ -0,0 +1,19 @@
+from math import floor
+import re
+
+
+def get_floorplans():
+    floorplans = {
+        "floorplan_config": 2,
+        "floorplans" :
+        [
+            {
+                "floorplan_id": 1,
+                "floorplan_name": "Floor 2",
+                "floorplan_url": "https://jublia.com/floorplan2",
+                "sliced_floorplan_url": "https://buzz-images.jublia.com/floorplan/d8ed1cb8afcae1cfb7d6c6d3cd5b3114772dd7274f41d8dcc98285569550bae68e50e1395fdfe8cb40b8cdcc46b9c95617933bec8f1bba6876d462786f7c0223/floorplan_37_4/sliced",
+                "markers": []
+            }
+        ]
+    }
+    return floorplans
