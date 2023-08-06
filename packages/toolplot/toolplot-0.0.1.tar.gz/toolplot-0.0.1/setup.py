dc9b0c5201ae55c62db7a283f42a7f55335c675c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:20 +0000

    Add package toolplot/0.0.1/toolplot-0.0.1.tar.gz

---
+++ packages/toolplot/toolplot-0.0.1.tar.gz/toolplot-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(
+    name='toolplot',
+    version='0.0.1',
+    packages=['toolplot'],
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'toolstr',
+        'tooltime',
+    ],
+)
+
