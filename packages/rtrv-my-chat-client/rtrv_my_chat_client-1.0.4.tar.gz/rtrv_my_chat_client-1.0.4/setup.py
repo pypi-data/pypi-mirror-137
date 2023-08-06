c796cc2733e7973bad29e8d69a583b1da5d9cb8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:50 +0000

    Add package rtrv-my-chat-client/1.0.4/rtrv_my_chat_client-1.0.4.tar.gz

---
+++ packages/rtrv-my-chat-client/rtrv_my_chat_client-1.0.4.tar.gz/rtrv_my_chat_client-1.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name="rtrv_my_chat_client",
+      version="1.0.4",
+      description="my_chat_client",
+      author="RTretyakov",
+      author_email="rtretyakov@outlook.com",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex'],
+      scripts=['client/client_run'],
+      package_data={
+            # If any package contains *.txt files, include them:
+            "": ["*.ini"],
+            # And include any *.dat files found in the "data" subdirectory
+            # of the "mypkg" package, also:
+            # "mypkg": ["data/*.dat"],
+      }
+      )
