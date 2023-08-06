1fdd76038cb5c72cd59b00ed7d719d9d08a416b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:22 +0000

    Add package dewloosh.math/0.0.1a1/dewloosh.math-0.0.1a1.tar.gz

---
+++ packages/dewloosh.math/dewloosh.math-0.0.1a1.tar.gz/dewloosh.math-0.0.1a1/src/dewloosh/math/linalg/frontal/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from dewloosh.math.linalg.frontal.frontal import *
+from dewloosh.math.linalg.frontal.proc import *
+from dewloosh.math.linalg.frontal.path import optimal_elimination_path
