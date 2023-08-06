b6e6feadc2e1e3dc0763b5b5b4fcc2c37e8b89db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:38 +0000

    Add package pylzy-nightly/0.0.13.dev20220205/pylzy-nightly-0.0.13.dev20220205.tar.gz

---
+++ packages/pylzy-nightly/pylzy-nightly-0.0.13.dev20220205.tar.gz/pylzy-nightly-0.0.13.dev20220205/lzy/api/utils.py
@@ -0,0 +1,68 @@
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterable,
+    Type,
+    Tuple,
+    TypeVar,
+    get_type_hints,
+)
+
+from lzy.api._proxy import proxy
+from lzy.api.lazy_op import LzyOp
+from lzy.api.result import Result, Just, Nothing
+
+T = TypeVar("T")  # pylint: disable=invalid-name
+
+
+# TODO: remove?
+def print_lzy_ops(ops: Iterable[LzyOp]) -> None:
+    for lzy_op in ops:
+        # noinspection PyProtectedMember
+        # pylint: disable=protected-access
+        print(repr(lzy_op.signature.func))
+
+
+TypeInferResult = Result[type]
+
+
+def infer_return_type(func: Callable) -> TypeInferResult:
+    hints = get_type_hints(func)
+    if "return" not in hints:
+        return Nothing()
+
+    or_type = hints["return"]
+    if hasattr(or_type, "__origin__"):
+        return Just(or_type.__origin__)  # type: ignore
+
+    if isinstance(or_type, type):
+        return Just(or_type)
+
+    return Nothing()
+
+
+def infer_arg_types(*args) -> Tuple[type, ...]:
+    # noinspection PyProtectedMember
+    # pylint: disable=protected-access
+    return tuple(
+        arg._op.return_type
+        if is_lazy_proxy(arg) else type(arg)
+        for arg in args
+    )
+
+
+def is_lazy_proxy(obj: Any) -> bool:
+    cls = type(obj)
+    return hasattr(cls, "__lzy_proxied__") and cls.__lzy_proxied__
+
+
+def lazy_proxy(
+    materialization: Callable[[], T], return_type: Type[T], obj_attrs: Dict[str, Any]
+) -> Any:
+    return proxy(
+        materialization,
+        return_type,
+        cls_attrs={"__lzy_proxied__": True},
+        obj_attrs=obj_attrs,
+    )
