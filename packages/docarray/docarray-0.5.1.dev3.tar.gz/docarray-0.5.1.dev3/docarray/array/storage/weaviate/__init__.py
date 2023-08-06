839b8076ac43d0947a4c24575eb9939d82a70818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:35 +0000

    Add package docarray/0.5.1.dev3/docarray-0.5.1.dev3.tar.gz

---
+++ packages/docarray/docarray-0.5.1.dev3.tar.gz/docarray-0.5.1.dev3/docarray/array/storage/weaviate/__init__.py
@@ -0,0 +1,9 @@
+from .backend import BackendMixin, WeaviateConfig
+from .getsetdel import GetSetDelMixin
+from .seqlike import SequenceLikeMixin
+
+__all__ = ['StorageMixins', 'WeaviateConfig']
+
+
+class StorageMixins(BackendMixin, GetSetDelMixin, SequenceLikeMixin):
+    ...
