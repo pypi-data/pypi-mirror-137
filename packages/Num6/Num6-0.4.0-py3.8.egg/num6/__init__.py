39f36992b046aa574c55bef033433cb139769524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:31 +0000

    Add package Num6/0.4.0/Num6-0.4.0-py3.8.egg

---
+++ packages/Num6/Num6-0.4.0-py3.8.egg/num6/__init__.py
@@ -0,0 +1,18 @@
+'''A powerfull encrypter and decrypter tool.
+
+Num6 is a powerfull Encrypter and Decrypter tool for developers or hackers. Depends on user prospective.
+
+This tool has been developed for educational and research based use only. We will not responsible for any type of miss use.
+
+Stay with open source, keep contributing in open source.
+
+Thank you.
+Developer Md. Almas Ali
+Email: almaspr3@gmail.com
+'''
+
+from .packages import *
+
+__version__ = '0.3.2'
+
+
