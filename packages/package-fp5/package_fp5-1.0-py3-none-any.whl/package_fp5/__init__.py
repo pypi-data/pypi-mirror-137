dd6c5bcdc4086bab6dce3b799ab882be7eccf6b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:40 +0000

    Add package package-fp5/1.0/package_fp5-1.0-py3-none-any.whl

---
+++ packages/package-fp5/package_fp5-1.0-py3-none-any.whl/package_fp5/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  5 20:36:41 2022
+
+@author: ALEXANDRA
+"""
+
+import sub_package1
+
+print(sub_package1.mod1())
+
+print(sub_package1.mod2())
+
+
+import sub_package2
+
+print(sub_package2.mod3())
+
+print(sub_package2.mod4())
+    
+
