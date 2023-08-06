d6ae12852a1b055465aa91df039c364d3471aa9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:49 +0000

    Add package pulsar-excel/0.0.2/pulsar_excel-0.0.2.tar.gz

---
+++ packages/pulsar-excel/pulsar_excel-0.0.2.tar.gz/pulsar_excel-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pulsar_excel',
+    version='0.0.2',
+    license='MIT',
+    author="Gyeongmin Kim",
+    author_email='kgm1306@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Gyeongmin-lucid/pulsar-pip',
+    keywords='pulsar',
+    install_requires=[
+      'pandas',
+    ],
+)
