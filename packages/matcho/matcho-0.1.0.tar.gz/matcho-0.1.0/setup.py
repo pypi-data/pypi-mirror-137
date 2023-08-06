6d34db2f2bc711383837be1248eec3b6c3eb6dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:07 +0000

    Add package matcho/0.1.0/matcho-0.1.0.tar.gz

---
+++ packages/matcho/matcho-0.1.0.tar.gz/matcho-0.1.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='matcho',
+    version='0.1.0',
+    author='Martin Billinger-Finke',
+    packages=['matcho'],
+    description='A pattern matching and template library.',
+    url='https://github.com/mbillingr/matcho',
+    license='LICENSE',
+    python_requires='>=3.10'
+)
