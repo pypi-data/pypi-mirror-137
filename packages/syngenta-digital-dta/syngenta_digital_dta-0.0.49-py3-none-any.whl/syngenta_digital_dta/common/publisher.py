1e804aedfa40eccd25749735de0377437c6dc78f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:52 +0000

    Add package syngenta-digital-dta/0.0.49/syngenta_digital_dta-0.0.49-py3-none-any.whl

---
+++ packages/syngenta-digital-dta/syngenta_digital_dta-0.0.49-py3-none-any.whl/syngenta_digital_dta/common/publisher.py
@@ -0,0 +1,18 @@
+import boto3
+import simplejson as json
+
+from syngenta_digital_dta.common import logger
+
+
+def publish(**kwargs):
+    if not kwargs.get('arn') or not kwargs.get('data'):
+        return
+    try:
+        publisher = boto3.client('sns', region_name=kwargs.get('region'), endpoint_url=kwargs.get('endpoint'))
+        publisher.publish(
+            TopicArn=kwargs['arn'],
+            Message=json.dumps(kwargs['data']),
+            MessageAttributes=kwargs.get('attributes', {})
+        )
+    except Exception as e:
+        logger.log(level='WARN', log={'error': f'publish_sns_error: {e}'})
