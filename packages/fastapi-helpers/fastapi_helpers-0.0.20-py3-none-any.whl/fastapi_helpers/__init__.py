733bcdf96a74d1a71e92ff2dad9398712cde22f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:41 +0000

    Add package fastapi-helpers/0.0.20/fastapi_helpers-0.0.20-py3-none-any.whl

---
+++ packages/fastapi-helpers/fastapi_helpers-0.0.20-py3-none-any.whl/fastapi_helpers/__init__.py
@@ -0,0 +1,32 @@
+from fastapi_helpers.workers import Worker
+from fastapi_helpers.logging import DefaultLogger
+from fastapi_helpers.middlewares import HeadersMiddleware
+from fastapi_helpers.routes import (
+    DefaultModelRouter,
+    load_data_callback,
+    Pagination,
+    paginate_object
+)
+from fastapi_helpers.crud import BaseCrud, to_dict
+from fastapi_helpers.security import Encoder
+from fastapi_helpers.settings import DefaultSettings
+from fastapi_helpers.db import DbConfig
+from fastapi_helpers.db.seeders import DbSeeder
+
+__version__ = "0.0.20"
+
+__all__ = [
+    "Worker",
+    "DefaultLogger",
+    "DbConfig",
+    "DbSeeder",
+    "HeadersMiddleware",
+    "DefaultModelRouter",
+    "load_data_callback",
+    "paginate_object",
+    "Pagination",
+    "BaseCrud",
+    "to_dict",
+    "Encoder",
+    "DefaultSettings"
+]
---
+++ packages/fastapi-helpers/fastapi_helpers-0.0.20-py3-none-any.whl/fastapi_helpers/logging/DefaultLogger.py
@@ -0,0 +1,58 @@
+from .ColoredFormater import ColoredFormatter
+import watchtower
+import logging
+from ..settings.DefaultSettings import DefaultSettings
+
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;%dm"
+
+
+def formatter_message(message):
+    m = message.replace(
+        "$RESET", RESET_SEQ
+    )
+    return m
+
+
+class DefaultLogger(logging.Logger):
+
+    FORMAT = "%(levelname)s: %(message)s \t(%(pathname)s Line:%(lineno)d$RESET)"
+    COLOR_FORMAT = formatter_message(FORMAT)
+    NONE_FORMAT = "%(levelname)s: %(message)s \t(%(pathname)s:%(lineno)d)"
+
+    def __init__(
+        self,
+        name,
+        settings: DefaultSettings = None,
+        log_format=None,
+    ):
+        super().__init__(f"{name}")
+        self.addHandler(self.get_handler_logger(settings, log_format))
+
+    @classmethod
+    def get_handler_logger(
+            cls, 
+            settings: DefaultSettings = None, 
+            log_format=None,
+            use_queues = False,
+        ) -> logging.Handler:
+        handler = None
+        if(settings is not None and settings.is_production()):
+            if(log_format is None):
+                log_format = cls.NONE_FORMAT
+            color_formatter = ColoredFormatter(
+                log_format,
+                use_color=False
+            )
+            handler = watchtower.CloudWatchLogHandler(
+                log_group=settings.app_name,
+                use_queues=use_queues,
+            )
+            handler.setFormatter(color_formatter)
+        else:
+            if(log_format is None):
+                log_format = cls.COLOR_FORMAT
+            color_formatter = ColoredFormatter(log_format)
+            handler = logging.StreamHandler()
+            handler.setFormatter(color_formatter)
+        return handler
