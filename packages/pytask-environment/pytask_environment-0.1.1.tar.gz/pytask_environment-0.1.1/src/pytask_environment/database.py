6747a5ff6842b00aaaf8414af02a5098e16e4eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:24 +0000

    Add package pytask-environment/0.1.1/pytask_environment-0.1.1.tar.gz

---
+++ packages/pytask-environment/pytask_environment-0.1.1.tar.gz/pytask_environment-0.1.1/src/pytask_environment/database.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from _pytask.database import db
+from pony import orm
+
+
+class Environment(db.Entity):
+    """Collects all information of the environment which should be checked."""
+
+    name = orm.PrimaryKey(str)
+    version = orm.Required(str)
+    path = orm.Required(str)
