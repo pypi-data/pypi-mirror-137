41af72f9158bf322490bf04bfbc3b253788a15d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:30 +0000

    Add package pywapor-test/2.3.4/pywapor-test-2.3.4.tar.gz

---
+++ packages/pywapor-test/pywapor-test-2.3.4.tar.gz/pywapor-test-2.3.4/pywapor/general/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Tim Hessels
+Module: WAPOR/Functions
+"""
+
+from pywapor.general import outputs, variables, tests, processing_functions, compositer, pre_defaults
+
+__all__ = ['outputs', 'variables', 'tests', 'processing_functions', 'compositer', 'pre_defaults']
+
+__version__ = '0.1'
