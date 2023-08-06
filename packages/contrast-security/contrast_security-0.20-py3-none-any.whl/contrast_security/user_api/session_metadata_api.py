3d248290e91c2600ccda4447bff3403aeab6b3ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:27 +0000

    Add package contrast-security/0.20/contrast_security-0.20-py3-none-any.whl

---
+++ packages/contrast-security/contrast_security-0.20-py3-none-any.whl/contrast_security/user_api/session_metadata_api.py
@@ -0,0 +1,12 @@
+from .api_support import _ApiSupport
+
+
+class _SessionMetadataApi(_ApiSupport):
+
+    def __init__(self):
+        super(_SessionMetadataApi, self).__init__()
+
+    def filter_app_session_metadata(self, org_uuid, app_id):
+        path = '{org_uuid}/metadata/session/{app_id}/filters'.format(org_uuid=org_uuid, app_id=app_id)
+        no_filter = {}
+        return self._post(path,data=no_filter)
