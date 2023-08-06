beaaea3dde2c2647be4c25be60864167a8e4dc56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:21 +0000

    Add package types-aiobotocore-fsx/2.1.0/types-aiobotocore-fsx-2.1.0.tar.gz

---
+++ packages/types-aiobotocore-fsx/types-aiobotocore-fsx-2.1.0.tar.gz/types-aiobotocore-fsx-2.1.0/types_aiobotocore_fsx/paginator.py
@@ -0,0 +1,103 @@
+"""
+Type annotations for fsx service client paginators.
+
+[Open documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_fsx/paginators.html)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_fsx.client import FSxClient
+    from types_aiobotocore_fsx.paginator import (
+        DescribeBackupsPaginator,
+        DescribeFileSystemsPaginator,
+        ListTagsForResourcePaginator,
+    )
+
+    session = get_session()
+    with session.create_client("fsx") as client:
+        client: FSxClient
+
+        describe_backups_paginator: DescribeBackupsPaginator = client.get_paginator("describe_backups")
+        describe_file_systems_paginator: DescribeFileSystemsPaginator = client.get_paginator("describe_file_systems")
+        list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+    ```
+"""
+from typing import Generic, Iterator, Sequence, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    DescribeBackupsResponseTypeDef,
+    DescribeFileSystemsResponseTypeDef,
+    FilterTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = (
+    "DescribeBackupsPaginator",
+    "DescribeFileSystemsPaginator",
+    "ListTagsForResourcePaginator",
+)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class DescribeBackupsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/fsx.html#FSx.Paginator.DescribeBackups)
+    [Show types-aiobotocore documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_fsx/paginators.html#describebackupspaginator)
+    """
+
+    async def paginate(
+        self,
+        *,
+        BackupIds: Sequence[str] = ...,
+        Filters: Sequence["FilterTypeDef"] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[DescribeBackupsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/fsx.html#FSx.Paginator.DescribeBackups.paginate)
+        [Show types-aiobotocore documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_fsx/paginators.html#describebackupspaginator)
+        """
+
+
+class DescribeFileSystemsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/fsx.html#FSx.Paginator.DescribeFileSystems)
+    [Show types-aiobotocore documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_fsx/paginators.html#describefilesystemspaginator)
+    """
+
+    async def paginate(
+        self, *, FileSystemIds: Sequence[str] = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[DescribeFileSystemsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/fsx.html#FSx.Paginator.DescribeFileSystems.paginate)
+        [Show types-aiobotocore documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_fsx/paginators.html#describefilesystemspaginator)
+        """
+
+
+class ListTagsForResourcePaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/fsx.html#FSx.Paginator.ListTagsForResource)
+    [Show types-aiobotocore documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_fsx/paginators.html#listtagsforresourcepaginator)
+    """
+
+    async def paginate(
+        self, *, ResourceARN: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListTagsForResourceResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/fsx.html#FSx.Paginator.ListTagsForResource.paginate)
+        [Show types-aiobotocore documentation](https://vemel.github.io/types_aiobotocore_docs/types_aiobotocore_fsx/paginators.html#listtagsforresourcepaginator)
+        """
