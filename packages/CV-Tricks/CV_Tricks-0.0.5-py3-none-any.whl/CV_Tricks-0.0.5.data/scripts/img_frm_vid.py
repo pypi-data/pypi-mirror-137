ea1dbc7698b995e6648b09df91bdf64ef9c1d4e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:34 +0000

    Add package CV-Tricks/0.0.5/CV_Tricks-0.0.5-py3-none-any.whl

---
+++ packages/CV-Tricks/CV_Tricks-0.0.5-py3-none-any.whl/CV_Tricks-0.0.5.data/scripts/img_frm_vid.py
@@ -0,0 +1,37 @@
+#!python
+from sys import exit 
+from argparse import ArgumentParser
+from cv2 import VideoCapture, imwrite, CAP_PROP_FRAME_COUNT
+from os import mkdir
+from os.path import join, isdir
+from dataclasses import dataclass
+from tqdm import tqdm
+
+@dataclass
+class ImgFrmVid:
+    video_name: str
+    save_dir: str
+
+    def img_frm_video (self):
+        cap = VideoCapture(self.video_name)
+        if not isdir(self.save_dir):
+            mkdir(self.save_dir)
+        frame_count = int(cap.get(CAP_PROP_FRAME_COUNT))
+        if frame_count == 0:
+            exit("Video file not found")
+        for i in tqdm(range(frame_count), desc ="Extracting"):
+            ret, img = cap.read()
+            if ret:
+                imwrite(join(self.save_dir,'image%d.jpg' %i), img)
+        cap.release()
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-f","--file", required=True, type=str,
+    	help="Video File")
+    parser.add_argument("-d","--dir", default="images", type=str,
+        help="Directory for saving images")
+    args = parser.parse_args()
+    writer = ImgFrmVid(args.file,args.dir)
+    writer.img_frm_video()
+    
