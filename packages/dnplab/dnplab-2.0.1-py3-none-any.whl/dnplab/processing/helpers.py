eb07733db224152c907a257a5494bd072194a335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:51 +0000

    Add package dnplab/2.0.1/dnplab-2.0.1-py3-none-any.whl

---
+++ packages/dnplab/dnplab-2.0.1-py3-none-any.whl/dnplab/processing/helpers.py
@@ -0,0 +1,92 @@
+import numpy as np
+from scipy.signal import savgol_filter
+
+from ..core.data import DNPData
+from ..processing.integration import integrate
+
+
+def calculate_enhancement():
+    """Calculate enhancement
+
+    Returns:
+        NotImplemented
+
+    """
+
+    return NotImplemented
+
+
+def signal_to_noise():
+    """Find signal-to-noise ratio
+
+    Returns:
+        NotImplemented
+    """
+
+    return NotImplemented
+
+
+def smooth(data, dim="t2", window_length=11, polyorder=3):
+    out = data.copy()
+
+    out.unfold(dim)
+
+    out.values = savgol_filter(out.values, window_length, polyorder, axis=0)
+
+    out.fold()
+
+    return out
+
+
+def left_shift(data, dim="t2", shift_points=0):
+    """Remove points from the left of data
+
+    Args:
+        all_data (dnpdata, dict): Data container for data
+
+    +---------------+------+---------+--------------------------------------------------+
+    | parameter     | type | default | description                                      |
+    +===============+======+=========+==================================================+
+    | dim           | str  | "t2"    | dimension to shift                               |
+    +---------------+------+---------+--------------------------------------------------+
+    | shift_points  | int  | 0       | Number of points to remove from left of data     |
+    +---------------+------+---------+--------------------------------------------------+
+
+    Returns:
+        dnpdata: data object with left-shifted data
+    """
+
+    data = data[dim, shift_points:]
+
+    proc_attr_name = "left_shift"
+    proc_parameters = {
+        "dim": dim,
+        "points": shift_points,
+    }
+    data.add_proc_attrs(proc_attr_name, proc_parameters)
+
+    return data
+
+
+def normalize():
+    return NotImplemented
+
+
+def reference(data, dim="f2", old_ref=0, new_ref=0):
+    """Function for referencing NMR spectra
+
+    Args:
+        data (DNPData): Data for referencing
+        dim (str): dimension to perform referencing down. By default this dimension is "f2".
+        old_ref (float): Value of old reference
+        new_ref (float): New reference value
+
+    Returns:
+        DNPData: referenced data
+    """
+
+    data = data.copy()
+
+    data.coords[dim] -= old_ref - new_ref
+
+    return data
