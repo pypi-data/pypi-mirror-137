b44a4f71505970324db84d23746c9762288d80d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:08 +0000

    Add package huskey/0.0.0/huskey-0.0.0.tar.gz

---
+++ packages/huskey/huskey-0.0.0.tar.gz/huskey-0.0.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+import sys
+
+setup(
+      name='huskey',
+      version='0.0.0',
+      author='Walker Lab',
+      author_email='eywalker@uw.edu',
+      url='https://github.com/walkerlab/huskey',
+      packages=find_packages(),
+      )
