c57577695eb8672ca7e65a6affd848e130491f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:59 +0000

    Add package configParsePy/0.1/configParsePy-0.1-py3-none-any.whl

---
+++ packages/configParsePy/configParsePy-0.1-py3-none-any.whl/configPy/__init__.py
@@ -0,0 +1,8 @@
+# JSONConfigParser - JSON config Parser Object
+from configPy.jsonConfigParser import JSONConfigParser
+
+# TXTConfigParser - TXT config Parser Object
+from configPy.txtConfigParser import TXTConfigParser
+
+# NDJSONConfigParser - Newline Delimited JSON config Parser Object
+from configPy.ndjsonConfigParser import NDJSONConfigParser
