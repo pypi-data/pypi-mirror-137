e263066dd6e1fb31bf4976d9e885d5dffee71f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:31 +0000

    Add package icsdll/2022.2.6/icsdll-2022.2.6.tar.gz

---
+++ packages/icsdll/icsdll-2022.2.6.tar.gz/icsdll-2022.2.6/ics/ics_version.h
@@ -0,0 +1,10 @@
+/* ics_version.h */
+
+#define ICS_VERSION_STR "2022.2.6"
+#define ICS_FILEVERSION 2022, 2, 6, 0
+#define ICS_NAME "ICSx64.dll"
+#define ICS_DESCRIPTION "Image Correlation Spectroscopy Library"
+#define ICS_AUTHOR "Christoph Gohlke"
+#define ICS_COPYRIGHT "Copyright (C) 2016-2022"
+#define ICS_COMPANY \
+    "Laboratory for Fluorescence Dynamics, University of California, Irvine"
