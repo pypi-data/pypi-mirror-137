3d00deb3fda92ac1e15bbae1d9513696945d2246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:13 +0000

    Add package pytask-latex/0.1.1/pytask_latex-0.1.1.tar.gz

---
+++ packages/pytask-latex/pytask_latex-0.1.1.tar.gz/pytask_latex-0.1.1/src/pytask_latex/parametrize.py
@@ -0,0 +1,13 @@
+"""Parametrize tasks."""
+from __future__ import annotations
+
+from _pytask.config import hookimpl
+from _pytask.mark import MARK_GEN as mark  # noqa: N811
+
+
+@hookimpl
+def pytask_parametrize_kwarg_to_marker(obj, kwargs):
+    """Register kwargs as latex marker."""
+    if callable(obj):
+        if "latex" in kwargs:
+            mark.latex(kwargs.pop("latex"))(obj)
