aa9f8b89120ca093df16f82d5383bb25b744bb63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:58 +0000

    Add package convert-case-py/0.0.2/convert_case_py-0.0.2.tar.gz

---
+++ packages/convert-case-py/convert_case_py-0.0.2.tar.gz/convert_case_py-0.0.2/convert_case_py/Case.py
@@ -0,0 +1,116 @@
+import re
+
+
+def camelcase_to_lowercase(data):
+    """
+
+
+    :param data:
+    :return:
+    """
+
+    """Convert all keys to lowercase"""
+    result = {}
+    for key, value in data.items():
+        try:
+            result[key.lower()] = camelcase_to_lowercase(value) if type(value) is dict else value
+        except AttributeError:
+            result[key] = value
+    return result
+
+
+def snakecase_to_lowercase(data):
+    """
+
+
+    :param data:
+    :return:
+    """
+
+    """Convert all keys to lowercase"""
+    result = {}
+    for key, value in data.items():
+        try:
+            result[key.replace('_', '').lower()] = snakecase_to_lowercase(value) if type(value) is dict else value
+        except AttributeError:
+            result[key] = value
+    return result
+
+
+def snakecase_to_camelcase(data, count_underscore=False, pascal_mode=False):
+    """
+
+
+    :param data:
+    :param count_underscore:
+    :param pascal_mode:
+    :return:
+    """
+
+    result = {}
+    for key, value in data.items():
+        try:
+            if pascal_mode:
+                new_key = ''.join(x.capitalize() or '_' for x in key.split('_')) if count_underscore else ''.join(
+                    x.capitalize() for x in key.split('_'))
+                result[new_key] = snakecase_to_camelcase(value, count_underscore, True) if type(
+                    value) is dict else value
+            else:
+                new_key = ''.join(x.capitalize() or '_' for x in key.split('_')) if count_underscore else ''.join(
+                    x.capitalize() for x in key.split('_'))
+
+                new_key_list = list(new_key)
+                new_key_list[0] = new_key_list[0].lower()
+
+                new_key = ''.join(new_key_list)
+
+                result[new_key] = snakecase_to_camelcase(value, count_underscore) if type(value) is dict else value
+        except AttributeError:
+            result[key] = value
+    return result
+
+
+def camelcase_to_snakecase(data, pascal_mode=False):
+    """
+
+
+    :param data:
+    :param pascal_mode:
+    :return:
+    """
+
+    result = {}
+    for key, value in data.items():
+        try:
+            key = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', key)
+            key = re.sub('([a-z0-9])([A-Z])', r'\1_\2', key).lower()
+            if pascal_mode:
+                result[key] = camelcase_to_snakecase(value, True) if type(value) is dict else value
+            else:
+                result[key] = camelcase_to_snakecase(value) if type(value) is dict else value
+        except AttributeError:
+            result[key] = value
+    return result
+
+
+def snakecase_to_pascalcase(data, count_underscore=False):
+    """
+
+
+    :param data:
+    :param count_underscore:
+    :return:
+    """
+
+    return snakecase_to_camelcase(data, count_underscore, True)
+
+
+def pascalcase_to_snakecase(data):
+    """
+
+
+    :param data:
+    :return:
+    """
+
+    return camelcase_to_snakecase(data, True)
---
+++ packages/convert-case-py/convert_case_py-0.0.2.tar.gz/convert_case_py-0.0.2/convert_case_py/__init__.py
@@ -0,0 +1,6 @@
+from .Case import camelcase_to_lowercase
+from .Case import pascalcase_to_snakecase
+from .Case import camelcase_to_snakecase
+from .Case import snakecase_to_camelcase
+from .Case import snakecase_to_lowercase
+from .Case import snakecase_to_pascalcase
