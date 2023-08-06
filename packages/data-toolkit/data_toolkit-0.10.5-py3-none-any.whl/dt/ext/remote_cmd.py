e0fa4f6bf5850fd90e98aba354b79db8961e7434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:40 +0000

    Add package data-toolkit/0.10.5/data_toolkit-0.10.5-py3-none-any.whl

---
+++ packages/data-toolkit/data_toolkit-0.10.5-py3-none-any.whl/dt/ext/remote_cmd.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# TODO: early remote code execution.
+
+import os
+import sys
+import sh
+import fabric
+import uuid
+
+
+def main():
+    if os.getcwd() == os.path.expanduser('~'):
+        print('NO')
+        exit(1)
+    addr = sys.argv[1]
+    del sys.argv[1]
+    path = '/tmp/{}'.format(uuid.uuid4())
+    env = []
+    while '=' in sys.argv[1]:
+        env.append(sys.argv[1].split('='))
+        del sys.argv[1]
+    env = dict(env)
+
+    print(sh.rsync('-a', '-v', '-z', '--exclude', '__pycache__', '--exclude', '*.swp', '--exclude', '.git', '{}/'.format(os.getcwd()), '{}:{}'.format(addr, path)))
+
+    cmd = 'cd {} && {}'.format(path, ' '.join(sys.argv[1:]))
+
+    connection = fabric.Connection(addr, inline_ssh_env=True)
+    connection.client.load_system_host_keys()
+    connection.run(cmd, env=env)
+
+    print(sh.rsync('-a', '-v', '-z', '{}:{}/logs/'.format(addr, path), os.getcwd() + '/logs'))
+
+
+if __name__ == '__main__':
+    main()
