647b83ef7506a19313dbce1ee1e7ce4c73da705a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:10 +0000

    Add package tmtoolkit/0.11.0rc1/tmtoolkit-0.11.0rc1-py3-none-any.whl

---
+++ packages/tmtoolkit/tmtoolkit-0.11.0rc1-py3-none-any.whl/tmtoolkit/__init__.py
@@ -0,0 +1,23 @@
+"""
+tmtoolkit – Text Mining and Topic Modeling Toolkit for Python
+
+Markus Konrad <markus.konrad@wzb.eu>
+"""
+
+from importlib.util import find_spec
+import logging
+
+__title__ = 'tmtoolkit'
+__version__ = '0.11.0.rc1'
+__author__ = 'Markus Konrad'
+__license__ = 'Apache License 2.0'
+
+logger = logging.getLogger(__title__)
+logger.addHandler(logging.NullHandler())
+logger.setLevel(logging.WARNING)   # set default level
+
+
+from . import bow, topicmod, tokenseq, types, utils
+
+if find_spec('spacy') and find_spec('globre'):
+    from . import corpus
