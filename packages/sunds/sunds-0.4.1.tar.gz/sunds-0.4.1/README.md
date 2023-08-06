8285104f5972f2f3dfd65088e8c418860513343a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:45 +0000

    Add package sunds/0.4.1/sunds-0.4.1.tar.gz

---
+++ packages/sunds/sunds-0.4.1.tar.gz/sunds-0.4.1/README.md
@@ -0,0 +1,26 @@
+# Scene understanding datasets
+
+[![Unittests](https://github.com/google-research/sunds/actions/workflows/pytest.yml/badge.svg)](https://github.com/google-research/sunds/actions/workflows/pytest.yml)
+[![PyPI version](https://badge.fury.io/py/sunds.svg)](https://badge.fury.io/py/sunds)
+
+SunDs is a collection of ready-to-use datasets for scene understanding tasks (3d
+object detection, semantic segmentation, nerf rendering,...). It provides:
+
+*   An API to easily load datasets to feed into your ML models.
+*   A collection of ready-to-use datasets.
+*   Helper tools to create new datasets.
+
+```python
+import sunds
+
+ds = sunds.load('nerf_synthetic/lego', split='train', task=sunds.task.Nerf())
+for ex in ds:
+  ex['ray_origin']
+```
+
+To use sunds, see the documentation:
+
+*  [Intro guide](https://github.com/google-research/sunds/blob/master/docs/intro.md)
+*  [Nerf guide](https://github.com/google-research/sunds/blob/master/docs/nerf.md)
+
+*This is not an official Google product.*
---
+++ packages/sunds/sunds-0.4.1.tar.gz/sunds-0.4.1/sunds/core/dataset_semantic_types.py
@@ -0,0 +1,38 @@
+# Copyright 2022 The sunds Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Helper custom types."""
+
+import enum
+
+
+class MetricGroupings(enum.Enum):
+  """Semantic metric groupings."""
+  MOST_IMPORTANT = "most_important"
+  IMPORTANT = "important"
+  MOVERS = "movers"
+  GROUND = "ground"
+  SMALL = "small"
+  RARE = "rare"
+  ATOMIC_MAPS = "atomic_maps"
+  IMAGE_ONLY = "image_only"
+  LIDAR_ONLY = "lidar_only"
+
+  @classmethod
+  def has_value(cls, value):
+    return value in cls._value2member_map_.keys()
+
+  @classmethod
+  def lidar_only(cls, value):
+    return value == cls.LIDAR_ONLY
