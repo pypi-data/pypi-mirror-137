6c2369a309fc21d9d628e2529040d0afb5111c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:10 +0000

    Add package django-generic-search/2.1.0/django_generic_search-2.1.0-py3-none-any.whl

---
+++ packages/django-generic-search/django_generic_search-2.1.0-py3-none-any.whl/tests/settings.py
@@ -0,0 +1,64 @@
+import os
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'test-secret-key'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    'localhost',
+]
+SPYDER_CONFIG = {
+    'gammaspider':{
+        'allowed_domains':[
+            'gammaadvocates.co.ke'
+        ],
+        'start_urls':[
+            'https://gammaadvocates.co.ke/'
+        ]
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
+    'generic_search',
+]
+
+ROOT_URLCONF = 'generic_search.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+# Extra places for collectstatic to find static files.
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'generic_search/static'),
+]
+
