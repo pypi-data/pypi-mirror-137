5385a29f278a45f47d1c6a081fa4d27a1f179666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:07 +0000

    Add package nonebot-plugin-admin/0.3.10/nonebot-plugin-admin-0.3.10.tar.gz

---
+++ packages/nonebot-plugin-admin/nonebot-plugin-admin-0.3.10.tar.gz/nonebot-plugin-admin-0.3.10/setup.py
@@ -0,0 +1,28 @@
+import io
+import sys
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nonebot-plugin-admin",
+    version="0.3.10",
+    author="yzyyz1387",
+    author_email="youzyyz1384@qq.com",
+    keywords=("pip", "nonebot2", "nonebot", "admin", "nonebot_plugin"),
+    description="""nonebot2 plugin for group administration""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yzyyz1387/nonebot_plugin_admin",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    platforms="any",
+    install_requires=["aiofiles", "fuzzyfinder", 'nonebot-adapter-onebot>=2.0.0-beta.1,<3.0.0','nonebot2>=2.0.0-beta.1,<3.0.0', "jieba", "httpx"]
+)
