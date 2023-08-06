e00c268b3c4c12903d4d3ffb931241b5d420b3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:22 +0000

    Add package databaseci/4.0.2/databaseci-4.0.2.tar.gz

---
+++ packages/databaseci/databaseci-4.0.2.tar.gz/databaseci-4.0.2/databaseci/call.py
@@ -0,0 +1,11 @@
+import requests
+
+BASE_URL = "https://api.databaseci.com/v1/copy"
+
+
+def get_response(request):
+    r = requests.post(BASE_URL, json=request)
+
+    r.raise_for_status()
+
+    return r.json()
