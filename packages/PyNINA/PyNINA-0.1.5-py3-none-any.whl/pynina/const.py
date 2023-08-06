7609c97210d9a366a7985fad00ef37a809545a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:59 +0000

    Add package PyNINA/0.1.5/PyNINA-0.1.5-py3-none-any.whl

---
+++ packages/PyNINA/PyNINA-0.1.5-py3-none-any.whl/pynina/const.py
@@ -0,0 +1,12 @@
+ENDPOINT_REGIONAL_CODE: str = "https://www.xrepository.de/api/xrepository/urn:de:bund:destatis:bevoelkerungsstatistik:schluessel:rs_2021-07-31/download/Regionalschl_ssel_2021-07-31.json"
+ENDPOINT_NINA_BASE: str = "https://warnung.bund.de/api31/dashboard/"
+
+CITY_STATES_CODE = [
+    "02",
+    "11"
+]
+
+
+class ReadOnlyClass(type):
+    def __setattr__(self, name, value):
+        raise ValueError(name)
