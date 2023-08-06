54b783192134b2df0568764ed24094b47b00b819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:18 +0000

    Add package vimala/0.1.1/vimala-0.1.1-py3-none-any.whl

---
+++ packages/vimala/vimala-0.1.1-py3-none-any.whl/vimala/_vim.py
@@ -0,0 +1,32 @@
+"""The vimala package's catch-all module.
+
+You should only add code to this module when you are unable to find ANY other
+module to add it to.
+"""
+
+from __future__ import annotations
+
+from typing import Iterable
+
+from eris import ErisResult
+import proctor
+from typist import PathLike
+
+
+def vim(
+    *args: PathLike,
+    lineno: int = None,
+    commands: Iterable[str] = (),
+    vim_exe: PathLike = "vim",
+) -> ErisResult[proctor.Process]:
+    """Execute `vim`."""
+    extra_args: list[str] = []
+    if lineno is not None:
+        extra_args.append(f"+{lineno}")
+
+    for cmd in commands:
+        extra_args.extend(["-c", cmd])
+
+    cmd_args = [str(x) for x in [vim_exe, *args, *extra_args]]
+    proc = proctor.safe_popen(cmd_args)
+    return proc
