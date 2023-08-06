53796f024f38ddee2b0b055b6f56654ef2f48b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:09 +0000

    Add package RAiSEHD/0.0.2/RAiSEHD-0.0.2.tar.gz

---
+++ packages/RAiSEHD/RAiSEHD-0.0.2.tar.gz/RAiSEHD-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, Extension
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='RAiSEHD',
+    version='0.0.2',
+    author = "Ross Turner",
+    author_email = "turner.rj@icloud.com",
+    description = ("RAiSE HD: Lagrangian particle-based radio AGN model."),
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages=['RAiSEHD'],
+    install_requires=[
+        'numpy', 'pandas', 'matplotlib', 'h5py', 'astropy', 'scipy', 'numba'
+    ],
+)
