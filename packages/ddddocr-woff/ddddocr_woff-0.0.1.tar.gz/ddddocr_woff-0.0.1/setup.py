df5b599e5af584b64d83653bd390b8eb7e0fa9a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:09 +0000

    Add package ddddocr-woff/0.0.1/ddddocr_woff-0.0.1.tar.gz

---
+++ packages/ddddocr-woff/ddddocr_woff-0.0.1.tar.gz/ddddocr_woff-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="ddddocr_woff",
+    version="0.0.1",
+    author="Fan&MuYiSen",
+    descripyion="基于ddddocr的字体文件一键识别，适用于小白",
+    long_description="基于ddddocr的字体文件一键识别，适用于小白",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(where='.', exclude=(), include=('*',)),
+    classifiers=["Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",],
+    python_requires='<3.10',
+
+)
