d48e892d64e7feacf898358a30041b3cce61a172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:32 +0000

    Add package Imagegen/0.1/Imagegen-0.1.tar.gz

---
+++ packages/Imagegen/Imagegen-0.1.tar.gz/Imagegen-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='Imagegen',
+    version='0.1',
+    description='HomeWork project for graph generation',
+    install_requires=['networkx', 'matplotlib'],
+    url='https://pypi.org/pypi'
+)
