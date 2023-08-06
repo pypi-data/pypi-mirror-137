44f9a9097894697511ccfdf38ee8d427a7090495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:33:36 +0000

    Add package warehub/1.1.2/warehub-1.1.2-py3-none-any.whl

---
+++ packages/warehub/warehub-1.1.2-py3-none-any.whl/warehub/__init__.py
@@ -0,0 +1,24 @@
+import importlib.metadata
+
+__all__ = (
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+)
+
+metadata = importlib.metadata.metadata("warehub")
+
+__title__ = metadata["name"]
+__summary__ = metadata["summary"]
+__uri__ = metadata["home_page"]
+__version__ = metadata["version"]
+__author__ = metadata["author"]
+__email__ = metadata["author_email"]
+__license__ = metadata["license"]
+
+__copyright__ = "Copyright 2022 Ryan Smith"
