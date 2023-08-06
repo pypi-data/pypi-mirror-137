e6f4c75ee752c886b5214e03a78d56184e27fd63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:36 +0000

    Add package django-wspay/0.3.2/django-wspay-0.3.2.tar.gz

---
+++ packages/django-wspay/django-wspay-0.3.2.tar.gz/django-wspay-0.3.2/setup.py
@@ -0,0 +1,55 @@
+from setuptools import find_packages, setup
+
+
+NAME = "django-wspay"
+DESCRIPTION = "a payments Django app for WSPay"
+AUTHOR = "Vedran Vojvoda"
+AUTHOR_EMAIL = "vedran@pinkdroids.com"
+URL = "https://github.com/pinkdroids/django-wspay"
+LONG_DESCRIPTION = """
+============
+Django WSPay
+============
+
+This django app provides simple support for payments using WSPay gateway.
+"""
+
+tests_require = [
+    "mock",
+    "pytest",
+    "pytest-django",
+    "responses"
+]
+
+setup(
+    name=NAME,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/x-rst',
+    version="0.3.2",
+    license="MIT",
+    url=URL,
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta ",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+    ],
+    include_package_data=True,
+    install_requires=[
+        "django>=3.0",
+        "pytz",
+        "requests"
+    ],
+    extras_require={
+        "testing": tests_require,
+    },
+    zip_safe=False,
+)
