bf91da12ffc7e0cefa00032090f8b5cfe2f6a911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:22 +0000

    Add package etpy2png/0.0.6/etpy2png-0.0.6.tar.gz

---
+++ packages/etpy2png/etpy2png-0.0.6.tar.gz/etpy2png-0.0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.6'
+DESCRIPTION = 'Converting .py Code-Files to .png with Code-Highlighting.'
+
+
+
+setup(
+    name="etpy2png",
+    version=VERSION,
+    author="ExperTeach GmbH",
+    author_email="et_dbu@web.de",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'selenium',
+        'nbconvert'
+        ],
+    keywords=['python', 'code', 'screenshot', 'png', 'py', 'code-highlighting'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
