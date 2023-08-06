5b0dae18da69eb6fd5e7ba4df50dede962cbb235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:36 +0000

    Add package citric/0.0.8/citric-0.0.8-py3-none-any.whl

---
+++ packages/citric/citric-0.0.8-py3-none-any.whl/citric/exceptions.py
@@ -0,0 +1,25 @@
+"""Common exceptions."""
+
+
+class ResponseMismatchError(Exception):
+    """Exception raised when request and response ID don't match."""
+
+
+class LimeSurveyError(Exception):
+    """Basic exception raised by LimeSurvey."""
+
+    def __init__(self, message: str) -> None:  # noqa:: ANN101
+        """Create a generic error for the LimeSurvey RPC API.
+
+        Args:
+            message: Exception message. By default none, and a generic message is used.
+        """
+        super().__init__(message)
+
+
+class LimeSurveyStatusError(LimeSurveyError):
+    """Exception raised when LimeSurvey responds with an error status."""
+
+
+class LimeSurveyApiError(LimeSurveyError):
+    """Exception raised when LimeSurvey responds with a non-null error."""
