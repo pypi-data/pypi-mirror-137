8b51e75c73bfb9ed956702430a7d909b07c8e005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:45 +0000

    Add package etpyfitest1/0.0.1/etpyfitest1-0.0.1.tar.gz

---
+++ packages/etpyfitest1/etpyfitest1-0.0.1.tar.gz/etpyfitest1-0.0.1/README.md
@@ -0,0 +1,11 @@
+# etpyfitest1
+
+Simple package to demonstrate the usage of PyPI and advertise ExperTeach.
+
+## Usage
+```python
+from etpyfitest1 import werbung
+
+# Output some advertisement for ExperTeach
+werbung()
+```
---
+++ packages/etpyfitest1/etpyfitest1-0.0.1.tar.gz/etpyfitest1-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Demonstrating how to use PyPI'
+
+
+
+setup(
+    name="etpyfitest1",
+    version=VERSION,
+    author="ExperTeach GmbH",
+    author_email="daniel.busse@experteach.de",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[
+        'selenium',
+        'jupyter'
+        ],
+    keywords=['python', 'code', 'screenshot', 'png', 'py', 'code-highlighting'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
