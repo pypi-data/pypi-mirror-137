93767eba3e5110a244c5e4300b85c662af1712a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:16 +0000

    Add package wood-nester/1.1.2/wood_nester-1.1.2.tar.gz

---
+++ packages/wood-nester/wood_nester-1.1.2.tar.gz/wood_nester-1.1.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'wood_nester',
+    version = '1.1.2',
+    py_modules= ['wood_nester'],
+    author = 'wood',
+    author_email = 'lezvinskyi@gmail.com',
+    url = 'https://www.booksaround.net',
+    description= 'Simple printer of nested lists'
+)
