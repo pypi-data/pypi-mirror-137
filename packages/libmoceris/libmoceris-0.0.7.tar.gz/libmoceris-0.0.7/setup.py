6e0c71799fc577b0a5528ff8cd751389ae214319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:32 +0000

    Add package libmoceris/0.0.7/libmoceris-0.0.7.tar.gz

---
+++ packages/libmoceris/libmoceris-0.0.7.tar.gz/libmoceris-0.0.7/setup.py
@@ -0,0 +1,26 @@
+"""
+@author: bakabaka9405
+@file:setup.py
+@time:2022/1/27 22:23
+@file_desc:
+"""
+
+import setuptools
+
+setuptools.setup(
+    name='libmoceris',
+    version='0.0.7',
+    author='MOCERIS STUDIO',
+    author_email='MocerisStudio@outlook.com',
+    description='MOCERIS Gaming Engine',
+    long_description='This is MOCERIS Gaming Engine',
+    url='https://gitee.com/moceris-studio/libmoceris',
+    packages=setuptools.find_packages(),
+    platforms='any',
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
