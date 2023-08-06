c3cb548f205f6dc8ac0a5931eeccd32284f8726c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:31 +0000

    Add package verity-sdk/0.6.1/verity-sdk-0.6.1.tar.gz

---
+++ packages/verity-sdk/verity-sdk-0.6.1.tar.gz/verity-sdk-0.6.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+from verity_sdk.version import VERSION
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="verity-sdk",
+    version=VERSION,  # see verity_sdk/version.py
+    author="Evernym, Inc.",
+    author_email="dev@evernym.com",
+    description='The official Python SDK for Evernym\'s Verity',
+    license="Apache-2.0",
+    url="https://github.com/evernym/verity-sdk",
+    install_requires=[
+        'vdr-tools~=0.8.4',
+        'requests~=2.25.1',
+        'base58~=2.1.0'
+    ],
+    python_requires='~=3.6',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+)
+
---
+++ packages/verity-sdk/verity-sdk-0.6.1.tar.gz/verity-sdk-0.6.1/verity_sdk/transports.py
@@ -0,0 +1,38 @@
+from urllib.parse import urljoin
+import requests
+
+
+def msg_endpoint(url: str) -> str:
+    """
+    Joins a given url to the message route on the verity-application
+
+    Args:
+        url: the given url to be jointed
+    Returns:
+        str: the full url to the message route
+    """
+    return urljoin(url, '/agency/msg')
+
+
+def send_packed_message(context, packed_message: bytes):
+    """
+    Sends a packaged message to the verity-application.
+
+    Args:
+        context (Context): an instance of the Context object initialized to a verity-application agent
+        packed_message (bytes): the message to send to Verity
+    Returns:
+        bytes: bytes returned from the verity-application
+    """
+    url = msg_endpoint(context.verity_url)
+    try:
+        r = requests.post(
+            url,
+            data=packed_message,
+            headers={'Content-Type': 'application/octet-stream'}
+        )
+        r.raise_for_status()
+        return r.content
+    except requests.RequestException as e:
+        print('ERROR: Failed to POST message to {}'.format(url))
+        raise e
