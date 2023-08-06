efb7ea7c9572eed376ac0ce9bbfb95133927eeb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:36 +0000

    Add package tree-sitter-python-language/0.0.0/tree-sitter-python-language-0.0.0.tar.gz

---
+++ packages/tree-sitter-python-language/tree-sitter-python-language-0.0.0.tar.gz/tree-sitter-python-language-0.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+from tree_sitter_python_language import BUILD_OUTPUT
+
+if not BUILD_OUTPUT.exists():
+    import subprocess
+    subprocess.check_output(["python3", "tree_sitter_python_language"])
+    assert BUILD_OUTPUT.exists(), f'build failed, {BUILD_OUTPUT} does not exist'
+
+setup(
+    name='tree-sitter-python-language',
+    packages=find_packages(),
+    package_data={'': [str(BUILD_OUTPUT)]},
+    include_package_data=True,
+    install_requires=['tree_sitter'],
+    setup_requires=['tree_sitter'],
+)
