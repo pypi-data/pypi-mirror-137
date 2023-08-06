c005ffcc559d4ec49f1431cdfae76d24b5270937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:50 +0000

    Add package GEDSpy/1.0.5/GEDSpy-1.0.5-py3-none-any.whl

---
+++ packages/GEDSpy/GEDSpy-1.0.5-py3-none-any.whl/GEDSpy/__init__.py
@@ -0,0 +1,9 @@
+print()
+print('Welcome in GEDSpy v.1.0.5 library')
+print('')
+print('Loading required  packages...')
+
+from GEDSpy.zinc import zinc
+from GEDSpy.GOPa import GOPa
+
+print('GEDSpy is ready to use')
