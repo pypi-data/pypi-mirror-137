4d10d385b89728cdef0dcd489a215352c1deb5f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:26 +0000

    Add package citizenofnerdvana-test-project/1.0/citizenofnerdvana_test_project-1.0.tar.gz

---
+++ packages/citizenofnerdvana-test-project/citizenofnerdvana_test_project-1.0.tar.gz/citizenofnerdvana_test_project-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='citizenofnerdvana_test_project',
+      version='1.0',
+      description='test project',
+      author='okrasnos',
+      author_email='citizenofnerdvana@gmail.com',
+      url='https://www.google.com/',
+      packages=['module1', 'module2'],
+     )
