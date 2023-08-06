5d1cc3d753722129b859e30e19992e8ba4f4aa60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:56 +0000

    Add package ts4py/0.0.1/ts4py-0.0.1.tar.gz

---
+++ packages/ts4py/ts4py-0.0.1.tar.gz/ts4py-0.0.1/ts4py/src/__init__.py
@@ -0,0 +1,7 @@
+# @Time     : 2021/11/5
+# @Project  : wanba_py
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+from ._timeunt import SECOND, MILLISECOND, MICROSECOND
+from ._timestamp import convert, second, millisecond, microsecond, ConvertFn
---
+++ packages/ts4py/ts4py-0.0.1.tar.gz/ts4py-0.0.1/ts4py/src/_timeunt.py
@@ -0,0 +1,13 @@
+# @Time     : 2022/2/8
+# @Project  : wanba_py
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+# seconds
+SECOND = 1
+
+# milliseconds
+MILLISECOND = 10 ** 3
+
+# microseconds
+MICROSECOND = 10 ** 6
