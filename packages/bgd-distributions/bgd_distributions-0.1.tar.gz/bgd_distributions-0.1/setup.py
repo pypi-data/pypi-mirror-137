e49a132528a897482e6062901fb13ee49133094b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:12 +0000

    Add package bgd-distributions/0.1/bgd_distributions-0.1.tar.gz

---
+++ packages/bgd-distributions/bgd_distributions-0.1.tar.gz/bgd_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bgd_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['bgd_distributions'],
+      author = 'Tamiris Martins',
+      author_email = 'tamiriscrepalde@gmail.com',
+      zip_safe=False)
