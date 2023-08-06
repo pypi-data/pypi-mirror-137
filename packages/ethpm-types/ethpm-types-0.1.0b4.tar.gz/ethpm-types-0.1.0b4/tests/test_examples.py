dca23e820e9c321f10698c5fffa11331df76ea4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:57 +0000

    Add package ethpm-types/0.1.0b4/ethpm-types-0.1.0b4.tar.gz

---
+++ packages/ethpm-types/ethpm-types-0.1.0b4.tar.gz/ethpm-types-0.1.0b4/tests/test_examples.py
@@ -0,0 +1,30 @@
+import os
+
+import github
+import pytest  # type: ignore
+import requests  # type: ignore
+from pydantic import ValidationError
+
+from ethpm_types import PackageManifest
+
+ETHPM_SPEC_REPO = github.Github(os.environ.get("GITHUB_ACCESS_TOKEN", None)).get_repo(
+    "ethpm/ethpm-spec"
+)
+
+EXAMPLES_RAW_URL = "https://raw.githubusercontent.com/ethpm/ethpm-spec/master/examples"
+
+
+@pytest.mark.parametrize(
+    "example_name",
+    [f.name for f in ETHPM_SPEC_REPO.get_contents("examples")],  # type: ignore
+)
+def test_examples(example_name):
+    example = requests.get(f"{EXAMPLES_RAW_URL}/{example_name}/v3.json").json()
+
+    if "invalid" not in example_name:
+        package = PackageManifest.parse_obj(example)
+        assert package.dict() == example
+
+    else:
+        with pytest.raises((ValidationError, ValueError)):
+            PackageManifest.parse_obj(example).dict()
