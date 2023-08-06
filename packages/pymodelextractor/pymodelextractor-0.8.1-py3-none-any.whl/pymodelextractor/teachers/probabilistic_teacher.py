0124999b7390a35e099a2fdfea41d99924d8f545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:09 +0000

    Add package pymodelextractor/0.8.1/pymodelextractor-0.8.1-py3-none-any.whl

---
+++ packages/pymodelextractor/pymodelextractor-0.8.1-py3-none-any.whl/pymodelextractor/teachers/probabilistic_teacher.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+
+from pythautomata.base_types.alphabet import Alphabet
+from pythautomata.base_types.sequence import Sequence
+from pythautomata.automata.wheighted_automaton_definition.weighted_automaton import WeightedAutomaton
+
+from typing import Union
+
+class ProbabilisticTeacher(ABC):
+
+    def __init__(self):        
+        self._equivalence_queries_count = 0
+        self._last_token_weight_queries_count = 0
+
+    @abstractmethod
+    def sequence_weight(self, sequence: Sequence) -> float:
+        raise NotImplementedError
+
+    @abstractmethod
+    def log_sequence_weight(self, sequence: Sequence) -> float:
+        raise NotImplementedError
+
+    @abstractmethod
+    def last_token_weights(self, sequence: Sequence, required_suffixes: list[Sequence]) -> list[float]:        
+        raise NotImplementedError
+
+    def equivalence_query(self, aut: WeightedAutomaton) -> tuple[bool, Union[Sequence,None]]:
+        raise NotImplementedError
+
+    def reset(self) -> None:
+        self._equivalence_queries_count = 0
+        self._last_token_weight_queries_count = 0
+
+    def log_probability_error(self, seq, aut: WeightedAutomaton) -> float:
+        return abs(aut.log_sequence_weight(seq) - self.log_sequence_weight(seq))
+
+    @property
+    @abstractmethod
+    def alphabet(self) -> Alphabet:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def terminal_symbol(self) -> Sequence:
+        raise NotImplementedError
+
+    @property
+    def equivalence_queries_count(self):
+        return self._equivalence_queries_count
+
+    @property
+    def last_token_weight_queries_count(self):
+        return self._last_token_weight_queries_count
