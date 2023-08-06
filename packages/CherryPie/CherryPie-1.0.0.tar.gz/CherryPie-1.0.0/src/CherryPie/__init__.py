71d0082f20e78309023098044332bf1aa586ebc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:25 +0000

    Add package CherryPie/1.0.0/CherryPie-1.0.0.tar.gz

---
+++ packages/CherryPie/CherryPie-1.0.0.tar.gz/CherryPie-1.0.0/src/CherryPie/__init__.py
@@ -0,0 +1,14 @@
+""" 
+ _____ _                   _____ _
+|     | |_ ___ ___ ___ _ _|  _  |_|___
+|   --|   | -_|  _|  _| | |   __| | -_|
+|_____|_|_|___|_| |_| |_  |__|  |_|___|
+                      |___| 
+Github : https://github.com/NotReeceHarris/CherryPie
+Developer: Reece Harris (https://github.com/NotReeceHarris)
+"""
+
+from .menu import Menu
+from .lib.color import Color
+
+__version__ = "1.0.0"
