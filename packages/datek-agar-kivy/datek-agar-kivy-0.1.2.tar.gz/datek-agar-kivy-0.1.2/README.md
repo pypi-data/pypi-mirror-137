b7fe179f6eb48863997431112c850beaf59d7040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:47 +0000

    Add package datek-agar-kivy/0.1.2/datek-agar-kivy-0.1.2.tar.gz

---
+++ packages/datek-agar-kivy/datek-agar-kivy-0.1.2.tar.gz/datek-agar-kivy-0.1.2/README.md
@@ -0,0 +1,15 @@
+<a href="https://github.com/psf/black"><img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"></a>
+<a href="https://github.com/psf/black/blob/main/LICENSE"><img alt="License: MIT" src="https://black.readthedocs.io/en/stable/_static/license.svg"></a>
+
+# Agar - Kivy
+Kivy client for `datek-agar-core`.
+The project is in early development stage.
+
+## Usage
+After installation, run the `agar-kivy` command.
+
+## Controls
+increase speed: &#8593;  
+decrease speed: &#8595;  
+turn left: &#8592;  
+turn right: &#8594;
---
+++ packages/datek-agar-kivy/datek-agar-kivy-0.1.2.tar.gz/datek-agar-kivy-0.1.2/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['datek_agar_kivy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Kivy>=2.1.0dev0,<3.0.0',
+ 'datek-agar-core>=0.1.0,<0.2.0',
+ 'numpy>=1.20.0,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['agar-kivy = datek_agar_kivy.agar:main']}
+
+setup_kwargs = {
+    'name': 'datek-agar-kivy',
+    'version': '0.1.2',
+    'description': 'Kivy client for Agar game',
+    'long_description': '<a href="https://github.com/psf/black"><img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"></a>\n<a href="https://github.com/psf/black/blob/main/LICENSE"><img alt="License: MIT" src="https://black.readthedocs.io/en/stable/_static/license.svg"></a>\n\n# Agar - Kivy\nKivy client for `datek-agar-core`.\nThe project is in early development stage.\n\n## Usage\nAfter installation, run the `agar-kivy` command.\n\n## Controls\nincrease speed: &#8593;  \ndecrease speed: &#8595;  \nturn left: &#8592;  \nturn right: &#8594;',
+    'author': 'Attila Dudas',
+    'author_email': 'dudasa7@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/DAtek/datek-agar-kivy',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
