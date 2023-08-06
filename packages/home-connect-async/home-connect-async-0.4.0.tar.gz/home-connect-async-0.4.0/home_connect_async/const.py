7ab647b6a63cae88068f7aef9d7f70b7b064a40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:28 +0000

    Add package home-connect-async/0.4.0/home-connect-async-0.4.0.tar.gz

---
+++ packages/home-connect-async/home-connect-async-0.4.0.tar.gz/home-connect-async-0.4.0/home_connect_async/const.py
@@ -0,0 +1,8 @@
+SIM_HOST = "https://simulator.home-connect.com"
+API_HOST = "https://api.home-connect.com"
+ENDPOINT_AUTHORIZE = "/security/oauth/authorize"
+ENDPOINT_TOKEN = "/security/oauth/token"
+DEFAULT_SCOPES = [ 'IdentifyAppliance', 'Monitor', 'Control', 'Settings'  ]
+
+EVENT_DATA_REFRESHED = "DATA_REFRESHED"
+EVENT_CONNECTION_CHANGED = "CONNECTION_CHANGED"
