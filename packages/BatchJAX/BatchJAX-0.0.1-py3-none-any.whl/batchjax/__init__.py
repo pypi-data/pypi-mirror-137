f6c2ca7095aecf509101fc6f0ae99e34adb344a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:43 +0000

    Add package BatchJAX/0.0.1/BatchJAX-0.0.1-py3-none-any.whl

---
+++ packages/BatchJAX/BatchJAX-0.0.1-py3-none-any.whl/batchjax/__init__.py
@@ -0,0 +1,13 @@
+from .batcher import get_batched_vars, batch_over_batched_list, batch_over_objax_list, Batched
+from .looper import loop_fn
+from .core import BatchType, batch_or_loop
+
+__all__ = [ 
+    'get_batched_vars',
+    'batch_over_batched_list',
+    'batch_over_objax_list',
+    'loop_fn',
+    'BatchType',
+    'batch_or_loop',
+    'Batched'
+]
