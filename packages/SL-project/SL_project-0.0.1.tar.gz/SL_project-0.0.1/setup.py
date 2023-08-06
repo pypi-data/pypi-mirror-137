0454f6faa49c2a03f561a7bf9251984786f12e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:51 +0000

    Add package SL-project/0.0.1/SL_project-0.0.1.tar.gz

---
+++ packages/SL-project/SL_project-0.0.1.tar.gz/SL_project-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+setuptools.setup(
+    name="SL_project",
+    version="0.0.1",                        
+    author="Djeinaba",
+    author_email="djeinaba.ba@insa.ueuromed.org",
+    description="long description",
+    long_description="python package for ML models",
+    long_description_content_type="text/markdown",
+    install_requires=[                      
+        "numpy",
+        "sklearn"
+                                                  
+    ],                                             
+    url="https://gitlab.com/Djeinaba_Doro/test_project",  
+    packages=setuptools.find_packages(),
+    classifiers=(                                 
+        "Programming Language :: Python :: 3",    
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",   
+    ),
+)
