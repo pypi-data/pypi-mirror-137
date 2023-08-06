ae2aba8ae4299f34cb70820a8c77e7d813848a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:50 +0000

    Add package packageSatel/1.5/packageSatel-1.5.tar.gz

---
+++ packages/packageSatel/packageSatel-1.5.tar.gz/packageSatel-1.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='packageSatel',
+    version='1.5',
+    license='MIT',
+    description='test package',
+    long_description=open('README.md').read(),
+    author='Juan Dombald',
+    packages=['packageSatel'],
+    url='https://github.com/juanedomb/SATELLOGICTEST',
+    zip_safe= False 
+
+)
