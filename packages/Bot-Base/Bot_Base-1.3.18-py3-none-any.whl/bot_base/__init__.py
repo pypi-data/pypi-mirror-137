3259a3fa4ea350edc537659a4208681f4244850e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:52 +0000

    Add package Bot-Base/1.3.18/Bot_Base-1.3.18-py3-none-any.whl

---
+++ packages/Bot-Base/Bot_Base-1.3.18-py3-none-any.whl/bot_base/__init__.py
@@ -0,0 +1,15 @@
+import logging
+from collections import namedtuple
+
+from .bot import BotBase
+from .context import BotContext
+from .exceptions import *
+
+__version__ = "1.3.18"
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+version_info = VersionInfo(
+    major=1, minor=3, micro=18, releaselevel="production", serial=0
+)
