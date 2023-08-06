95bd4e377a434c44aa7d38ea5c14e932a2b7f527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:43 +0000

    Add package pypadb/0.1.1/pypadb-0.1.1-py3-none-any.whl

---
+++ packages/pypadb/pypadb-0.1.1-py3-none-any.whl/pypadb/conf/table_configurer.py
@@ -0,0 +1,35 @@
+import inspect
+
+from pypadb.table.base_table import BaseTable
+
+
+class TableConfigurer:
+    __slots__ = ['tables']
+
+    tables: dict[str, BaseTable]
+
+    def __init__(self):
+        self.tables = {}
+
+    def __getattr__(self, item: str) -> BaseTable:
+        return self.tables[item]
+
+    def __getitem__(self, item: str) -> BaseTable:
+        return self.tables[item]
+
+    def init_tables(self, module_=None, escape_upper: bool = False, **kwargs):
+        if inspect.ismodule(module_):
+            module_dict: dict = {}
+            for i in inspect.getmembers(__import__('entities')):
+                member_name = ''
+                for ch in i[0]:
+                    if ch.isupper() and not escape_upper:
+                        member_name += '_'
+                    member_name += ch.lower()
+                module_dict[member_name] = i[1]
+            self.tables = {**self.tables, **{key: BaseTable(key, module_dict[key]) for key in module_dict}}
+        if kwargs:
+            self.tables = {**self.tables, **{key: BaseTable(key, kwargs[key]) for key in kwargs}}
+
+
+tables: TableConfigurer = TableConfigurer()
