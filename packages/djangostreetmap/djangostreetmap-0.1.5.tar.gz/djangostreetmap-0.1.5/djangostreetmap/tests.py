3ad98aaa30b516c98ad724d59c732f72ea196f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:43 +0000

    Add package djangostreetmap/0.1.5/djangostreetmap-0.1.5.tar.gz

---
+++ packages/djangostreetmap/djangostreetmap-0.1.5.tar.gz/djangostreetmap-0.1.5/djangostreetmap/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+
+class MyTestCase(TestCase):
+    def test_foo(self):
+        self.assertEqual(True, True)
---
+++ packages/djangostreetmap/djangostreetmap-0.1.5.tar.gz/djangostreetmap-0.1.5/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['djangostreetmap',
+ 'djangostreetmap.management.commands',
+ 'djangostreetmap.migrations']
+
+package_data = \
+{'': ['*'], 'djangostreetmap': ['templates/*']}
+
+install_requires = \
+['osmflex>=0.1.2,<0.2.0']
+
+setup_kwargs = {
+    'name': 'djangostreetmap',
+    'version': '0.1.5',
+    'description': '',
+    'long_description': None,
+    'author': 'Joshua Brooks',
+    'author_email': 'josh.vdbroek@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
