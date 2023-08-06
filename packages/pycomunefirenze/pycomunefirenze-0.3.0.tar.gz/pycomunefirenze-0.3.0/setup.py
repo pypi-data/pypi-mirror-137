bf38662f8bcd8a3ea42bdaa384dcd0042dddd4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:41 +0000

    Add package pycomunefirenze/0.3.0/pycomunefirenze-0.3.0.tar.gz

---
+++ packages/pycomunefirenze/pycomunefirenze-0.3.0.tar.gz/pycomunefirenze-0.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pycomunefirenze",
+    version="0.3.0",
+    author="Ubaldo Puocci",
+    author_email="ubaldo.puocci@comune.fi.it",
+    packages=["pycomunefirenze"],
+    include_package_data=True,
+    install_requires=["requests", "psycopg2", "redmail"],
+)
