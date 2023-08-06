c62c80eca03feeba219132da79eb147c459a3692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:16 +0000

    Add package osinku-lib/0.0/osinku_lib-0.0.tar.gz

---
+++ packages/osinku-lib/osinku_lib-0.0.tar.gz/osinku_lib-0.0/setup.py
@@ -0,0 +1,9 @@
+# encoding utf-8
+import setuptools
+
+setuptools.setup(
+    name="osinku_lib",
+    version="0.0",
+    description="La ricerca scientifica ci ha portato a questo.",
+    author="Komodo, Takaso",
+)
