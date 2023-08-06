9cbbb595a2db80a394edf1ec965fc4c049ed5529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:03 +0000

    Add package hero1/0.5/hero1-0.5.tar.gz

---
+++ packages/hero1/hero1-0.5.tar.gz/hero1-0.5/hero1/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+from hero1.hero1 import find_ip, setup, init, cmd, render, setCanMoveAll, scan, scanx, testx, moveRight, moveLeft, moveUp, moveDown, canMoveRight, canMoveLeft, canMoveUp, canMoveDown
+from hero1.command_pb2 import CmdType, Cmd
