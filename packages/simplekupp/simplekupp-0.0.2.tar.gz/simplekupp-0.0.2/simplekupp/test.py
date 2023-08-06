d495b2412b8a3f2c13aa55d9e1c285de6d994125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:55 +0000

    Add package simplekupp/0.0.2/simplekupp-0.0.2.tar.gz

---
+++ packages/simplekupp/simplekupp-0.0.2.tar.gz/simplekupp-0.0.2/simplekupp/test.py
@@ -0,0 +1,9 @@
+import simplekupp as vsm
+
+print(vsm.add(2,5))
+print(vsm.subtract(5, 4))
+
+print(vsm.extras.devide(27,3))
+print(vsm.extras.multiply(5,6))
+print(vsm.extras.divide(4,2))
+print(vsm.extras.multiple(5,3))
