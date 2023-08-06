98cff1904e1b14d03e9c16e2fdab018b9ef3b1f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:28 +0000

    Add package dev-utils/4.0.0/dev_utils-4.0.0.tar.gz

---
+++ packages/dev-utils/dev_utils-4.0.0.tar.gz/dev_utils-4.0.0/dev_utils/__init__.py
@@ -0,0 +1,6 @@
+from .mongo_bulk import MongoDB
+from .proxy_utils import Proxy
+from .email_utils import Email
+from .log import Logger
+from .downloader import Downloader
+from .uas import UAS
---
+++ packages/dev-utils/dev_utils-4.0.0.tar.gz/dev_utils-4.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dev_utils",  # Replace with your own username
+    version="4.0.0",
+    author="p",
+    author_email="author@example.com",
+    description="utils",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
