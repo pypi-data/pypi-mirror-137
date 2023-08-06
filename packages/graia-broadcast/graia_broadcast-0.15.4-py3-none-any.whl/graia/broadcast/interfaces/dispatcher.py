926217c08a817786f99e9f8528b956a0979881bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:31 +0000

    Add package graia-broadcast/0.15.4/graia_broadcast-0.15.4-py3-none-any.whl

---
+++ packages/graia-broadcast/graia_broadcast-0.15.4-py3-none-any.whl/graia/broadcast/interfaces/dispatcher.py
@@ -0,0 +1,112 @@
+from typing import (TYPE_CHECKING, Any, ClassVar, Dict, Generic, List, Tuple,
+                    TypeVar)
+
+from ..entities.dispatcher import BaseDispatcher
+from ..entities.event import Dispatchable
+from ..entities.signatures import Force
+from ..exceptions import RequirementCrashed
+from ..typing import T_Dispatcher
+from ..utilles import Ctx, NestableIterable
+
+if TYPE_CHECKING:
+    from .. import Broadcast
+
+
+T_Event = TypeVar("T_Event", bound=Dispatchable)
+
+
+class DispatcherInterface(Generic[T_Event]):
+    __slots__ = {"broadcast", "dispatchers", "parameter_contexts", "local_storage", "current_path", "current_oplog"}
+
+    ctx: "ClassVar[Ctx[DispatcherInterface]]" = Ctx("bcc_dii")
+
+    broadcast: "Broadcast"
+    dispatchers: List[T_Dispatcher]
+    local_storage: Dict[str, Any]
+    current_path: NestableIterable[T_Dispatcher]
+    current_oplog: List[T_Dispatcher]
+
+    parameter_contexts: List[Tuple[str, Any, Any]]
+
+    def __init__(self, broadcast_instance: "Broadcast", dispatchers: List[T_Dispatcher]) -> None:
+        self.broadcast = broadcast_instance
+        self.dispatchers = dispatchers
+        self.parameter_contexts = []
+        self.local_storage = {}
+        self.current_path = NestableIterable([])
+        self.current_oplog = []
+
+    @property
+    def name(self) -> str:
+        return self.parameter_contexts[-1][0]
+
+    @property
+    def annotation(self) -> Any:
+        return self.parameter_contexts[-1][1]
+
+    @property
+    def default(self) -> Any:
+        return self.parameter_contexts[-1][2]
+
+    @property
+    def event(self) -> T_Event:
+        return self.broadcast.event_ctx.get()  # type: ignore
+
+    def inject_execution_raw(self, *dispatchers: T_Dispatcher):
+        for dispatcher in dispatchers:
+            self.dispatchers.insert(0, dispatcher)
+
+    async def lookup_param(
+        self,
+        name: str,
+        annotation: Any,
+        default: Any,
+    ) -> Any:
+        self.parameter_contexts.append((name, annotation, default))
+        oplog = self.current_oplog
+
+        try:
+            if oplog:
+                self.current_path.iterable = oplog
+                for dispatcher in self.current_path:
+                    result = await dispatcher.catch(self)
+                    if result is None:  # 不可靠.
+                        break
+                    if result.__class__ is Force:
+                        return result.target
+                    return result
+                oplog.clear()
+            self.current_path.iterable = self.dispatchers
+            for dispatcher in self.current_path:
+                result = await dispatcher.catch(self)
+
+                if result is None:
+                    continue
+
+                oplog.insert(0, dispatcher)
+
+                if result.__class__ is Force:
+                    return result.target
+
+                return result
+            else:
+                raise RequirementCrashed(
+                    "the dispatching requirement crashed: ",
+                    self.name,
+                    self.annotation,
+                    self.default,
+                )
+        finally:
+            self.parameter_contexts.pop()
+
+    async def lookup_by_directly(self, dispatcher: T_Dispatcher, name: str, annotation: Any, default: Any) -> Any:
+        self.parameter_contexts.append((name, annotation, default))
+
+        try:
+            result = await dispatcher.catch(self)
+            if result.__class__ is Force:
+                return result.target
+
+            return result
+        finally:
+            self.parameter_contexts.pop()
---
+++ packages/graia-broadcast/graia_broadcast-0.15.4-py3-none-any.whl/graia/broadcast/utilles.py
@@ -0,0 +1,130 @@
+import inspect
+from contextlib import contextmanager
+from contextvars import ContextVar, Token
+from functools import lru_cache
+from typing import (TYPE_CHECKING, Any, Awaitable, Callable, Generic, Iterable,
+                    List, Type, TypeVar, Union)
+
+from .entities.dispatcher import BaseDispatcher
+
+if TYPE_CHECKING:
+    from graia.broadcast.typing import T_Dispatcher
+
+
+async def run_always_await(any_callable: Union[Awaitable, Callable]):
+    if inspect.isawaitable(any_callable):
+        return await any_callable
+    else:
+        return any_callable
+
+
+async def run_always_await_safely(callable, *args, **kwargs):
+    if iscoroutinefunction(callable):
+        return await callable(*args, **kwargs)
+    return callable(*args, **kwargs)
+
+
+T = TypeVar("T")
+D = TypeVar("D")
+
+
+class Ctx(Generic[T]):
+    current_ctx: ContextVar[T]
+
+    def __init__(self, name: str) -> None:
+        self.current_ctx = ContextVar(name)
+
+    def get(self, default: Union[T, D] = None) -> Union[T, D]:
+        return self.current_ctx.get(default)
+
+    def set(self, value: T):
+        return self.current_ctx.set(value)
+
+    def reset(self, token: Token):
+        return self.current_ctx.reset(token)
+
+    @contextmanager
+    def use(self, value: T):
+        token = self.set(value)
+        yield
+        self.reset(token)
+
+
+def printer(value: Any):
+    print(value)
+    return value
+
+
+def group_dict(iterable: Iterable, key_callable: Callable[[Any], Any]):
+    temp = {}
+    for i in iterable:
+        k = key_callable(i)
+        temp.setdefault(k, [])
+        temp[k].append(i)
+    return temp
+
+
+cache_size = 4096
+
+
+@lru_cache(cache_size)
+def argument_signature(callable_target: Callable):
+    return [
+        (
+            name,
+            param.annotation if param.annotation is not inspect.Signature.empty else None,
+            param.default if param.default is not inspect.Signature.empty else None,
+        )
+        for name, param in inspect.signature(callable_target).parameters.items()
+    ]
+
+
+@lru_cache(cache_size)
+def is_asyncgener(o):
+    return inspect.isasyncgenfunction(o)
+
+
+@lru_cache(cache_size)
+def iscoroutinefunction(o):
+    return inspect.iscoroutinefunction(o)
+
+
+@lru_cache(cache_size)
+def isasyncgen(o):
+    return inspect.isasyncgen(o)
+
+
+@lru_cache(None)
+def dispatcher_mixin_handler(dispatcher: Union[Type[BaseDispatcher], BaseDispatcher]) -> "List[T_Dispatcher]":
+    unbound_mixin = getattr(dispatcher, "mixin", [])
+    result: "List[T_Dispatcher]" = [dispatcher]
+
+    for i in unbound_mixin:
+        if issubclass(i, BaseDispatcher):
+            result.extend(dispatcher_mixin_handler(i))
+        else:
+            result.append(i)
+    return result
+
+
+class NestableIterable(Iterable[T]):
+    index_stack: list
+    iterable: List[T]
+
+    def __init__(self, iterable: List[T]) -> None:
+        self.iterable = iterable
+        self.index_stack = [0]
+
+    def __iter__(self):
+        index = self.index_stack[-1]
+        self.index_stack.append(index)
+
+        start_offset = index + index and 1
+        try:
+            for self.index_stack[-1], content in enumerate(
+                self.iterable[start_offset:],
+                start=start_offset,
+            ):
+                yield content
+        finally:
+            self.index_stack.pop()
