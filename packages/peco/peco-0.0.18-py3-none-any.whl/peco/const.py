c6a81bebb2ec7732b6d30925284f3c7ffec7af74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:18 +0000

    Add package peco/0.0.18/peco-0.0.18-py3-none-any.whl

---
+++ packages/peco/peco-0.0.18-py3-none-any.whl/peco/const.py
@@ -0,0 +1,10 @@
+API_URL = "https://kubra.io/stormcenter/api/v1/stormcenters/39e6d9f3-fdea-4539-848f-b8631945da6f/views/74de8a50-3f45-4f6a-9483-fd618bb9165d/currentState?preview=false"
+REPORT_URL = "https://kubra.io/{}/public/reports/a36a6292-1c55-44de-a6a9-44fedf9482ee_report.json"
+COUNTY_LIST = [
+    "BUCKS",
+    "CHESTER",
+    "DELAWARE",
+    "MONTGOMERY",
+    "PHILADELPHIA",
+    "YORK",
+]
