82dfe13c9db5e9a8d53b9b489ddd56e6b0b9c98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:35:06 +0000

    Add package lazy-table/0.2.1/lazy_table-0.2.1-py3-none-any.whl

---
+++ packages/lazy-table/lazy_table-0.2.1-py3-none-any.whl/lazy_table/__init__.py
@@ -0,0 +1,5 @@
+"""A python-tabulate wrapper for producing tables from generators."""
+
+__all__ = ['stream']
+
+from ._lazy_table import stream
---
+++ packages/lazy-table/lazy_table-0.2.1-py3-none-any.whl/lazy_table/_lazy_table.py
@@ -0,0 +1,47 @@
+"""_lazy_table.py"""
+
+from tabulate import tabulate
+
+from .artists import Console
+
+
+def stream(table, artist=None, n_rows=None, **kwargs):
+    """Streams a table.
+
+    kwargs are forwarded to tabulate.
+
+    Examples
+    --------
+    >>> import time
+    >>> import lazy_table as lt
+    >>>
+    >>> def fib_table(n):
+    ...     x0, x1 = 0, 1
+    ...     yield [0, x0]
+    ...     yield [1, x1]
+    ...     for i in range(2, n + 1):
+    ...         x0, x1 = x1, x0 + x1
+    ...         yield [i, x1]
+    ...         time.sleep(1)  # Simulate work
+    >>>
+    >>> lt.stream(fib_table(10), headers=['N', 'F_N'])
+
+    Parameters
+    ----------
+    table : Generator[List[T], None, None]
+        A generator which yields rows of the table.
+    artist : Callable[[str], None], optional
+        A callable which determines how to render a table. If unspecified, ``lazy_table.artists.Console()`` is used.
+    n_rows : int, optional
+        Number of rows in table.
+    """
+    if artist is None:
+        artist = Console()
+    rows = []
+    result = tabulate(rows, **kwargs)
+    artist.init(n_rows)
+    artist.render(result)
+    for row in table:
+        rows.append(row)
+        result = tabulate(rows, **kwargs)
+        artist.render(result)
