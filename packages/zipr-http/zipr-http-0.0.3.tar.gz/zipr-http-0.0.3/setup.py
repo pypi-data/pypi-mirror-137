f8315065eed46bb731b1cb1be7f3347d7c078585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:01 +0000

    Add package zipr-http/0.0.3/zipr-http-0.0.3.tar.gz

---
+++ packages/zipr-http/zipr-http-0.0.3.tar.gz/zipr-http-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='zipr-http',
+    version='0.0.3',
+    author='Andrew Hoekstra',
+    author_email='andrew@pointevector.com',
+    url='https://github.com/Pointe-Vector/zipr',
+    packages=find_namespace_packages(include=['zipr.*']),
+    install_requires=[
+        'zipr-core',
+    ],
+    entry_points={'zipr.plugins': 'Http = zipr.http'},
+)
