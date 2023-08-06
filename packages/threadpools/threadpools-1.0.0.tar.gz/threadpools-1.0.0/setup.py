798a266b59cd85bc83bcfbb919ec71fe4ff5c054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:34 +0000

    Add package threadpools/1.0.0/threadpools-1.0.0.tar.gz

---
+++ packages/threadpools/threadpools-1.0.0.tar.gz/threadpools-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="threadpools",
+    version="1.0.0",
+    author="Dmitry Kalinin",
+    packages=["threadpools"],
+    url="https://github.com/null-none/threadpools",
+    keywords=[
+        "thread",
+        "pool",
+        "thread pool"
+        "thread pools"
+    ],
+    install_requires=[],
+)
