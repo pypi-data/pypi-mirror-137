ff7023ab1748e809c928768f0978fb5ea62c53e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:43 +0000

    Add package testversioningpkg/0.1.2/testversioningpkg-0.1.2.tar.gz

---
+++ packages/testversioningpkg/testversioningpkg-0.1.2.tar.gz/testversioningpkg-0.1.2/testversioningpkg/versions/v2/agenda.py
@@ -0,0 +1,19 @@
+def get_agenda_session():
+    session_dummy = {
+        "desc": "This event will tell you about Travel around the world using google technology and any",
+        "favorited": 1,
+        "has_livestream": 1,
+        "id_object": 4133,
+        "loc": "Google Office",
+        "name": "Welcome to Travel Fair Event 2019",
+        "speaker_photos": ["https://buzz-images.jublia.com/agenda/475/16192_0.png"],
+        "tags": ["First", "Second", "Third", "Do Bee Doo Bee Doo Bee Doo", "Tag1"],
+        "time": "06:00",
+        "local_time": "08:00",
+        "local_date": "20 Dec",
+        "image_url": "image url",
+        "timestamp": "2020-12-20 06:00:00",
+        "track": "Google Auditorium",
+        "type": "session"
+    }
+    return session_dummy
