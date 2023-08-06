4953204c07afb7a326bb4ee2f5a5b42153eac6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:16 +0000

    Add package xarrayfrac/0.0.5/xarrayfrac-0.0.5.tar.gz

---
+++ packages/xarrayfrac/xarrayfrac-0.0.5.tar.gz/xarrayfrac-0.0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name='xarrayfrac',
+    version='0.0.5',
+    url='https://github.com/benjaminleighton/xarrayfrac',
+    author='Ben Leighton',
+    author_email='benplei@gmail.com',
+    description='mandelbrot dynamically generated xarray backend',
+    packages=['xarrayfrac'],
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+          'xarray',
+          'dask',
+          'distributed',
+          'numpy>=1.21.5',
+      ],
+    entry_points={
+        "xarray.backends": ["xarrayfrac=xarrayfrac.frac_backend:DynamicMandelbrotEntrypoint"],
+    },
+)
