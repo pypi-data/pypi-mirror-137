83e467fb44803a959b5cbfce4c0cbfb093de6b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:13 +0000

    Add package egoreliseev-python-hw1/0.0/egoreliseev_python_hw1-0.0.tar.gz

---
+++ packages/egoreliseev-python-hw1/egoreliseev_python_hw1-0.0.tar.gz/egoreliseev_python_hw1-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='egoreliseev_python_hw1',
+      version='0.0',
+      description='Build AST for fibonacci func',
+      packages=['egoreliseev_python_hw1'],
+      author_email='eliseevegor1920@gmail.com',
+      zip_safe=False)
