4f5e0e174147c9cf385e34989ec4bcb45d0c7f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:34 +0000

    Add package bioluigi/0.0.7/bioluigi-0.0.7.tar.gz

---
+++ packages/bioluigi/bioluigi-0.0.7.tar.gz/bioluigi-0.0.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import sys
+
+# Luigi 3+ does not support Python 2
+if sys.version_info.major == 2:
+    luigi_dep = 'luigi~=2.8'
+else:
+    luigi_dep = 'luigi'
+
+setup(name='bioluigi',
+      version='0.0.7',
+      description='Reusable and maintained Luigi tasks to incorporate in bioinformatics pipelines',
+      long_description='file: README.md',
+      author='Guillaume Poirier-Morency',
+      author_email='poirigui@msl.ubc.ca',
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'bioluigi = bioluigi.cli:main']},
+          install_requires=['click', luigi_dep, 'requests', 'babel'])
