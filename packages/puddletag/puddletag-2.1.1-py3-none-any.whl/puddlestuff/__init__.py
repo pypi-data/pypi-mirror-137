7633da309b3f26e52c363e9cdcc5b79c680414a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:14 +0000

    Add package puddletag/2.1.1/puddletag-2.1.1-py3-none-any.whl

---
+++ packages/puddletag/puddletag-2.1.1-py3-none-any.whl/puddlestuff/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import os
+from os.path import dirname
+
+
+_buildid = None
+version = [2, 1, 1]
+if _buildid:
+    version.append(f"post{_buildid}")
+version_string = ".".join(map(str, version))
+
+changeset = None
+
+filedir = dirname(dirname(dirname(__file__)))
+hash_file = os.path.join(filedir, ".git/refs/heads/master")
+if os.path.exists(hash_file):
+    try:
+        with open(hash_file) as fo:
+            changeset = fo.read().strip()
+    except (EnvironmentError, AttributeError):
+        pass
