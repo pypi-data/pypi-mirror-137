028fbba1e410f4ba1e5d7a2a0f0f875dbdff4496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:51 +0000

    Add package hexdragon/0.0.5/hexdragon-0.0.5.tar.gz

---
+++ packages/hexdragon/hexdragon-0.0.5.tar.gz/hexdragon-0.0.5/README.md
@@ -0,0 +1,11 @@
+# Hexdragon
+
+This is an art project. 
+
+Print a file as coloured hexadecimal values to console.
+
+```bash
+> python3 -m hexdragon <file>
+```
+
+![console](./docs/console.png)
---
+++ packages/hexdragon/hexdragon-0.0.5.tar.gz/hexdragon-0.0.5/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="hexdragon",
+    version="0.0.5",
+    description="Print a file as coloured hexadecimal to console",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/0xflotus/hexdragon",
+    author="0xflotus",
+    author_email="0xflotus+pypi@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["hexdragon"],
+    include_package_data=True,
+    install_requires=["argparse"],
+    tests_require=["pytest"],
+    entry_points={"console_scripts": ["hexdragon=hexdragon.__main__:main"]},
+)
