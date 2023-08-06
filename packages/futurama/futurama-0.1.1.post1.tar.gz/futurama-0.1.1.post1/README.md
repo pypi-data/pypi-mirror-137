66dbb221613c291b2ebd262e6b58b293d1454cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:19 +0000

    Add package futurama/0.1.1.post1/futurama-0.1.1.post1.tar.gz

---
+++ packages/futurama/futurama-0.1.1.post1.tar.gz/futurama-0.1.1.post1/README.md
@@ -0,0 +1,5 @@
+# futurama
+
+Random Futurama Quotes
+
+[![Build Status](https://drone.decapod.one/api/badges/brethil/futurama/status.svg?ref=refs/heads/main)](https://drone.decapod.one/brethil/futurama)
---
+++ packages/futurama/futurama-0.1.1.post1.tar.gz/futurama-0.1.1.post1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['futurama']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['futurama = futurama.__main__:main']}
+
+setup_kwargs = {
+    'name': 'futurama',
+    'version': '0.1.1.post1',
+    'description': 'Random Futurama Quotes',
+    'long_description': '# futurama\n\nRandom Futurama Quotes\n\n[![Build Status](https://drone.decapod.one/api/badges/brethil/futurama/status.svg?ref=refs/heads/main)](https://drone.decapod.one/brethil/futurama)\n',
+    'author': 'brethil',
+    'author_email': 'bretello@distruzione.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://git.decapod.one/brethil/futurama',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
