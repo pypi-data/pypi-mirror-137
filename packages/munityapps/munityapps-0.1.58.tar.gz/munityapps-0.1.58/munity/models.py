229a7d1696e614ff64d10b2a575ab9ca7c04016d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:11 +0000

    Add package munityapps/0.1.58/munityapps-0.1.58.tar.gz

---
+++ packages/munityapps/munityapps-0.1.58.tar.gz/munityapps-0.1.58/munity/models.py
@@ -0,0 +1,28 @@
+import uuid
+
+# from .groups.models import Group
+
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+from django.db.models.signals import post_save
+
+
+class MunityModel(TimeStampedModel):
+    """
+    Default base class for munity models
+    use uuid for ids
+    add creation and update date
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    generic_groups = models.ManyToManyField('generic_groups.GenericGroup', blank=True)
+    disabled = models.BooleanField(default=False)
+    workspace = models.ForeignKey(
+        "workspaces.Workspace", to_field="slug", on_delete=models.CASCADE, null=True, blank=True
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f"{self.id}@{self.workspace or 'overmind'}"
