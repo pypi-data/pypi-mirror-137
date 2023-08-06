54fd14d1b5237a5d28611b7fd3eeb553c0fb86d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:40 +0000

    Add package validatortoolbox/1.2.10/validatortoolbox-1.2.10.tar.gz

---
+++ packages/validatortoolbox/validatortoolbox-1.2.10.tar.gz/validatortoolbox-1.2.10/toolbox/start.py
@@ -0,0 +1,31 @@
+import os
+import time
+import dotenv
+from os import environ
+from utils.config import validatorToolbox
+from utils.installer import firstSetup, printStars, recheckVars, passphraseStatus
+from utils.shared import loaderIntro, setWalletEnv
+from utils.toolbox import runRegularNode, runFullNode
+
+
+if __name__ == "__main__":
+    os.system("clear")
+    loaderIntro()
+    if not os.path.exists(validatorToolbox.dotenv_file):
+        firstSetup()
+    print("* Configuration file detected, loading the validatortoolbox menu application.")
+    printStars()
+    if validatorToolbox.easyVersion != environ.get("EASY_VERSION"):
+        dotenv.unset_key(validatorToolbox.dotenv_file, "EASY_VERSION")
+        dotenv.set_key(validatorToolbox.dotenv_file, "EASY_VERSION", validatorToolbox.easyVersion)
+    if environ.get("SETUP_STATUS") != "2":
+        recheckVars()
+        passphraseStatus()
+    if environ.get("NODE_TYPE") == "regular":
+        if not environ.get("VALIDATOR_WALLET"):
+            setWalletEnv()
+        runRegularNode()
+    if environ.get("NODE_TYPE") == "full":
+        runFullNode()
+    print("Uh oh, you broke me! Contact Easy Node")
+    raise SystemExit(0)
