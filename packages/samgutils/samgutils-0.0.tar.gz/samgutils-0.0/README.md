f61329eaae0f5edf5c6f93669aa1fc0e86958d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:37 +0000

    Add package samgutils/0.0/samgutils-0.0.tar.gz

---
+++ packages/samgutils/samgutils-0.0.tar.gz/samgutils-0.0/README.md
@@ -0,0 +1,2 @@
+# samgutils
+ descriptions
---
+++ packages/samgutils/samgutils-0.0.tar.gz/samgutils-0.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="samgutils",
+    version="0.0",
+    author="Luis Liborio",
+    author_email="lsmliborio@gmail.com",
+    description="A Package descr",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lsmliborio/samgutils.git",
+    packages=setuptools.find_packages(),
+    install_requires  = [], # List all your dependencies inside the list
+    license = 'MIT'
+)
