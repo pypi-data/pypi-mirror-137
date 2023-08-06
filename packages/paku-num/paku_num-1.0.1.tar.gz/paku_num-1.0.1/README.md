fc4ca08fbf114b8fe838129329db52ed0a6d078d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:18 +0000

    Add package paku-num/1.0.1/paku_num-1.0.1.tar.gz

---
+++ packages/paku-num/paku_num-1.0.1.tar.gz/paku_num-1.0.1/README.md
@@ -0,0 +1,5 @@
+# Paku_Num Package
+
+This is a simple package. You can use
+[Github-flavored Markdown](http://robub.rf.gd/)
+to write your content.
---
+++ packages/paku-num/paku_num-1.0.1.tar.gz/paku_num-1.0.1/src/paku_num/number.py
@@ -0,0 +1,21 @@
+##############################################
+### DO NOT COPYRIGHT OUR PACKAGE OR MODULE ###
+##############################################
+
+# module needed
+import random
+
+# start of module
+def random_num():
+    number = random.randint(1, 9999999)
+    print(f'You just got random number {number} !')
+
+def show_answer(name):
+    return print(f'Hi {name} !')
+
+def delete_all(howmuch):
+    return None
+
+####################
+### End Of Files ###
+####################
