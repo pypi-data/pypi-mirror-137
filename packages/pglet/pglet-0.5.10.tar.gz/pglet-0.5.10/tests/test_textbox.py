953d00adb42ae875492d483ea5f5b72af0ac12a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:02 +0000

    Add package pglet/0.5.10/pglet-0.5.10.tar.gz

---
+++ packages/pglet/pglet-0.5.10.tar.gz/pglet-0.5.10/tests/test_textbox.py
@@ -0,0 +1,10 @@
+import pglet
+
+
+def test_textbox_add():
+    tb = pglet.Textbox(id="txt1", label="Your name:")
+    assert isinstance(tb, pglet.Control)
+    assert isinstance(tb, pglet.Textbox)
+    assert (
+        tb.get_cmd_str(indent="  ") == '  textbox id="txt1" label="Your name:"'
+    ), "Test failed"
