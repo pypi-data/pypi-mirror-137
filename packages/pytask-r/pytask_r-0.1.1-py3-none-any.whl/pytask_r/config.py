460d0bd3bd4a11c13fbf6411970ec950c733c1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:07 +0000

    Add package pytask-r/0.1.1/pytask_r-0.1.1-py3-none-any.whl

---
+++ packages/pytask-r/pytask_r-0.1.1-py3-none-any.whl/pytask_r/config.py
@@ -0,0 +1,11 @@
+"""Configure pytask."""
+from __future__ import annotations
+
+from _pytask.config import hookimpl
+
+
+@hookimpl
+def pytask_parse_config(config, config_from_file):
+    """Register the r marker."""
+    config["markers"]["r"] = "Tasks which are executed with Rscript."
+    config["r_source_key"] = config_from_file.get("r_source_key", "source")
