157cd85e33cf337546e8f64a7bfe7c25426a1843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:53 +0000

    Add package atec-gdy/0.0.2/atec_gdy-0.0.2.tar.gz

---
+++ packages/atec-gdy/atec_gdy-0.0.2.tar.gz/atec_gdy-0.0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='atec_gdy',
+    version='0.0.2',
+    author='xxx',
+    author_email='xxx@qq.com',
+    description=u'atec_gdy',
+    url='https://zhuanlan.zhihu.com/p/26159930',
+    packages=['atec'],
+    install_requires=[],
+)
