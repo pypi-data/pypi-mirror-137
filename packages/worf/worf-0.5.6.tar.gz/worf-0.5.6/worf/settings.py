e0045d17d98921f9035537abfabbfa42584a4e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:32 +0000

    Add package worf/0.5.6/worf-0.5.6.tar.gz

---
+++ packages/worf/worf-0.5.6.tar.gz/worf-0.5.6/worf/settings.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+
+WORF_API_NAME = getattr(settings, "WORF_API_NAME", "Worf API")
+WORF_API_ROOT = getattr(settings, "WORF_API_ROOT", "/api/")
+
+WORF_BROWSABLE_API = getattr(settings, "WORF_BROWSABLE_API", True)
+
+WORF_DEBUG = getattr(settings, "WORF_DEBUG", settings.DEBUG)
+
+WORF_SERIALIZER_DEFAULT_OPTIONS = getattr(
+    settings, "WORF_SERIALIZER_DEFAULT_OPTIONS", {}
+)
