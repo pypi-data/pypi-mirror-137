62f19a28b319c9d254534c2e5ce7528bbfca7c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:39 +0000

    Add package zoofs/0.1.7/zoofs-0.1.7.tar.gz

---
+++ packages/zoofs/zoofs-0.1.7.tar.gz/zoofs-0.1.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(name='zoofs',
+      version='0.1.7',
+      url='https://github.com/jaswinder9051998/zoofs',
+      author='JaswinderSingh',
+      author_email='jaswinder9051998@gmail.com',
+      license='Apache License 2.0',
+      packages=['zoofs'],
+      zip_safe=True,
+	description="zoofs is a Python library for performing feature selection using an variety of nature inspired wrapper algorithms..",
+      long_description=long_description  ,
+	long_description_content_type='text/markdown',
+	  install_requires=[
+		"pandas",
+		"numpy",
+		"scipy",
+            "plotly",
+            "colorlog"
+		],
+
+      )
