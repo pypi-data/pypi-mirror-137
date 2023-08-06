b3490bb9d9381f9955bb568ce8ba87bfe97693ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:07 +0000

    Add package texipy/0.0.0/TeXiPy-0.0.0.tar.gz

---
+++ packages/texipy/TeXiPy-0.0.0.tar.gz/TeXiPy-0.0.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['texipy']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'texipy',
+    'version': '0.0.0',
+    'description': 'Python for LaTeX',
+    'long_description': '"# TeXiPy" \n',
+    'author': 'Praveen Kulkarni',
+    'author_email': 'praveenneuron@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
