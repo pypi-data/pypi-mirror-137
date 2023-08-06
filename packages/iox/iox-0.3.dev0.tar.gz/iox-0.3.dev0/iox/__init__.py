91b21fcecc5f7fd995ecac07a4b94eaf29e37c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:56 +0000

    Add package iox/0.3.dev0/iox-0.3.dev0.tar.gz

---
+++ packages/iox/iox-0.3.dev0.tar.gz/iox-0.3.dev0/iox/__init__.py
@@ -0,0 +1,18 @@
+
+# from ._config import *
+# from .core import *
+# from .google import *
+from .numpy_ import *
+from .pandas_ import *
+from ._version import __version__
+
+__all__ = [
+    # 'authenticate',
+    # 'BigQuery',
+    # 'GoogleSheet',
+    # 'query',
+    'loadtxt',
+    'read_csv',
+    'read_table',
+    '__version__'
+]
