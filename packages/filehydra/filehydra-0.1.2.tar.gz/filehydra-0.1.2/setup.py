399ce6f95069fe5ff064074e367714e1583f3431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:32 +0000

    Add package filehydra/0.1.2/filehydra-0.1.2.tar.gz

---
+++ packages/filehydra/filehydra-0.1.2.tar.gz/filehydra-0.1.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+    
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name='filehydra',
+    version='0.1.2',
+    author='DovaX',
+    author_email='dovax.ai@gmail.com',
+    description='A Python package focusing on file management and routine file operations',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/DovaX/filehydra',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'pandas'
+     ],
+    python_requires='>=3.6',
+)
+    
