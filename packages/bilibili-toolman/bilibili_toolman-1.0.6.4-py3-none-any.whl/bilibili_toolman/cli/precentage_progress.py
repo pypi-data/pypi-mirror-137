986c44aa60b4ad7f2a2c555752bedf39e00f8a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:44 +0000

    Add package bilibili-toolman/1.0.6.4/bilibili_toolman-1.0.6.4-py3-none-any.whl

---
+++ packages/bilibili-toolman/bilibili_toolman-1.0.6.4-py3-none-any.whl/bilibili_toolman/cli/precentage_progress.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+try:
+    from tqdm import tqdm
+    tqdm_ = tqdm(desc='↑', unit='B', unit_scale=True)  
+except:
+    tqdm_ = None
+def report(current,max_val):
+    if tqdm_ is None:return
+    tqdm_.total = max_val
+    tqdm_.n = current    
+    tqdm_.update(0)
+    tqdm_.refresh()
+def close():
+    if tqdm_ is None:return
+    tqdm_.close()
