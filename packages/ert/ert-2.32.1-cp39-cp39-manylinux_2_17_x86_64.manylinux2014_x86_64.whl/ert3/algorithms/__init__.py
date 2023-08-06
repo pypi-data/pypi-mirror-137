52d91430b52220bd37aaa45588c5d36c9618c4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:56 +0000

    Add package ert/2.32.1/ert-2.32.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ert/ert-2.32.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ert3/algorithms/__init__.py
@@ -0,0 +1,3 @@
+from ert3.algorithms._sensitivity import one_at_the_time, fast_sample, fast_analyze
+
+__all__ = ["one_at_the_time", "fast_sample", "fast_analyze"]
---
+++ packages/ert/ert-2.32.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ert3/stats/_stats.py
@@ -0,0 +1,146 @@
+from typing import Optional, Callable, cast
+
+import numpy as np
+import scipy.stats
+
+import ert
+
+
+class Distribution:
+    def __init__(
+        self,
+        *,
+        size: Optional[int],
+        index: Optional[ert.data.RecordIndex],
+        rvs: Callable[[int], np.ndarray],  # type: ignore
+        ppf: Callable[[np.ndarray], np.ndarray]  # type: ignore
+    ) -> None:
+        if size is None and index is None:
+            raise ValueError("Cannot create distribution with neither size nor index")
+        if size is not None and index is not None:
+            raise ValueError("Cannot create distribution with both size and index")
+
+        self._index: ert.data.RecordIndex = ()
+
+        if size is not None:
+            self._size = size
+            self._as_array = True
+            self._index = tuple(range(self._size))
+        elif index is not None:
+            self._size = len(tuple(index))
+            self._as_array = False
+            self._index = cast(ert.data.RecordIndex, tuple(idx for idx in index))
+
+        self._raw_rvs = rvs
+        self._raw_ppf = ppf
+
+    @property
+    def size(self) -> int:
+        return self._size
+
+    @property
+    def index(self) -> ert.data.RecordIndex:
+        return self._index
+
+    def _to_record(self, x: np.ndarray) -> ert.data.Record:  # type: ignore
+        if self._as_array:
+            return ert.data.NumericalRecord(data=x.tolist())
+        else:
+            return ert.data.NumericalRecord(
+                data={
+                    idx: float(val) for idx, val in zip(self.index, x)  # type: ignore
+                }
+            )
+
+    def sample(self) -> ert.data.Record:
+        return self._to_record(self._raw_rvs(self._size))
+
+    def ppf(self, x: float) -> ert.data.Record:
+        x_array = np.full(self._size, x)
+        result = self._raw_ppf(x_array)
+        return self._to_record(result)
+
+
+class Gaussian(Distribution):
+    def __init__(
+        self,
+        mean: float,
+        std: float,
+        *,
+        size: Optional[int] = None,
+        index: Optional[ert.data.RecordIndex] = None
+    ) -> None:
+        self._mean = mean
+        self._std = std
+
+        def rvs(size: int) -> np.ndarray:  # type: ignore
+            return np.array(
+                scipy.stats.norm.rvs(loc=self._mean, scale=self._std, size=size)
+            )
+
+        def ppf(x: np.ndarray) -> np.ndarray:  # type: ignore
+            return np.array(scipy.stats.norm.ppf(x, loc=self._mean, scale=self._std))
+
+        super().__init__(
+            size=size,
+            index=index,
+            rvs=rvs,
+            ppf=ppf,
+        )
+
+    @property
+    def mean(self) -> float:
+        return self._mean
+
+    @property
+    def std(self) -> float:
+        return self._std
+
+    @property
+    def type(self) -> str:
+        return "gaussian"
+
+
+class Uniform(Distribution):
+    def __init__(
+        self,
+        lower_bound: float,
+        upper_bound: float,
+        *,
+        size: Optional[int] = None,
+        index: Optional[ert.data.RecordIndex] = None
+    ) -> None:
+        self._lower_bound = lower_bound
+        self._upper_bound = upper_bound
+        self._scale = upper_bound - lower_bound
+
+        def rvs(size: int) -> np.ndarray:  # type: ignore
+            return np.array(
+                scipy.stats.uniform.rvs(
+                    loc=self._lower_bound, scale=self._scale, size=self._size
+                )
+            )
+
+        def ppf(x: np.ndarray) -> np.ndarray:  # type: ignore
+            return np.array(
+                scipy.stats.uniform.ppf(x, loc=self._lower_bound, scale=self._scale)
+            )
+
+        super().__init__(
+            size=size,
+            index=index,
+            rvs=rvs,
+            ppf=ppf,
+        )
+
+    @property
+    def lower_bound(self) -> float:
+        return self._lower_bound
+
+    @property
+    def upper_bound(self) -> float:
+        return self._upper_bound
+
+    @property
+    def type(self) -> str:
+        return "uniform"
