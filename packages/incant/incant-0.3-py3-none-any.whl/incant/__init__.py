20fa09dda7d552999ae10402befb49c93c61f36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:28 +0000

    Add package incant/0.3/incant-0.3-py3-none-any.whl

---
+++ packages/incant/incant-0.3-py3-none-any.whl/incant/__init__.py
@@ -0,0 +1,330 @@
+from functools import lru_cache
+from inspect import Parameter, Signature, iscoroutinefunction, signature
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Sequence,
+    Set,
+    Tuple,
+    Type,
+    TypeVar,
+    Union,
+)
+
+from attr import Factory, define, field, frozen
+
+from ._codegen import (
+    LocalVarFactory,
+    ParameterDep,
+    compile_incant_wrapper,
+    compile_invoke,
+)
+
+
+_type = type
+
+
+R = TypeVar("R")
+
+
+@frozen
+class FactoryDep:
+    factory: Callable  # The fn to call.
+    arg_name: str  # The name of the param this is fulfulling.
+
+
+Dep = Union[FactoryDep, ParameterDep]
+
+
+PredicateFn = Callable[[Parameter], bool]
+
+
+def is_subclass(type, superclass) -> bool:
+    try:
+        return issubclass(type, superclass)
+    except Exception:
+        return False
+
+
+@frozen
+class Hook:
+    predicate: PredicateFn
+    factory: Optional[Callable[[Parameter], Callable]]
+
+    @classmethod
+    def for_name(cls, name: str, hook: Optional[Callable]):
+        return cls(lambda p: p.name == name, None if hook is None else (lambda _: hook))  # type: ignore
+
+    @classmethod
+    def for_type(cls, type: Any, hook: Optional[Callable]):
+        """Register by exact type (subclasses won't match)."""
+        return cls(
+            lambda p: p.annotation == type,
+            None if hook is None else lambda _: hook,  # type: ignore
+        )
+
+
+@define
+class Incanter:
+    hook_factory_registry: List[Hook] = Factory(list)
+    _invoke_cache: Callable = field(
+        init=False,
+        default=Factory(
+            lambda self: lru_cache(None)(self._gen_invoke), takes_self=True
+        ),
+    )
+    _incant_cache: Callable = field(
+        init=False,
+        default=Factory(
+            lambda self: lru_cache(None)(self._gen_incant), takes_self=True
+        ),
+    )
+
+    def prepare(
+        self,
+        fn: Callable[..., R],
+        hooks: Sequence[Hook] = (),
+        is_async: Optional[bool] = None,
+    ) -> Callable[..., R]:
+        return self._invoke_cache(fn, tuple(hooks), is_async)
+
+    def invoke(self, fn: Callable[..., R], *args, **kwargs) -> R:
+        return self.prepare(fn, is_async=False)(*args, **kwargs)
+
+    async def ainvoke(self, fn: Callable[..., Awaitable[R]], *args, **kwargs) -> R:
+        return await self.prepare(fn, is_async=True)(*args, **kwargs)
+
+    def incant(self, fn: Callable[..., R], *args, **kwargs) -> R:
+        """Invoke `fn` the best way we can."""
+        return self._incant(fn, args, kwargs)
+
+    async def aincant(self, fn: Callable[..., Awaitable[R]], *args, **kwargs) -> R:
+        """Invoke async `fn` the best way we can."""
+        return await self._incant(fn, args, kwargs, is_async=True)
+
+    def register_by_name(
+        self, fn: Optional[Callable] = None, *, name: Optional[str] = None
+    ):
+        """
+        Register a factory to be injected by name. Can also be used as a decorator.
+
+        If the name is not provided, the name of the factory will be used.
+        """
+        if fn is None:
+            # Decorator
+            return lambda fn: self.register_by_name(fn, name=name)
+
+        if name is None:
+            name = fn.__name__
+        self.register_hook(lambda p: p.name == name, fn)
+
+    def register_by_type(self, fn: Union[Callable, Type], type: Optional[Type] = None):
+        """
+        Register a factory to be injected by type. Can also be used as a decorator.
+
+        If the type is not provided, the return annotation from the
+        factory will be used.
+        """
+        if type is None:
+            if isinstance(fn, _type):
+                type_to_reg = fn
+            else:
+                sig = signature(fn)
+                type_to_reg = sig.return_annotation
+                if type_to_reg is Signature.empty:
+                    raise Exception("No return type found, provide a type.")
+        else:
+            type_to_reg = type
+        self.register_hook(lambda p: is_subclass(p.annotation, type_to_reg), fn)
+
+    def register_hook(self, predicate: PredicateFn, factory: Callable):
+        self.register_hook_factory(predicate, lambda _: factory)
+
+    def register_hook_factory(
+        self, predicate: PredicateFn, hook_factory: Callable[[Parameter], Callable]
+    ):
+        self.hook_factory_registry.insert(0, Hook(predicate, hook_factory))
+        self._invoke_cache.cache_clear()  # type: ignore
+        self._incant_cache.cache_clear()  # type: ignore
+
+    def _incant(
+        self,
+        fn: Callable,
+        args: Tuple[Any, ...],
+        kwargs: Dict[str, Any],
+        is_async: bool = False,
+    ):
+        """The shared entrypoint for ``incant`` and ``aincant``."""
+
+        pos_args_types = tuple([a.__class__ for a in args])
+        kwargs_by_name_and_type = frozenset(
+            [(k, v.__class__) for k, v in kwargs.items()]
+        )
+        wrapper = self._incant_cache(
+            fn, pos_args_types, kwargs_by_name_and_type, is_async
+        )
+
+        return wrapper(*args, **kwargs)
+
+    def _gen_incant_plan(
+        self, fn, pos_args_types: Tuple, kwargs: Set[Tuple[str, Any]], is_async=False
+    ) -> List[Union[int, str]]:
+        """Generate a plan to invoke `fn`, potentially using `args` and `kwargs`."""
+        pos_arg_plan: List[Union[int, str]] = []
+        pos_args_by_type = {a: ix for ix, a in enumerate(pos_args_types)}
+        kwarg_names = {kw[0] for kw in kwargs}
+        sig = signature(fn)
+        for arg_name, arg in sig.parameters.items():
+            if (
+                arg.annotation is not Signature.empty
+                and (arg_name, arg.annotation) in kwargs
+            ):
+                pos_arg_plan.append(arg_name)
+            elif (
+                arg.annotation is not Signature.empty
+                and arg.annotation in pos_args_by_type
+            ):
+                pos_arg_plan.append(pos_args_by_type[arg.annotation])
+            elif arg_name in kwarg_names:
+                pos_arg_plan.append(arg_name)
+            else:
+                raise TypeError(f"Cannot fulfil argument {arg_name}")
+        return pos_arg_plan
+
+    def _gen_incant(
+        self,
+        fn: Callable,
+        pos_args_types: Tuple,
+        kwargs_by_name_and_type: Set,
+        is_async: Optional[bool] = False,
+    ) -> Callable:
+        plan = self._gen_incant_plan(
+            fn, pos_args_types, kwargs_by_name_and_type, is_async=is_async
+        )
+        incant = compile_incant_wrapper(
+            fn, plan, len(pos_args_types), len(kwargs_by_name_and_type)
+        )
+        return incant
+
+    def _gen_dep_tree(
+        self, fn: Callable, additional_hooks: Sequence[Hook]
+    ) -> List[Tuple[Callable, List[Dep]]]:
+        """Generate the dependency tree for `fn`."""
+        to_process = [fn]
+        final_nodes: List[Tuple[Callable, List[Dep]]] = []
+        hooks = list(additional_hooks) + self.hook_factory_registry
+        while to_process:
+            _nodes = to_process
+            to_process = []
+            for node in _nodes:
+                sig = signature(node)
+                dependents: List[Union[ParameterDep, FactoryDep]] = []
+                for name, param in sig.parameters.items():
+                    if (
+                        node is not fn
+                        and param.default is not Signature.empty
+                        and param.kind is Parameter.KEYWORD_ONLY
+                    ):
+                        # Do not expose optional kw-only params of dependencies.
+                        continue
+                    param_type = param.annotation
+                    for hook in hooks:
+                        if hook.predicate(param):
+                            # Match!
+                            if hook.factory is None:
+                                dependents.append(
+                                    ParameterDep(name, param_type, param.default)
+                                )
+                            else:
+                                factory = hook.factory(param)
+                                to_process.append(factory)
+                                dependents.append(FactoryDep(factory, name))
+                            break
+                    else:
+                        dependents.append(ParameterDep(name, param_type, param.default))
+                final_nodes.insert(0, (node, dependents))
+        return final_nodes
+
+    def _gen_invoke(
+        self,
+        fn: Callable,
+        hooks: Tuple[Hook, ...] = (),
+        is_async: Optional[bool] = False,
+    ):
+        dep_tree = self._gen_dep_tree(fn, hooks)
+
+        local_vars: List[LocalVarFactory] = []
+
+        # is_async = None means autodetect
+        if is_async is None:
+            is_async = any(iscoroutinefunction(factory) for factory, _ in dep_tree)
+        # All non-parameter deps become local vars.
+        for factory, deps in dep_tree[:-1]:
+            if not is_async and iscoroutinefunction(factory):
+                raise Exception(
+                    f"The function would be a coroutine because of {factory}, use `ainvoke` instead"
+                )
+            local_vars.append(
+                LocalVarFactory(
+                    factory,
+                    [
+                        dep.factory if isinstance(dep, FactoryDep) else dep
+                        for dep in deps
+                    ],
+                )
+            )
+
+        outer_args = [
+            dep for node in dep_tree for dep in node[1] if isinstance(dep, ParameterDep)
+        ]
+        # We need to do a pass over the outer args to consolidate duplicates.
+        per_outer_arg: dict[str, List[ParameterDep]] = {}
+        for arg in outer_args:
+            per_outer_arg.setdefault(arg.arg_name, []).append(arg)
+
+        outer_args.clear()
+        for arg_name, args in per_outer_arg.items():
+            if len(args) == 1:
+                arg_type = args[0].type
+                arg_default = args[0].default
+            else:
+                # If there are multiple competing argument defs,
+                # we need to pick a winning type.
+                arg_type = Signature.empty
+                arg_default = Signature.empty
+                for arg in args:
+                    try:
+                        arg_type = _reconcile_types(arg_type, arg.type)
+                    except Exception:
+                        raise Exception(
+                            f"Unable to reconcile types {arg_type} and {arg.type} for argument {arg_name}"
+                        )
+                    if arg.default is not Signature.empty:
+                        arg_default = arg.default
+            outer_args.append(ParameterDep(arg_name, arg_type, arg_default))
+
+        # outer_args need to be sorted by the presence of a default value
+        outer_args.sort(key=lambda a: a.default is not Signature.empty)
+
+        fn_factory_args = []
+        for dep in dep_tree[-1][1]:
+            if isinstance(dep, FactoryDep):
+                fn_factory_args.append(dep.arg_name)
+
+        return compile_invoke(
+            fn, fn_factory_args, outer_args, local_vars, is_async=is_async
+        )
+
+
+def _reconcile_types(type_a, type_b):
+    if type_a is Signature.empty:
+        return type_b
+    if type_b is Signature.empty:
+        return type_a
+    if type_a is type_b:
+        return type_a
+    raise Exception(f"Unable to reconcile types {type_a!r} and {type_b!r}")
---
+++ packages/incant/incant-0.3-py3-none-any.whl/incant/_codegen.py
@@ -0,0 +1,181 @@
+import linecache
+
+from contextlib import AbstractAsyncContextManager
+from inspect import (
+    Signature,
+    isasyncgenfunction,
+    iscoroutinefunction,
+    signature,
+)
+from typing import Any, Callable, List, Union
+
+from attr import define
+
+
+def _is_async_context_manager(fn: Any) -> bool:
+    return (
+        fn.__class__ is type and issubclass(fn, AbstractAsyncContextManager)
+    ) or bool(
+        (wrapped := getattr(fn, "__wrapped__", None)) and isasyncgenfunction(wrapped)
+    )
+
+
+@define
+class ParameterDep:
+    arg_name: str
+    type: Any
+    default: Any = Signature.empty
+
+
+@define
+class LocalVarFactory:
+    factory: Callable
+    args: List[Union[Callable, ParameterDep]]
+
+
+def compile_invoke(
+    fn: Callable,
+    fn_factory_args: List[str],
+    outer_args: List[ParameterDep],
+    local_vars: List[LocalVarFactory],
+    is_async: bool = False,
+) -> Callable:
+    # Some arguments need to be taken from outside.
+    # Some arguments need to be calculated from factories.
+    sig = signature(fn)
+    fn_name = f"invoke_{fn.__name__}" if fn.__name__ != "<lambda>" else "invoke_lambda"
+    globs = {"_incant_inner_fn": fn}
+    arg_lines = []
+    for dep in outer_args:
+        if dep.type is not Signature.empty:
+            arg_type_snippet = f": _incant_arg_{dep.arg_name}"
+            globs[f"_incant_arg_{dep.arg_name}"] = dep.type
+        else:
+            arg_type_snippet = ""
+        if dep.default is not Signature.empty:
+            arg_default = f"_incant_default_{dep.arg_name}"
+            arg_type_snippet = f"{arg_type_snippet} = {arg_default}"
+            globs[arg_default] = dep.default
+
+        arg_lines.append(f"{dep.arg_name}{arg_type_snippet}")
+    outer_arg_names = {o.arg_name for o in outer_args}
+
+    lines = []
+
+    ret_type = ""
+    if sig.return_annotation is not Signature.empty:
+        globs["_incant_return_type"] = sig.return_annotation
+        ret_type = " -> _incant_return_type"
+    if is_async:
+        lines.append(f"async def {fn_name}({', '.join(arg_lines)}){ret_type}:")
+    else:
+        lines.append(f"def {fn_name}({', '.join(arg_lines)}){ret_type}:")
+    local_vars_ix_by_factory = {
+        local_var.factory: ix for ix, local_var in enumerate(local_vars)
+    }
+    ind = 0  # Indentation level
+    for i, local_var in enumerate(local_vars):
+        local_name = f"_incant_local_{i}"
+        local_var_factory = f"_incant_local_factory_{i}"
+        globs[local_var_factory] = local_var.factory
+        local_arg_lines = []
+        for local_arg in local_var.args:
+            if isinstance(local_arg, ParameterDep):
+                local_arg_lines.append(local_arg.arg_name)
+            else:
+                local_arg_lines.append(
+                    f"_incant_local_{local_vars_ix_by_factory[local_arg]}"
+                )
+        if _is_async_context_manager(local_var.factory):
+            lines.append(
+                f"  {' ' * ind}async with {local_var_factory}({', '.join(local_arg_lines)}) as {local_name}:"
+            )
+            ind += 2
+        else:
+            aw = ""
+            if iscoroutinefunction(local_var.factory):
+                aw = "await "
+            lines.append(
+                f"  {' ' * ind}{local_name} = {aw}{local_var_factory}({', '.join(local_arg_lines)})"
+            )
+
+    incant_arg_lines = []
+    local_var_ix = len(local_vars) - 1
+    for name in sig.parameters:
+        if name not in fn_factory_args and name in outer_arg_names:
+            incant_arg_lines.append(name)
+        else:
+            incant_arg_lines.append(f"_incant_local_{local_var_ix}")
+            local_var_ix -= 1
+
+    aw = ""
+    if iscoroutinefunction(fn):
+        aw = "await "
+    lines.append(
+        f"  {' ' * ind}return {aw}_incant_inner_fn({', '.join(incant_arg_lines)})"
+    )
+
+    script = "\n".join(lines)
+
+    fname = _generate_unique_filename(fn.__name__, "invoke", lines)
+    eval(compile(script, fname, "exec"), globs)
+
+    fn = globs[fn_name]
+    return fn
+
+
+def compile_incant_wrapper(
+    fn: Callable, incant_plan: List[Union[int, str]], num_pos_args: int, num_kwargs: int
+):
+    fn_name = f"incant_{fn.__name__}" if fn.__name__ != "<lambda>" else "incant_lambda"
+    globs = {"_incant_inner_fn": fn}
+    arg_lines = []
+    if incant_plan:
+        if num_pos_args:
+            arg_lines.append("*args")
+        else:
+            arg_lines.append("*")
+
+    kwargs = [arg for arg in incant_plan if isinstance(arg, str)]
+    arg_lines.extend(kwargs)
+    if num_kwargs > len(kwargs):
+        arg_lines.append("**kwargs")
+
+    lines = []
+    lines.append(f"def {fn_name}({', '.join(arg_lines)}):")
+    lines.append("  return _incant_inner_fn(")
+    for arg in incant_plan:
+        if isinstance(arg, int):
+            lines.append(f"    args[{arg}],")
+        else:
+            lines.append(f"    {arg},")
+    lines.append("  )")
+
+    script = "\n".join(lines)
+
+    fname = _generate_unique_filename(fn.__name__, "incant", lines)
+    eval(compile(script, fname, "exec"), globs)
+
+    fn = globs[fn_name]
+    return fn
+
+
+def _generate_unique_filename(func_name: str, func_type: str, source: List[str]):
+    """
+    Create a "filename" suitable for a function being generated.
+    """
+    extra = ""
+    count = 1
+
+    while True:
+        unique_filename = f"<incant generated {func_type} of {func_name}{extra}>"
+        # To handle concurrency we essentially "reserve" our spot in
+        # the linecache with a dummy line.  The caller can then
+        # set this value correctly.
+        cache_line = (len(source), None, source, unique_filename)
+        if linecache.cache.setdefault(unique_filename, cache_line) == cache_line:  # type: ignore
+            return unique_filename
+
+        # Looks like this spot is taken. Try again.
+        count += 1
+        extra = f"-{count}"
