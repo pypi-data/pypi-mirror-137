a9fa2e2f3cd08ed234353e456d4e5bfd5b677187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:52 +0000

    Add package ahegao/1.0/ahegao-1.0-py3-none-any.whl

---
+++ packages/ahegao/ahegao-1.0-py3-none-any.whl/ahegao/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'ahegao'
+__description__ = 'Ahegao API wrapper'
+__url__ = 'https://github.com/AhegaoTeam/AhegaoAPI'
+__version__ = '1.0'
+__build__ = 10
+__author__ = 'SantaSpeen'
+__author_email__ = 'admin@ahegao.ovh'
+__license__ = "MIT"
+__copyright__ = 'Copyright 2022 Ahegao Team'
