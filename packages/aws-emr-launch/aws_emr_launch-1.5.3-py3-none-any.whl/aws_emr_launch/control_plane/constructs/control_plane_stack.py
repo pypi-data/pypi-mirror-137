e4ba4d99a3c29dcfb2b201af249503b6d4945c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:42 +0000

    Add package aws-emr-launch/1.5.3/aws_emr_launch-1.5.3-py3-none-any.whl

---
+++ packages/aws-emr-launch/aws_emr_launch-1.5.3-py3-none-any.whl/aws_emr_launch/control_plane/constructs/control_plane_stack.py
@@ -0,0 +1,18 @@
+from typing import Any
+
+from aws_cdk import core
+
+from aws_emr_launch import __product__, __version__
+from aws_emr_launch.control_plane.constructs.lambdas import apis
+
+
+class ControlPlaneStack(core.Stack):
+    def __init__(self, app: core.App, name: str = "aws-emr-launch-control-plane", **kwargs: Any) -> None:
+        super().__init__(app, name, **kwargs)
+        self.tags.set_tag("deployment:product:name", __product__)
+        self.tags.set_tag("deployment:product:version", __version__)
+        self._apis = apis.Apis(self, "Apis")
+
+    @property
+    def apis(self) -> apis.Apis:
+        return self._apis
