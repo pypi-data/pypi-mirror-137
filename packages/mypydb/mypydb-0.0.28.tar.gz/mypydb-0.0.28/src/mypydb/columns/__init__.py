17c7c7ca2bb3abc28c79034d66d3d30d863ce220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:06 +0000

    Add package mypydb/0.0.28/mypydb-0.0.28.tar.gz

---
+++ packages/mypydb/mypydb-0.0.28.tar.gz/mypydb-0.0.28/src/mypydb/columns/__init__.py
@@ -0,0 +1,6 @@
+from .column import Column
+from .double_column import DoubleColumn
+from .id_column import IdColumn
+from .int_column import IntColumn
+from .text_column import TextColumn
+from .varchar_column import VarcharColumn
