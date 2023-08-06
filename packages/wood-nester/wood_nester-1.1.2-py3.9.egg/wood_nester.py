2b50a63801d3e62e105c48cd2d54166f76e766cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:16 +0000

    Add package wood-nester/1.1.2/wood_nester-1.1.2-py3.9.egg

---
+++ packages/wood-nester/wood_nester-1.1.2-py3.9.egg/wood_nester.py
@@ -0,0 +1,16 @@
+""" print_lol receive list and show all elements
+"""
+def print_lol(the_list, level=0):
+    """ we use loop for list
+"""
+#U can use single line comment also
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,level + 1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(each_item)
+        
+
+        
