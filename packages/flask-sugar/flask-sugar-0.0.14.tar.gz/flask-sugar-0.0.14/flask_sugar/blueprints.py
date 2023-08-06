fbe5274024b74fe68ee4b590ddc2e9c3f8ee92f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:44 +0000

    Add package flask-sugar/0.0.14/flask-sugar-0.0.14.tar.gz

---
+++ packages/flask-sugar/flask-sugar-0.0.14.tar.gz/flask-sugar-0.0.14/flask_sugar/blueprints.py
@@ -0,0 +1,106 @@
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Type, Union
+
+from flask import Blueprint as _Blueprint
+from pydantic import BaseModel
+
+from flask_sugar.typing import MethodTyping
+
+if TYPE_CHECKING:
+    from pydantic.typing import AbstractSetIntStr, MappingIntStrAny
+
+
+class Blueprint(MethodTyping, _Blueprint):
+    def __init__(
+        self,
+        name: str,
+        import_name: str,
+        static_folder: Optional[str] = None,
+        static_url_path: Optional[str] = None,
+        template_folder: Optional[str] = None,
+        url_prefix: Optional[str] = None,
+        subdomain: Optional[str] = None,
+        url_defaults: Optional[dict] = None,
+        root_path: Optional[str] = None,
+        cli_group: Optional[str] = None,
+        tags: Optional[List[str]] = None,
+        deprecated: Optional[bool] = None,
+    ):
+        super().__init__(
+            name,
+            import_name,
+            static_folder=static_folder,
+            static_url_path=static_url_path,
+            template_folder=template_folder,
+            url_prefix=url_prefix,
+            subdomain=subdomain,
+            url_defaults=url_defaults,
+            root_path=root_path,
+            cli_group=cli_group,
+        )
+        self.tags: List[str] = [name] if tags is None else tags
+        self.deprecated = deprecated
+
+    def add_url_rule(
+        self,
+        rule: str,
+        endpoint: Optional[str] = None,
+        view_func: Optional[Callable] = None,
+        provide_automatic_options: Optional[bool] = None,
+        doc_enable: bool = True,
+        tags: Optional[List[str]] = None,
+        summary: Optional[str] = None,
+        description: Optional[str] = None,
+        response_model: Optional[Type[BaseModel]] = None,
+        response_description: str = "success",
+        responses: Optional[Dict[Union[int, str], Dict[str, Any]]] = None,
+        deprecated: Optional[bool] = None,
+        operation_id: Optional[str] = None,
+        security: Optional[List[Dict[str, Any]]] = None,
+        extra: Optional[Dict[str, Any]] = None,
+        response_model_include: Union["AbstractSetIntStr", "MappingIntStrAny"] = None,
+        response_model_exclude: Union["AbstractSetIntStr", "MappingIntStrAny"] = None,
+        response_model_by_alias: bool = True,
+        response_model_exclude_unset: bool = False,
+        response_model_exclude_defaults: bool = False,
+        response_model_exclude_none: bool = False,
+        **options: Any,
+    ) -> None:
+        """Like :meth:`Flask.add_url_rule` but for a blueprint.  The endpoint for
+        the :func:`url_for` function is prefixed with the name of the blueprint.
+        """
+        if endpoint and "." in endpoint:
+            raise ValueError("'endpoint' may not contain a dot '.' character.")
+
+        if view_func and hasattr(view_func, "__name__") and "." in view_func.__name__:
+            raise ValueError("'view_func' name may not contain a dot '.' character.")
+
+        current_tags = self.tags.copy()
+        if tags:
+            current_tags.extend(tags)
+
+        self.record(
+            lambda s: s.add_url_rule(
+                rule,
+                endpoint,
+                view_func,
+                provide_automatic_options=provide_automatic_options,
+                doc_enable=doc_enable,
+                tags=current_tags,
+                summary=summary,
+                description=description,
+                response_model=response_model,
+                response_description=response_description,
+                responses=responses,
+                deprecated=deprecated or self.deprecated,
+                operation_id=operation_id,
+                security=security,
+                extra=extra,
+                response_model_include=response_model_include,
+                response_model_exclude=response_model_exclude,
+                response_model_by_alias=response_model_by_alias,
+                response_model_exclude_unset=response_model_exclude_unset,
+                response_model_exclude_defaults=response_model_exclude_defaults,
+                response_model_exclude_none=response_model_exclude_none,
+                **options,
+            )
+        )
