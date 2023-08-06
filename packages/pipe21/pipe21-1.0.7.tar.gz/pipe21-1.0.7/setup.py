4b4167c043ad1a437987587377404755b990d26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:11 +0000

    Add package pipe21/1.0.7/pipe21-1.0.7.tar.gz

---
+++ packages/pipe21/pipe21-1.0.7.tar.gz/pipe21-1.0.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pipe21',
+    version='1.0.7',
+    description='simple functional pipes',
+    long_description_content_type='text/markdown',
+    url='https://github.com/tandav/pipe21',
+    py_modules=['pipe21'],
+    include_package_data=True,
+)
