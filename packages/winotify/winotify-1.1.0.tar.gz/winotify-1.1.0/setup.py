b8c30ab4f1778b34acf43cd23c67fc0bfda138c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:22 +0000

    Add package winotify/1.1.0/winotify-1.1.0.tar.gz

---
+++ packages/winotify/winotify-1.1.0.tar.gz/winotify-1.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='winotify',
+    version='1.1.0',
+    packages=["winotify"],
+    url='https://github.com/versa-syahptr/winotify',
+    license='MIT',
+    author='versa',
+    author_email='versa1220@gmail.com',
+    description='Show notification toast on Windows 10',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    entry_points={
+        'console_scripts': ['winotify=winotify.__main__:main'],
+        'gui_scripts': ['winotify-nc=winotify.__main__:main']
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
