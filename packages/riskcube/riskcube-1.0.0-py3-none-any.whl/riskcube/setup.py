ec8120f9e8f0094d9ed5001673a1c600d2eda893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:14 +0000

    Add package riskcube/1.0.0/riskcube-1.0.0-py3-none-any.whl

---
+++ packages/riskcube/riskcube-1.0.0-py3-none-any.whl/riskcube/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright: giordano.ch AG
+
+import setuptools
+
+setuptools.setup(
+    name="RiskCube",
+    version="0.0.1",
+    author="Andreas Meer (giordano.ch)",
+    author_email="andreas.meer@giordano.ch",
+    description="A small example package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
