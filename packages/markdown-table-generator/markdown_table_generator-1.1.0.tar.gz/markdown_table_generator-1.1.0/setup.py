d44bb4e854217cd7679472555ab43ba3596b89e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:27 +0000

    Add package markdown-table-generator/1.1.0/markdown_table_generator-1.1.0.tar.gz

---
+++ packages/markdown-table-generator/markdown_table_generator-1.1.0.tar.gz/markdown_table_generator-1.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="markdown_table_generator",
+    version="1.1.0",
+    author="Guillaume Leroy",
+    author_email="pro.guillaume.leroy@gmail.com",
+    description="Library to generate Markdown table",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leroyguillaume/py-markdown-table-generator",
+    packages=["markdown_table_generator"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["typing"],
+    entry_points={
+        "console_scripts": [
+            "csv-to-md = markdown_table_generator:csv_to_md"
+        ]
+    }
+)
