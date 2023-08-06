6c41ef76aac9853c246ee45ef8cc8ba6c82a23d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:43 +0000

    Add package xnote/1.1a0/xnote-1.1a0.tar.gz

---
+++ packages/xnote/xnote-1.1a0.tar.gz/xnote-1.1a0/README.md
@@ -0,0 +1,6 @@
+# xnote-cli
+Easily keep track of your notes on your favorite terminal emulator!
+
+# Installation:
+
+``pip3 install xnote``
---
+++ packages/xnote/xnote-1.1a0.tar.gz/xnote-1.1a0/xnote/__main__.py
@@ -0,0 +1,10 @@
+from xnote.core.cli import xnote
+
+import argparse
+
+def main():
+    xnote.start()
+
+
+if __name__ == '__main__':
+    main()
