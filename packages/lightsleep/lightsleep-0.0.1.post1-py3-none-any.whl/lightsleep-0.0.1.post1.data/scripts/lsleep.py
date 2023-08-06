c39863de70763cde22b47d4ab2e648930b310b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:54 +0000

    Add package lightsleep/0.0.1.post1/lightsleep-0.0.1.post1-py3-none-any.whl

---
+++ packages/lightsleep/lightsleep-0.0.1.post1-py3-none-any.whl/lightsleep-0.0.1.post1.data/scripts/lsleep.py
@@ -0,0 +1,30 @@
+#!python
+
+import argparse
+from lightsleep import Sleep
+
+def get_args():
+
+    hooks = Sleep.hooks()
+
+    epilog = 'HOOKS\n=====\n\n'
+
+    for name, args in hooks.items():
+        epilog += f'--hook {name}\n'
+        for arg, default in args.items():
+            epilog += f'    {arg}={default}\n'
+        epilog += '\n'
+
+    parser = argparse.ArgumentParser(description='Sleep which can awake', epilog=epilog,  formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('seconds', type=int)
+    parser.add_argument('--hook', nargs='+', metavar=('METHOD', 'ARG'))
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+
+    s = Sleep(hook=args.hook)
+    s.sleep(args.seconds)
+
+if __name__ == '__main__':
+    main()
