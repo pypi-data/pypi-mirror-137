0029d63e162b8f7b9cab16acd5c38f67813a4679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:40 +0000

    Add package brainome/1.8.120/brainome-1.8.120-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/brainome/brainome-1.8.120-cp39-cp39-macosx_10_15_x86_64.whl/brainome.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# Brainome Daimensions(tm)
+#
+# The Brainome Table Compiler starter
+#
+# Copyright (c) 2019 - 2021 Brainome Incorporated. All Rights Reserved.
+# BSD license, all text above must be included in any redistribution.     
+# See LICENSE.TXT for more information.
+#
+# This program may use Brainome's servers for cloud computing. Server use
+# is subject to separate license agreement. 
+#
+# Contact: support@brainome.ai                                              
+# for questions and suggestions. 
+#
+
+import sys
+import btc
+btc.main(sys.argv[1:])
