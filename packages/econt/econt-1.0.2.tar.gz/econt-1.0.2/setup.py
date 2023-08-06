ad8accca5fb9ffa5bd040e8c0287c15673fa705e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:05 +0000

    Add package econt/1.0.2/econt-1.0.2.tar.gz

---
+++ packages/econt/econt-1.0.2.tar.gz/econt-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="econt",
+    version="1.0.2",
+    author="Dmitry Kalinin",
+    packages=["econt"],
+    url="https://github.com/lesthetique-store/econt",
+    install_requires=["requests", "dicttoxml", "xmltodict", "nested_lookup"],
+)
