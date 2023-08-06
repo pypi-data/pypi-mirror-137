c4b581cb31de0b2ae8a17af77ea661ea4b096da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:00 +0000

    Add package rtrv-my-chat-server/1.0.2/rtrv_my_chat_server-1.0.2.tar.gz

---
+++ packages/rtrv-my-chat-server/rtrv_my_chat_server-1.0.2.tar.gz/rtrv_my_chat_server-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="rtrv_my_chat_server",
+      version="1.0.2",
+      description="my_chat_server",
+      author="RTretyakov",
+      author_email="rtretyakov@outlook.com",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
