e21b94f1cd889bd1be2606813cd2430814043d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:34:15 +0000

    Add package design.plone.contenttypes/4.0.5/design.plone.contenttypes-4.0.5.tar.gz

---
+++ packages/design.plone.contenttypes/design.plone.contenttypes-4.0.5.tar.gz/design.plone.contenttypes-4.0.5/src/design/plone/contenttypes/indexers/common.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from plone.indexer.decorator import indexer
+from plone.dexterity.interfaces import IDexterityContainer
+from plone.dexterity.interfaces import IDexterityItem
+
+
+@indexer(IDexterityContainer)
+@indexer(IDexterityItem)
+def tassonomia_argomenti(context, **kw):
+    return [
+        x.to_object.Title()
+        for x in getattr(context.aq_base, "tassonomia_argomenti", [])
+        if x.to_object
+    ]
+
+
+@indexer(IDexterityContainer)
+@indexer(IDexterityItem)
+def ufficio_responsabile(context, **kw):
+    uffici = getattr(context.aq_base, "ufficio_responsabile", [])
+    return [ufficio.UID() for ufficio in filter(bool, [x.to_object for x in uffici])]
+
+
+@indexer(IDexterityContainer)
+@indexer(IDexterityItem)
+def parent(context):
+    obj_parent = context.aq_parent
+    return {
+        "title": obj_parent.Title(),
+        "UID": obj_parent.UID(),
+        "@id": obj_parent.absolute_url(),
+    }
