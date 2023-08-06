342aa804a0eb1329426e181714c378b9e5884eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:21 +0000

    Add package logging-easy/1.6/logging_easy-1.6.tar.gz

---
+++ packages/logging-easy/logging_easy-1.6.tar.gz/logging_easy-1.6/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="logging_easy",
+    version="1.6",
+    description="Easy logging hard variables to terminal, like in JavaScript!",
+    packages=['logging_easy'],
+    author_email="qwer.009771@gmail.com")
