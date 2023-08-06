8a8654793fe6a18aa910164e78f9dde324f7a585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:24 +0000

    Add package holograph/0.0.1/holograph-0.0.1.tar.gz

---
+++ packages/holograph/holograph-0.0.1.tar.gz/holograph-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="holograph",
+    version="0.0.1",
+    # Note, we could possibly lower this but haven't yet tested older versions and haven't yet been asked to :)
+    python_requires=">=3.6",
+    author="Transform Data",
+    author_email="marco@transformdata.io",
+    description=("Transform holograph Library"),
+    long_description="",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    # Package details
+    packages=[],
+)
