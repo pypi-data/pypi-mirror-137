55f94d5f713f777987a417236902e7e57bc3e4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:59 +0000

    Add package kfputils/0.4.4/kfputils-0.4.4-py32-none-any.whl

---
+++ packages/kfputils/kfputils-0.4.4-py32-none-any.whl/kfputils/__init__.py
@@ -0,0 +1,3 @@
+from .client import get_kfp_client_inside_cluster  # noqa: F401
+from .uploadrun import UploaderRunner  # noqa: F401
+from .run import _display_run, _print_runs, _wait_for_run_completion  # noqa: F401
