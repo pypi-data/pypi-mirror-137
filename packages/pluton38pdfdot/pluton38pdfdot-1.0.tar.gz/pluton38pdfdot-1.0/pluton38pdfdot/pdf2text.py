50b1283a27fb8ffe293c914e95465470eb149b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:31 +0000

    Add package pluton38pdfdot/1.0/pluton38pdfdot-1.0.tar.gz

---
+++ packages/pluton38pdfdot/pluton38pdfdot-1.0.tar.gz/pluton38pdfdot-1.0/pluton38pdfdot/pdf2text.py
@@ -0,0 +1,14 @@
+""" This module provides functions to convert a pdf to text"""
+
+
+def convert():
+    """
+    Convert the given PDF to text.
+
+    Parameters:
+    path (str): The path to a PDF file.
+
+    Returns:
+    str: The content of the PDF file as text.
+    """
+    print("pdf2text")
