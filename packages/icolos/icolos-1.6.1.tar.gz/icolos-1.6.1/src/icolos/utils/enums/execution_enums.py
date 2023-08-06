a17aa9e0c332ca0b45fb16a660ff881273cc56e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:52 +0000

    Add package icolos/1.6.1/icolos-1.6.1.tar.gz

---
+++ packages/icolos/icolos-1.6.1.tar.gz/icolos-1.6.1/src/icolos/utils/enums/execution_enums.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class ExecutionPlatformEnum(str, Enum):
+    LOCAL = "local"
+    SLURM = "slurm"
+    PARTITION = "partition"
+    TIME = "time"
+    GRES = "gres"
+    MEM = "mem"
+    CORES = "cores"
+    CORE = "core"
+    GPU = "gpu"
