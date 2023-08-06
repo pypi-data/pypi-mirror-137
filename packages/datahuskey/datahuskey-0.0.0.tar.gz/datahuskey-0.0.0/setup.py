902fffc4d2a43461c6d2356a0ea28bdebafb142c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:19 +0000

    Add package datahuskey/0.0.0/datahuskey-0.0.0.tar.gz

---
+++ packages/datahuskey/datahuskey-0.0.0.tar.gz/datahuskey-0.0.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+import sys
+
+setup(
+      name='datahuskey',
+      version='0.0.0',
+      author='Walker Lab',
+      author_email='eywalker@uw.edu',
+      url='https://github.com/walkerlab/datahuskey',
+      packages=find_packages(),
+      )
