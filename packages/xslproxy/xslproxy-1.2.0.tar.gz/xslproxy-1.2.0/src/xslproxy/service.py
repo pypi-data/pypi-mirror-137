1f6466a0b3474cf60329bcda7fa36221fb1cdad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:16 +0000

    Add package xslproxy/1.2.0/xslproxy-1.2.0.tar.gz

---
+++ packages/xslproxy/xslproxy-1.2.0.tar.gz/xslproxy-1.2.0/src/xslproxy/service.py
@@ -0,0 +1,43 @@
+from urllib.parse import urlsplit
+
+from twisted.application import strports
+from twisted.internet import endpoints, reactor
+from twisted.python import usage
+from twisted.web import resource
+
+from xslproxy.server import Site
+from xslproxy.xsl import XslRepository, XslTransformationReverseProxyResource
+
+
+class Options(usage.Options):
+    optParameters = [
+        ["backend", "-b", "http://localhost",
+            "Url to backend, no trailing slash"],
+        ["listen", "-l", "tcp:8080",
+            "Listen port (strports syntax)"],
+        ["path", "-p",
+            "A directory with xsl files"],
+    ]
+
+
+def makeService(options):
+    parts = urlsplit(options["backend"])
+    host = parts.hostname
+    path = parts.path.encode("utf-8")
+
+    if parts.scheme == "http":
+        port = int(parts.port) if parts.port is not None else 80
+        backend_strports = f"tcp:{host}:{port}"
+    else:
+        port = int(parts.port) if parts.port is not None else 443
+        backend_strports = f"ssl:{host}:{port}"
+
+    hostport = (f"{host}:{port}" if parts.port else f"{host}").encode("ascii")
+
+    backend = endpoints.clientFromString(reactor, backend_strports)
+
+    repository = XslRepository(options["path"])
+    root = resource.Resource()
+    root.putChild(b"transform", XslTransformationReverseProxyResource(
+        repository, backend, hostport, path))
+    return strports.service(options["listen"], Site(root))
