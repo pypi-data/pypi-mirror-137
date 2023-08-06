a7dff0e8e2f684253108c3f81373d79f71c942c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:43 +0000

    Add package yewtube/1.1.3/yewtube-1.1.3-py3-none-any.whl

---
+++ packages/yewtube/yewtube-1.1.3-py3-none-any.whl/mps_youtube/pafy.py
@@ -0,0 +1,56 @@
+from youtubesearchpython import VideosSearch, ChannelsSearch, PlaylistsSearch, Suggestions, Playlist
+import yt_dlp, random, os
+
+class MyLogger:
+    def debug(self, msg):
+        # For compatibility with youtube-dl, both debug and info are passed into debug
+        # You can distinguish them by the prefix '[debug] '
+        if msg.startswith('[debug] '):
+            pass
+        else:
+            self.info(msg)
+
+    def info(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def get_video_streams(ytid):
+    with yt_dlp.YoutubeDL({'logger':MyLogger()}) as ydl:
+        info_dict = ydl.extract_info(ytid, download=False)
+        return [i for i in info_dict['formats'] if i.get('format_note') != 'storyboard']
+
+def download_video(ytid, folder):
+    ytdl_format_options = {
+        'outtmpl': os.path.join(folder, '%(title)s-%(id)s.%(ext)s')
+    }
+
+    with yt_dlp.YoutubeDL(ytdl_format_options) as ydl:
+        ydl.download('https://www.youtube.com/watch?v=%s' % ytid)
+        return True
+
+def video_search(query):
+    videosSearch = VideosSearch(query, limit=50)
+    wdata = videosSearch.result()['result']
+    return wdata
+
+def channel_search(query):
+    channelsSearch = ChannelsSearch(query, limit=50, region='US')
+    return channelsSearch.result()['result']
+
+def playlist_search(query):
+    playlistsSearch = PlaylistsSearch(query, limit=50)
+    return playlistsSearch.result()['result']
+
+def get_playlist(playlist_id):
+    playlist = Playlist.get('https://www.youtube.com/playlist?list=%s' % playlist_id)
+    return playlist
+def get_video_title_suggestions(query):
+    suggestions = Suggestions(language = 'en', region = 'US')
+    related_searches = suggestions.get(query)['result']
+    return related_searches[random.randint(0,len(related_searches))]
---
+++ packages/yewtube/yewtube-1.1.3-py3-none-any.whl/mps_youtube/test.py
@@ -0,0 +1,8 @@
+from youtubesearchpython import Suggestions, ResultMode
+import random
+
+
+
+import pafy, json
+
+pafy.download_video('y7nzrBhyGPE', '/home/programmer/Downloads')
