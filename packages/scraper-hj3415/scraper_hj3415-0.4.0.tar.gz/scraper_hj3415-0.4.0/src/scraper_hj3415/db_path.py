242727937c400f2d65bbe53518132c3e88dd3a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:00 +0000

    Add package scraper-hj3415/0.4.0/scraper_hj3415-0.4.0.tar.gz

---
+++ packages/scraper-hj3415/scraper_hj3415-0.4.0.tar.gz/scraper_hj3415-0.4.0/src/scraper_hj3415/db_path.py
@@ -0,0 +1,6 @@
+MONGO_ADDR = 'mongodb://hj3415:piyrw421@hj3415.iptime.org:27017'
+
+IPTIME_ADDR = 'mongodb://hj3415:piyrw421@hj3415.iptime.org:27017'
+ATLAS_ADDR = "mongodb+srv://hj3415:piyrw421@nfs.2mnb4.mongodb.net/005930?retryWrites=true&w=majority"
+LOCAL_ADDR = 'mongodb://localhost:27017'
+INTERNAL_ADDR = 'mongodb://192.168.0.173:27017'
