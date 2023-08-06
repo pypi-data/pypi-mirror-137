94f1a9eb9049da73313d5a860da07287a0815495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:27:16 +0000

    Add package pysimconnect/0.1.0/pysimconnect-0.1.0.tar.gz

---
+++ packages/pysimconnect/pysimconnect-0.1.0.tar.gz/pysimconnect-0.1.0/simconnect/__init__.py
@@ -0,0 +1,4 @@
+from .scdefs import *
+from .sc import SimConnect, RECV_P
+from .receiver import Receiver, ReceiverInstance
+from .datadef import SimData, SimDataHandler, SIMVARS, EVENTS, UNITS, DataDefinition
