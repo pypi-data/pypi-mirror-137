13ea10b6a1c72b6d78f61e76daa4b1e47fbcd806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:27 +0000

    Add package pyclesperanto-prototype/0.14.0/pyclesperanto_prototype-0.14.0.tar.gz

---
+++ packages/pyclesperanto-prototype/pyclesperanto_prototype-0.14.0.tar.gz/pyclesperanto_prototype-0.14.0/pyclesperanto_prototype/_tier9/_imshow.py
@@ -0,0 +1,36 @@
+from .._tier0 import Image
+
+def imshow(image : Image, title : str = None, labels : bool = False, min_display_intensity : float = None, max_display_intensity : float = None, color_map = None, plot = None, colorbar:bool = False):
+    from .._tier0 import pull
+    from .._tier1 import maximum_z_projection
+
+    if len(image.shape) == 3:
+        image = maximum_z_projection(image)
+
+    image = pull(image)
+
+    cmap = color_map
+    if labels:
+        import matplotlib
+        import numpy as np
+
+        if not hasattr(imshow, "labels_cmap"):
+            from numpy.random import MT19937
+            from numpy.random import RandomState, SeedSequence
+            rs = RandomState(MT19937(SeedSequence(3)))
+            lut = rs.rand(65537, 3)
+            lut[0, :] = 0
+            imshow.labels_cmap = matplotlib.colors.ListedColormap(lut)
+        cmap = imshow.labels_cmap
+
+    if plot is None:
+        import matplotlib.pyplot as plt
+        plt.imshow(image, cmap=cmap, vmin=min_display_intensity, vmax=max_display_intensity, interpolation='nearest')
+        if colorbar:
+            plt.colorbar()
+        plt.show()
+    else:
+        plot.imshow(image, cmap=cmap, vmin=min_display_intensity, vmax=max_display_intensity, interpolation='nearest')
+        if colorbar:
+            plot.colorbar()
+
