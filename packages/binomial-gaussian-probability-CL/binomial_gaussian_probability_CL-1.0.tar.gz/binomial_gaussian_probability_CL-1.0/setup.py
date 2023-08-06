2ba6a3bf4fd365d0cad926e6d01026e39bb83f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:57 +0000

    Add package binomial-gaussian-probability-CL/1.0/binomial_gaussian_probability_CL-1.0.tar.gz

---
+++ packages/binomial-gaussian-probability-CL/binomial_gaussian_probability_CL-1.0.tar.gz/binomial_gaussian_probability_CL-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='binomial_gaussian_probability_CL',
+      version='1.0',
+      description='Binomial and Gaussian distributions',
+      packages=['binomial_gaussian_probability_CL'],
+      author = 'Charline LI',
+      author_email = 'lch129444@hotmail.com',
+      zip_safe=False)
