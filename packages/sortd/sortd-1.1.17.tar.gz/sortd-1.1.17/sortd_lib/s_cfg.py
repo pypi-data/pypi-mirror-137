9730ea249925d5fb93cfb8fe836fec79e83e3d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:22 +0000

    Add package sortd/1.1.17/sortd-1.1.17.tar.gz

---
+++ packages/sortd/sortd-1.1.17.tar.gz/sortd-1.1.17/sortd_lib/s_cfg.py
@@ -0,0 +1,21 @@
+from configparser import RawConfigParser, ParsingError
+from os import linesep
+from sys import stdin, stdout
+
+from .consts import ERROR
+from .lib import log, recur_sort
+
+
+def p_cfg() -> int:
+    parser = RawConfigParser(allow_no_value=True, interpolation=None)
+    try:
+        parser.read_file(stdin)
+    except ParsingError as e:
+        log.critical("%s", f"{ERROR}{linesep}{e}")
+        return 1
+    else:
+        cfg = recur_sort({**parser})
+        parser = RawConfigParser()
+        parser.read_dict(cfg)
+        parser.write(stdout)
+        return 0
