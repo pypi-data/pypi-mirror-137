9c2d243596dc740f9cb79c66292b8ba7f56e1d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:59 +0000

    Add package odoo12-addon-onaro/12.0.0.0.18/odoo12-addon-onaro-12.0.0.0.18.tar.gz

---
+++ packages/odoo12-addon-onaro/odoo12-addon-onaro-12.0.0.0.18.tar.gz/odoo12-addon-onaro-12.0.0.0.18/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    setup_requires=['setuptools-odoo'],
+    odoo_addon={
+        'depends_override': {
+            'auth_api_key': 'odoo12-addon-auth-api-key==12.0.2.1.0.99.dev2',
+            'base_rest': 'odoo12-addon-base-rest==12.0.3.0.6',
+            'crm_lead_product': 'odoo12-addon-crm-lead-product==12.0.1.0.0.99.dev12',
+            'telecom': 'odoo12-addon-telecom==12.0.0.0.9',
+        },
+        'external_dependencies_override': {
+            'python': {
+                'pycastiphone_client': 'pycastiphone-client==0.1.0',
+            },
+        },
+    },
+)
