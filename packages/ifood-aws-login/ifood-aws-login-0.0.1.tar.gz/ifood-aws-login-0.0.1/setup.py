3801b09ecca0e0531b745ddfc3ae1c0f0331c0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:12 +0000

    Add package ifood-aws-login/0.0.1/ifood-aws-login-0.0.1.tar.gz

---
+++ packages/ifood-aws-login/ifood-aws-login-0.0.1.tar.gz/ifood-aws-login-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+
+setup(name='ifood-aws-login',
+      version='0.0.1',
+      description='--',
+      author='pedro.aguiar',
+      license='MIT',
+      zip_safe=False)
