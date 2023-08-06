55fbcc64d18939c4cc4f647ef79cb63fc7452dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:47 +0000

    Add package easysciencecore/0.2.0/easyScienceCore-0.2.0.tar.gz

---
+++ packages/easysciencecore/easyScienceCore-0.2.0.tar.gz/easyScienceCore-0.2.0/easyCore/Utils/typing.py
@@ -0,0 +1,12 @@
+#  SPDX-FileCopyrightText: 2022 easyCore contributors  <core@easyscience.software>
+#  SPDX-License-Identifier: BSD-3-Clause
+#  © 2021-2022 Contributors to the easyCore project <https://github.com/easyScience/easyCore>
+
+__author__ = 'github.com/wardsimon'
+__version__ = '0.1.0'
+
+from easyCore import np
+from typing import Union, List
+
+noneType = type(None)
+Vector3Like = Union[List[float], np.ndarray]
