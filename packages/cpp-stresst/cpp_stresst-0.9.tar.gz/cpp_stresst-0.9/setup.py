62fc18854d32614f077c2dca53752140435f7d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:14 +0000

    Add package cpp-stresst/0.9/cpp_stresst-0.9.tar.gz

---
+++ packages/cpp-stresst/cpp_stresst-0.9.tar.gz/cpp_stresst-0.9/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+long_description = open("README.md").read()
+
+setup(
+    name='cpp_stresst',
+    version='0.9',
+    author='Ryan Chou',
+    author_email='',
+    url='https://ryanchou.dev',
+    description='Stress test your C++ solutions!',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'c_stresst = c_stresst.strs:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords='competitive-programming cpp c++ problems testing',
+    install_requires="",
+    zip_safe=False
+)
