e3f6e2cd19558fee36f53c1dfbb9fd004e408874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:53 +0000

    Add package RistoCalculator/1.0/RistoCalculator-1.0.tar.gz

---
+++ packages/RistoCalculator/RistoCalculator-1.0.tar.gz/RistoCalculator-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+name = 'RistoCalculator',
+version = '1.0',
+description='Makes Standard Calculations',
+author = 'Risto Tammistu',
+packages=find_packages(), # Installs all the requirements
+)
