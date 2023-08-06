92d79757d26d9e2303edb73cc18fcbef704b4539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:34 +0000

    Add package xpresso/0.11.2/xpresso-0.11.2.tar.gz

---
+++ packages/xpresso/xpresso-0.11.2.tar.gz/xpresso-0.11.2/xpresso/dependencies/models.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import sys
+import typing
+
+if sys.version_info < (3, 8):
+    from typing_extensions import Literal
+else:
+    from typing import Literal
+
+import di
+from di.api.providers import DependencyProvider
+
+T = typing.TypeVar("T")
+
+
+Scope = Literal["app", "connection", "endpoint"]
+
+
+class Dependant(di.Dependant[typing.Any]):
+    __slots__ = ()
+
+    def __init__(
+        self,
+        call: typing.Optional[DependencyProvider] = None,
+        scope: Scope = "connection",
+        use_cache: bool = True,
+        wire: bool = True,
+        sync_to_thread: bool = False,
+    ) -> None:
+        super().__init__(
+            call=call,
+            scope=scope,
+            use_cache=use_cache,
+            wire=wire,
+            sync_to_thread=sync_to_thread,
+        )
