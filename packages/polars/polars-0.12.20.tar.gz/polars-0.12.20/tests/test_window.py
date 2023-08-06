3b43d569953cf99431601839c91ac13e3cfefae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:24:29 +0000

    Add package polars/0.12.20/polars-0.12.20.tar.gz

---
+++ packages/polars/polars-0.12.20.tar.gz/polars-0.12.20/tests/test_window.py
@@ -0,0 +1,32 @@
+from typing import Type
+
+import numpy as np
+import pytest
+
+import polars as pl
+
+
+@pytest.mark.parametrize("dtype", [pl.Float32, pl.Float64, pl.Int32])
+def test_std(dtype: Type[pl.DataType]) -> None:
+    if dtype == pl.Int32:
+        df = pl.DataFrame(
+            [
+                pl.Series("groups", ["a", "a", "b", "b"]),
+                pl.Series("values", [1, 2, 3, 4], dtype=dtype),
+            ]
+        )
+    else:
+        df = pl.DataFrame(
+            [
+                pl.Series("groups", ["a", "a", "b", "b"]),
+                pl.Series("values", [1.0, 2.0, 3.0, 4.0], dtype=dtype),
+            ]
+        )
+
+    out = df.select(pl.col("values").std().over("groups"))
+    assert np.isclose(out["values"][0], 0.7071067690849304)
+
+    out = df.select(pl.col("values").var().over("groups"))
+    assert np.isclose(out["values"][0], 0.5)
+    out = df.select(pl.col("values").mean().over("groups"))
+    assert np.isclose(out["values"][0], 1.5)
