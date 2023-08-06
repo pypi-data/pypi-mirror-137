7a37ef5b059a0a46f6f5ad406f637db2bb4a7360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:10 +0000

    Add package complaintClassify/0.0.5/complaintClassify-0.0.5.tar.gz

---
+++ packages/complaintClassify/complaintClassify-0.0.5.tar.gz/complaintClassify-0.0.5/classification_model/pipeline.py
@@ -0,0 +1,21 @@
+from classification_model.preprocessing import preprocessing as pp
+from sklearn.pipeline import Pipeline
+
+from classification_model.config.core import config
+
+
+text_process_pipe = Pipeline([
+
+    ('text_cleanup', pp.textcleanup()),
+
+    ('text_tokenenize', pp.texttokenize()),
+
+    ('text_stopwordremove', pp.textstopwordremove()),
+
+    ('text_lemmatize', pp.textlemmatize()),
+
+    ('text_stemmer', pp.textstemmer()),
+
+    ('text_token', pp.texttokenize2(config.model_config.MAX_LENGHT)),
+
+])
