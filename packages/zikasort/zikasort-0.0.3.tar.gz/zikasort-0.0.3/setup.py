850d384e9bc7b273426c8387d2ce7509d850b4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:05 +0000

    Add package zikasort/0.0.3/zikasort-0.0.3.tar.gz

---
+++ packages/zikasort/zikasort-0.0.3.tar.gz/zikasort-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: End Users/Desktop',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: Apache Software License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='zikasort',
+  version='0.0.3',
+  description='Sorter for files.',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  scripts={'zikasort.py'},
+  url='',  
+  author='Zika Walter',
+  author_email='example@example.com',
+  license='APACHE', 
+  classifiers=classifiers,
+  keywords='sorter', 
+  packages=find_packages(),
+  install_requires=['']  
+)
