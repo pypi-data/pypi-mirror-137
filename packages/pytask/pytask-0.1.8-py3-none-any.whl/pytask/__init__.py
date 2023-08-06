5c3137b45e62ea47255daf39ba413189298f0b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:30 +0000

    Add package pytask/0.1.8/pytask-0.1.8-py3-none-any.whl

---
+++ packages/pytask/pytask-0.1.8-py3-none-any.whl/pytask/__init__.py
@@ -0,0 +1,12 @@
+"""This module contains the main namespace for pytask."""
+from __future__ import annotations
+
+from _pytask import __version__
+from _pytask.build import main
+from _pytask.cli import cli
+from _pytask.config import hookimpl
+from _pytask.graph import build_dag
+from _pytask.mark import MARK_GEN as mark  # noqa: N811
+
+
+__all__ = ["__version__", "build_dag", "cli", "hookimpl", "main", "mark"]
