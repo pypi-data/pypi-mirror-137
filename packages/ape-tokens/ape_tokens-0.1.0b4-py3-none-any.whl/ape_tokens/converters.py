7fcb2242d7bd455b71f3930020f5df52fcc4c80c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:54 +0000

    Add package ape-tokens/0.1.0b4/ape_tokens-0.1.0b4-py3-none-any.whl

---
+++ packages/ape-tokens/ape_tokens-0.1.0b4-py3-none-any.whl/ape_tokens/converters.py
@@ -0,0 +1,43 @@
+from decimal import Decimal
+from typing import Any
+
+from ape.api import ConverterAPI
+from ape.utils import cached_property
+from tokenlists import TokenListManager
+
+
+class TokenConversions(ConverterAPI):
+    """Converts token amounts like `100 LINK` to 1e18"""
+
+    @cached_property
+    def manager(self) -> TokenListManager:
+        return TokenListManager()
+
+    def is_convertible(self, value: Any) -> bool:
+        if not isinstance(value, str):
+            return False
+
+        if " " not in value or len(value.split(" ")) > 2:
+            return False
+
+        _, symbol = value.split(" ")
+
+        provider = self.networks.active_provider
+        if not provider:
+            raise Exception("Not connected to a provider!")
+
+        tokens = self.manager.get_tokens(chain_id=provider.network.chain_id)
+        token_map = map(lambda t: t.symbol, tokens)
+
+        return symbol in token_map
+
+    def convert(self, value: str) -> int:
+        value, symbol = value.split(" ")
+
+        provider = self.networks.active_provider
+        assert provider  # Really just to help mypy
+
+        assert self.manager  # Really just to help mypy
+        token = self.manager.get_token_info(symbol, chain_id=provider.network.chain_id)
+
+        return int(Decimal(value) * 10 ** token.decimals)
