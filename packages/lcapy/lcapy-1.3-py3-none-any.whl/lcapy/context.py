8ca71091becb9273b7c5256966d44c1de9c4a9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:00 +0000

    Add package lcapy/1.3/lcapy-1.3-py3-none-any.whl

---
+++ packages/lcapy/lcapy-1.3-py3-none-any.whl/lcapy/context.py
@@ -0,0 +1,18 @@
+"""This module provides the Context class that maintains the context
+for a Circuit.
+
+Copyright 2014--2021 Michael Hayes, UCECE
+
+"""
+
+from .attrdict import AttrDict
+
+class Context(object):
+
+    def __init__(self):
+        self.symbols = AttrDict()
+        self.user_symbols = {}
+        self.domain_symbols = {}        
+        self.assumptions = {}
+        # Noise instance identifier
+        self.nid = 0
---
+++ packages/lcapy/lcapy-1.3-py3-none-any.whl/lcapy/netlistmaker.py
@@ -0,0 +1,28 @@
+"""This module provides the NetlistMaker class for generating
+horizontal or vertical netlists from a network.
+
+Copyright 2020 Michael Hayes, UCECE
+
+"""
+
+from .netlisthelper import NetlistHelper
+
+class NetlistMaker(NetlistHelper):
+
+    def __init__(self, net, layout='horizontal', evalf=None):
+
+        self.net = net
+        self.evalf = evalf        
+
+        if layout == 'horizontal':
+            self.dir = 'right'
+        elif layout == 'vertical':
+            self.dir = 'down'
+        else:
+            raise ValueError('Unknown layout ' + layout)
+
+    def __call__(self):
+
+        n1 = self._node
+        n2 = self._node        
+        return self.net._net_make(self, n2, n1, dir=self.dir)    
