20a031485f52115aa30384c7780915ad2a59843c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:26 +0000

    Add package tadpak/0.2/tadpak-0.2.tar.gz

---
+++ packages/tadpak/tadpak-0.2.tar.gz/tadpak-0.2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+        name="tadpak",
+        version="0.2",
+        license="MIT",
+        author="SiwonKim",
+        packages=setuptools.find_packages(),
+)
