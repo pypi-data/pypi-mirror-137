117754703962f2fdfe8716586825c0222f3fe996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:03 +0000

    Add package xlaz/0.3.0/xlaz-0.3.0.tar.gz

---
+++ packages/xlaz/xlaz-0.3.0.tar.gz/xlaz-0.3.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['xlaz']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'xlaz',
+    'version': '0.3.0',
+    'description': 'XLA utilities in pure Python',
+    'long_description': '# xlaz\n',
+    'author': 'Shawn Presser',
+    'author_email': 'shawnpresser@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/shawwn/xla',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
