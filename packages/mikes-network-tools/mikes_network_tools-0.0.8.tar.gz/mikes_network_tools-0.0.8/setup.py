d41c8436a6d93b9ca5fcc062f26ff10f304bf1c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:16 +0000

    Add package mikes-network-tools/0.0.8/mikes_network_tools-0.0.8.tar.gz

---
+++ packages/mikes-network-tools/mikes_network_tools-0.0.8.tar.gz/mikes_network_tools-0.0.8/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mikes_network_tools",
+    version="0.0.8",
+    author="Mike Saint-Antoine",
+    author_email="mikest@udel.com",
+    description="Tools for gene regulatory network inference.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mikesaint-antoine/network_inference",
+    # packages=setuptools.find_packages(),
+    packages=["net_inference_tools"],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
