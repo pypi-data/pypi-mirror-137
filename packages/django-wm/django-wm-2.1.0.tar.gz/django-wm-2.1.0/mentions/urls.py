1e2e31e02d201be0da57a5e8691300a0f86b2e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:55 +0000

    Add package django-wm/2.1.0/django-wm-2.1.0.tar.gz

---
+++ packages/django-wm/django-wm-2.1.0.tar.gz/django-wm-2.1.0/mentions/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path, re_path
+
+from mentions.views import view_names
+from mentions.views.retrieve_mentions import GetMentionsView
+from mentions.views.submit_mentions import WebmentionView
+from tests.views import TemplateTagTestView
+
+"""
+/webmention/
+Configure in root project urls.py
+e.g.
+    from django.conf import settings
+    urlpatterns = [
+        ...,
+        path(r'webmention/', include('mentions.urls')),
+    ]
+"""
+urlpatterns = [
+    path("", WebmentionView.as_view(), name=view_names.webmention_api_incoming),
+    re_path(
+        r"^get/?$",
+        GetMentionsView.as_view(),
+        name=view_names.webmention_api_get_for_object,
+    ),
+    path(
+        "templatetagstest",
+        TemplateTagTestView.as_view(),
+        name="test-template-tags",
+    ),
+]
