b52089c0302431cfeccffbd0c5918257e19b55f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:38 +0000

    Add package ExplainAI/0.1.7/ExplainAI-0.1.7.tar.gz

---
+++ packages/ExplainAI/ExplainAI-0.1.7.tar.gz/ExplainAI-0.1.7/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: Feini Huang
+# Mail: 386899557@qq.com
+# Created Time:  2022-2-5 19:17:34
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "ExplainAI",
+    version = "0.1.7",
+    keywords = ["pip", "ML","XAI", "visualization"],
+    description = "explain AI tool",
+    long_description = "",
+    license = "MIT Licence",
+
+    url = "https://github.com/HuangFeini/ExplainAI",
+    author = "Feini Huang",
+    author_email = "386899557@qq.com",
+
+
+    include_package_data = True,
+    platforms = "any",
+    install_requires = [],
+    
+    
+    
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6",
+)
+
+
