6d8afa3c195e2e1f2e50b1d67c9f9d259c6bebca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:45 +0000

    Add package graphdatascience/0.0.9/graphdatascience-0.0.9.tar.gz

---
+++ packages/graphdatascience/graphdatascience-0.0.9.tar.gz/graphdatascience-0.0.9/graphdatascience/pipeline/lp_training_pipeline.py
@@ -0,0 +1,29 @@
+from typing import Any, Dict, List
+
+from ..query_runner.query_runner import QueryRunner
+from .lp_prediction_pipeline import LPPredictionPipeline
+from .training_pipeline import TrainingPipeline
+
+
+class LPTrainingPipeline(TrainingPipeline):
+    def addFeature(self, feature_type: str, **config: Any) -> None:
+        query = (
+            f"{self._query_prefix()}addFeature($pipeline_name, $feature_type, $config)"
+        )
+        params = {
+            "pipeline_name": self.name(),
+            "feature_type": feature_type,
+            "config": config,
+        }
+        self._query_runner.run_query(query, params)
+
+    def feature_steps(self) -> List[Dict[str, Any]]:
+        return self._list_info()["modelInfo"]["featurePipeline"]["featureSteps"]  # type: ignore
+
+    def _query_prefix(self) -> str:
+        return "CALL gds.alpha.ml.pipeline.linkPrediction."
+
+    def _create_trained_model(
+        self, name: str, query_runner: QueryRunner
+    ) -> LPPredictionPipeline:
+        return LPPredictionPipeline(name, query_runner)
