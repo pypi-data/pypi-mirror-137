49a2d66af06693ec9d5081508ac1db74395bc239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:34:22 +0000

    Add package m9s-country-zip/6.0.0/m9s_country_zip-6.0.0.tar.gz

---
+++ packages/m9s-country-zip/m9s_country_zip-6.0.0.tar.gz/m9s_country_zip-6.0.0/configuration.py
@@ -0,0 +1,19 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from trytond.model import ModelSQL, ValueMixin, fields
+from trytond.modules.party.configuration import _ConfigurationValue
+from trytond.pool import PoolMeta
+
+default_country = fields.Many2One('country.country', 'Default Country')
+
+
+class Configuration(metaclass=PoolMeta):
+    __name__ = 'party.configuration'
+    default_country = fields.MultiValue(default_country)
+
+
+class ConfigurationCountry(_ConfigurationValue, ModelSQL, ValueMixin):
+    'Party Configuration Country'
+    __name__ = 'party.configuration.default_country'
+    default_country = default_country
+    _configuration_value_field = 'default_country'
