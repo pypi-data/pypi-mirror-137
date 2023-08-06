931069d6ac31fd6a3694c403c4e36757cb3fb488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:22 +0000

    Add package orion-common/1.0.2/orion_common-1.0.2-py3-none-any.whl

---
+++ packages/orion-common/orion_common-1.0.2-py3-none-any.whl/orion_common/orion_core/dependencies/parler_rest/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+This package provides support for integrating translatable fields into *django-rest-framework*.
+"""
+
+# following PEP 440
+__version__ = "2.1"
+
+from fields import *
+from serializers import *
+from utils import *
