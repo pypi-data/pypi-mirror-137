848a5949742a929bb8494bf7e3554f814a91a9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:34 +0000

    Add package prefect/2.0a10/prefect-2.0a10-py3-none-any.whl

---
+++ packages/prefect/prefect-2.0a10-py3-none-any.whl/prefect/utilities/testing.py
@@ -0,0 +1,24 @@
+""""
+Internal utilities for tests.
+"""
+import sys
+
+
+def exceptions_equal(a, b):
+    """
+    Exceptions cannot be compared by `==`. They can be compared using `is` but this
+    will fail if the exception is serialized/deserialized so this utility does its
+    best to assert equality using the type and args used to initialize the exception
+    """
+    if a == b:
+        return True
+    return type(a) == type(b) and getattr(a, "args", None) == getattr(b, "args", None)
+
+
+# AsyncMock has a new import path in Python 3.8+
+
+if sys.version_info < (3, 8):
+    # https://docs.python.org/3/library/unittest.mock.html#unittest.mock.AsyncMock
+    from mock import AsyncMock
+else:
+    from unittest.mock import AsyncMock
