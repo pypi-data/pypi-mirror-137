2da096d42a4a25e2ebe29c5f6d4ee1e8f8bab5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:20 +0000

    Add package coxbuild/0.1.4/coxbuild-0.1.4-py3-none-any.whl

---
+++ packages/coxbuild/coxbuild-0.1.4-py3-none-any.whl/coxbuild/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import os
+from pathlib import Path
+
+import click
+
+__version__ = "0.1.4"
+
+
+def get_app_directory() -> Path:
+    """Get coxbuild root directory."""
+    return Path(os.path.split(__file__)[0])
+
+
+def get_working_directory() -> Path:
+    """Get coxbuild working directory."""
+    return Path(os.curdir)
