2e8d2a6b72b7ee83033347014f8a2f57c92df8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:03 +0000

    Add package RandomStringGen/0.4/RandomStringGen-0.4.tar.gz

---
+++ packages/RandomStringGen/RandomStringGen-0.4.tar.gz/RandomStringGen-0.4/RandomStringGen/__init__.py
@@ -0,0 +1,11 @@
+"""
+:authors: Itskekoff
+:license: MIT License, see LICENSE file
+:copyright: (c) 2022 itskekoff
+"""
+
+from .RandomStringGen import (Strings)
+
+__author__ = "itskekoff"
+__version__ = "0.4"
+__email__ = "itskekoff@gmail.com"
