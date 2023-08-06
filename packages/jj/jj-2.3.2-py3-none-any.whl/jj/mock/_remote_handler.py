0324d86dc6c057f8819570cf4c4bfccd28074519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:59 +0000

    Add package jj/2.3.2/jj-2.3.2-py3-none-any.whl

---
+++ packages/jj/jj-2.3.2-py3-none-any.whl/jj/mock/_remote_handler.py
@@ -0,0 +1,63 @@
+from types import TracebackType
+from typing import TYPE_CHECKING, Any, List, Optional, Type, Union
+from uuid import UUID, uuid4
+
+from jj.matchers import LogicalMatcher, RequestMatcher
+
+if TYPE_CHECKING:
+    from ._remote_mock import RemoteMock
+    MockType = RemoteMock
+else:
+    MockType = Any
+
+from ._history import HistoryAdapterType, HistoryItem, default_history_adapter
+from ._remote_response import RemoteResponseType
+
+__all__ = ("RemoteHandler",)
+
+
+class RemoteHandler:
+    def __init__(self,
+                 mock: MockType,
+                 matcher: Union[RequestMatcher, LogicalMatcher],
+                 response: RemoteResponseType,
+                 *,
+                 history_adapter: Optional[HistoryAdapterType] = default_history_adapter) -> None:
+        self._id = uuid4()
+        self._mock = mock
+        self._matcher = matcher
+        self._response = response
+        self._history_adapter = history_adapter
+
+    @property
+    def id(self) -> UUID:
+        return self._id
+
+    @property
+    def matcher(self) -> Union[RequestMatcher, LogicalMatcher]:
+        return self._matcher
+
+    @property
+    def response(self) -> RemoteResponseType:
+        return self._response
+
+    async def register(self) -> None:
+        await self._mock.register(self)
+
+    async def deregister(self) -> None:
+        await self._mock.deregister(self)
+
+    async def fetch_history(self) -> List[HistoryItem]:
+        history = await self._mock.fetch_history(self)
+        if self._history_adapter:
+            return [self._history_adapter(x) for x in history]
+        return history
+
+    async def __aenter__(self) -> None:
+        await self.register()
+
+    async def __aexit__(self,
+                        exc_type: Optional[Type[BaseException]],
+                        exc_val: Optional[BaseException],
+                        exc_tb: Optional[TracebackType]) -> None:
+        await self.deregister()
---
+++ packages/jj/jj-2.3.2-py3-none-any.whl/jj/mock/_remote_mock.py
@@ -0,0 +1,70 @@
+from typing import List, Optional, Union, cast
+
+from aiohttp import ClientSession
+from packed import pack, unpack
+
+from jj.http.codes import OK
+from jj.matchers import LogicalMatcher, RequestMatcher
+
+from ._history import HistoryAdapterType, HistoryItem, default_history_adapter
+from ._remote_handler import RemoteHandler
+from ._remote_response import RemoteResponseType
+
+__all__ = ("RemoteMock",)
+
+
+class RemoteMock:
+    def __init__(self, url: str) -> None:
+        self._url = url
+
+    def create_handler(self,
+                       matcher: Union[RequestMatcher, LogicalMatcher],
+                       response: RemoteResponseType,
+                       *,
+                       history_adapter: Optional[HistoryAdapterType] = default_history_adapter
+                       ) -> RemoteHandler:
+        return RemoteHandler(self, matcher, response, history_adapter=history_adapter)
+
+    async def register(self, handler: RemoteHandler) -> "RemoteMock":
+        headers = {"x-jj-remote-mock": ""}
+        payload = {
+            "id": str(handler.id),
+            "request": handler.matcher,
+            "response": handler.response,
+        }
+        binary = pack(payload)
+
+        async with ClientSession() as session:
+            async with session.post(self._url, data=binary, headers=headers) as response:
+                assert response.status == OK, response
+        return self
+
+    async def deregister(self, handler: RemoteHandler) -> "RemoteMock":
+        headers = {"x-jj-remote-mock": ""}
+        payload = {
+            "id": str(handler.id),
+            "request": handler.matcher,
+            "response": handler.response,
+        }
+        binary = pack(payload)
+
+        async with ClientSession() as session:
+            async with session.delete(self._url, data=binary, headers=headers) as response:
+                assert response.status == OK, response
+        return self
+
+    async def fetch_history(self, handler: RemoteHandler) -> List[HistoryItem]:
+        headers = {"x-jj-remote-mock": ""}
+        payload = {
+            "id": str(handler.id),
+            "request": handler.matcher,
+            "response": handler.response,
+        }
+        binary = pack(payload)
+
+        async with ClientSession() as session:
+            async with session.get(self._url, data=binary, headers=headers) as response:
+                assert response.status == OK, response
+                body = await response.read()
+                unpacked = unpack(body)
+                return cast(List[HistoryItem], unpacked)
