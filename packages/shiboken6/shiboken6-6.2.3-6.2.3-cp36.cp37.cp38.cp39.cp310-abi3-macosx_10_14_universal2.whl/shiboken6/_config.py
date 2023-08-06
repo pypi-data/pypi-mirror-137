9d8d162f2f789a358c57139a212816140b4805c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:36:58 +0000

    Add package shiboken6/6.2.3/shiboken6-6.2.3-6.2.3-cp36.cp37.cp38.cp39.cp310-abi3-macosx_10_14_universal2.whl

---
+++ packages/shiboken6/shiboken6-6.2.3-6.2.3-cp36.cp37.cp38.cp39.cp310-abi3-macosx_10_14_universal2.whl/shiboken6/_config.py
@@ -0,0 +1,11 @@
+shiboken_library_soversion = str(6.2)
+
+version = "6.2.3"
+version_info = (6, 2, 3, "", "")
+
+__build_date__ = '2022-01-28T08:28:40+00:00'
+
+
+
+
+__setup_py_package_version__ = '6.2.3'
