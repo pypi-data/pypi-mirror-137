65d2baa2bcd73a116a601c45ca02e8013b3b0c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:43 +0000

    Add package bclib/3.3.0/bclib-3.3.0.tar.gz

---
+++ packages/bclib/bclib-3.3.0.tar.gz/bclib-3.3.0/bclib/dispatcher/socket_dispatcher.py
@@ -0,0 +1,38 @@
+import asyncio
+import signal
+import threading
+from ..dispatcher.routing_dispatcher import RoutingDispatcher
+from ..listener import Endpoint, Message, SocketListener
+
+
+class SocketDispatcher(RoutingDispatcher):
+    def __init__(self, options: dict):
+        super().__init__(options)
+        self.__lock = threading.Lock()
+        self.__listener = SocketListener(
+            Endpoint(self.options.receiver),
+            Endpoint(self.options.sender),
+            self._on_message_receive_async)
+
+    async def send_message_async(self, message: Message) -> bool:
+        """Send message to endpoint"""
+
+        try:
+            self.__lock.acquire()
+            return await self.__listener.send_message_async(message)
+        finally:
+            self.__lock.release()
+
+    def listening(self):
+        loop = asyncio.get_event_loop()
+        for sig in (signal.SIGTERM, signal.SIGINT):
+            signal.signal(sig, lambda sig, _: loop.stop())
+        super().initialize_task(loop)
+        self.__listener.initialize_task(loop)
+        loop.run_forever()
+        tasks = asyncio.all_tasks(loop=loop)
+        for task in tasks:
+            task.cancel()
+        group = asyncio.gather(*tasks, return_exceptions=True)
+        loop.run_until_complete(group)
+        loop.close()
