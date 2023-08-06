566e1e7e34af7c10d12f13f8240b38a66eaceaaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:25 +0000

    Add package pytoo/0.1.1/pytoo-0.1.1.tar.gz

---
+++ packages/pytoo/pytoo-0.1.1.tar.gz/pytoo-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pytoo']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pytoo',
+    'version': '0.1.1',
+    'description': 'Example Python Library',
+    'long_description': None,
+    'author': 'Juraj Figura',
+    'author_email': 'guitarist90@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
