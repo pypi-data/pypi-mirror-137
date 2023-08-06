5c9a28497f67966f243faafe7bffc7860d48491c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:58 +0000

    Add package miroflowexport/1.0.1/miroflowexport-1.0.1.tar.gz

---
+++ packages/miroflowexport/miroflowexport-1.0.1.tar.gz/miroflowexport-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='miroflowexport',
+      version='1.0.1',
+      description='Derive a Gantt-like plan from Miro sticky note workflow',
+      author='Sven Flake',
+      author_email='sven.flake@gmail.com',
+      url='https://gitlab.com/sven.flake/miroflowexport',
+      packages=find_packages(include = ['miroflowexport', 'miroflowexport.*']),
+      install_requires=[
+            "flexlog==1.5.0",
+            "coolname==1.1.0",
+            "openpyxl==3.0.9",
+            "requests==2.27.1",
+      ],
+     )
