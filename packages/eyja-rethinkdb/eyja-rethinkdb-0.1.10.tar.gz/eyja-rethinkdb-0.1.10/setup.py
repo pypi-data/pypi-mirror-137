31f1ed5944a4093e5cb8b78f2aeb48f5dccadba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:10 +0000

    Add package eyja-rethinkdb/0.1.10/eyja-rethinkdb-0.1.10.tar.gz

---
+++ packages/eyja-rethinkdb/eyja-rethinkdb-0.1.10.tar.gz/eyja-rethinkdb-0.1.10/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='eyja-rethinkdb',
+    zip_safe=True,
+    version='0.1.10',
+    description='RethinkDB Plugin for Eyja',
+    url='https://gitlab.com/public.eyja.dev/eyja-rethinkdb',
+    maintainer='Anton Berdnikov',
+    maintainer_email='agratoth@yandex.ru',
+    packages=find_packages(),
+    package_dir={'eyja_rethinkdb': 'eyja_rethinkdb'},
+    install_requires=[
+        'eyja-internal>==0.4.4',
+        'rethinkdb>=2.4.8',
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires='>=3.8',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
