7efeb1aba02856bffff72c0acdfe4f70e3a2dd08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:39 +0000

    Add package ipypdf/0.0.5/ipypdf-0.0.5.tar.gz

---
+++ packages/ipypdf/ipypdf-0.0.5.tar.gz/ipypdf-0.0.5/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+
+from pathlib import Path
+HERE = Path(__file__).parent
+long_description = (HERE / "README.md").read_text()
+
+requirements = [
+    "pytesseract",
+    "spacy",
+    "jupyterlab",
+    "ipycanvas",
+    "ipycytoscape",
+    "ipyevents",
+    "ipywidgets",
+    "ipytree",
+    "traitlets",
+    "pdf2image",
+    "opencv-python",
+    "numpy",
+    "pandas",
+    "matplotlib",
+]
+
+setup(
+    name='ipypdf',
+    version="0.0.5",
+    description="Jupyter widget for applying nlp to pdf documents",
+    python_requires='>=3.6,<=3.8',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="MIT",
+    author="Joel Stansbury",
+    author_email='stansbury.joel@gmail.com',
+    url='https://github.com/JoelStansbury/ipypdf',
+    packages=['ipypdf','tests'],
+    package_data = {
+        'ipypdf': [
+            'utils/*',
+            'widgets/*',
+            'style/*'
+        ],
+        'tests': [
+            '.py',
+            'fixture_data/*.pdf',
+            'fixture_data/*.json',
+        ]
+    },
+    install_requires=requirements,
+    keywords='ipypdf',
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
