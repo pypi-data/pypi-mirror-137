381186263bc546aa24f744354b36d429a45284cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:29 +0000

    Add package stick-protocol-server/1.2.2/stick-protocol-server-1.2.2.tar.gz

---
+++ packages/stick-protocol-server/stick-protocol-server-1.2.2.tar.gz/stick-protocol-server-1.2.2/setup.py
@@ -0,0 +1,8 @@
+#   Copyright © 2018-2021 STiiiCK.
+#
+#   This source code is licensed under the GPLv3 license found in the
+#   LICENSE file in the root directory of this source tree.
+
+from setuptools import setup
+
+setup()
