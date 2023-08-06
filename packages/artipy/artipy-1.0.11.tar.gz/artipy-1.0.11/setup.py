e9afa927d99fe0576c56cec92dd220c4f038de8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:25 +0000

    Add package artipy/1.0.11/artipy-1.0.11.tar.gz

---
+++ packages/artipy/artipy-1.0.11.tar.gz/artipy-1.0.11/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+# setup the package
+setuptools.setup(
+    name="artipy",
+    version="1.0.11",
+    author="daniel tal",
+    author_email="daniel@dtsoft.co.il",
+    requires=["setuptools"],
+    install_requires=["cython"],
+    description="lib for adding artifact creation setup options",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: MIT",
+        "Operating System :: OS Independent",
+    ],
+)
