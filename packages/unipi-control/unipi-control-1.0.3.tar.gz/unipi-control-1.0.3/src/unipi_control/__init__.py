c6440e24545aba4ed813592c66a2b811c0046c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:02 +0000

    Add package unipi-control/1.0.3/unipi-control-1.0.3.tar.gz

---
+++ packages/unipi-control/unipi-control-1.0.3.tar.gz/unipi-control-1.0.3/src/unipi_control/__init__.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+# https://stackoverflow.com/questions/16981921/relative-imports-in-python-3
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+__version__ = "1.0.3"
