c963c4b8a46530ed4bc941489f4bb649288d4f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:41:06 +0000

    Add package guimadeeasy/1.0/guimadeeasy-1.0.tar.gz

---
+++ packages/guimadeeasy/guimadeeasy-1.0.tar.gz/guimadeeasy-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="guimadeeasy",
+    version="1.0",
+    description="this is just a fun challenge for my friend",
+    packages=['src'],
+)
---
+++ packages/guimadeeasy/guimadeeasy-1.0.tar.gz/guimadeeasy-1.0/src/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+from calculator import *
+from notepad import *
+from todo import *
+from wheather import *
+from snake import *
+from awesomeclock import *
