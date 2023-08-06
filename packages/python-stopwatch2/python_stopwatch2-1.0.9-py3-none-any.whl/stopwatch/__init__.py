8f9a86e1a8935df05b96253c173dac0b567849cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:04 +0000

    Add package python-stopwatch2/1.0.9/python_stopwatch2-1.0.9-py3-none-any.whl

---
+++ packages/python-stopwatch2/python_stopwatch2-1.0.9-py3-none-any.whl/stopwatch/__init__.py
@@ -0,0 +1,9 @@
+from .contextmanagers.profile import profile
+from .contextmanagers.stopwatch import stopwatch
+from .stopwatch import Stopwatch
+
+__all__ = (
+    'profile',
+    'stopwatch',
+    'Stopwatch',
+)
