f0c5d7d2655f7e13e09edb8fc0eac932f6fe716b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:14 +0000

    Add package django-urlconfchecks/0.3.0/django_urlconfchecks-0.3.0-py3-none-any.whl

---
+++ packages/django-urlconfchecks/django_urlconfchecks-0.3.0-py3-none-any.whl/django_urlconfchecks/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for django-UrlConfChecks."""
+from django_urlconfchecks import url_checker
+
+__author__ = """ali sayyah"""
+__email__ = 'ali.sayyah2@gmail.com'
+__version__ = '0.3.0'
