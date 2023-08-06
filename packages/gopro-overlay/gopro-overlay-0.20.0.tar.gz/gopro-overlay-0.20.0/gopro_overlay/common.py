47201a22676bcdf609016df4661d562f83893559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:33 +0000

    Add package gopro-overlay/0.20.0/gopro-overlay-0.20.0.tar.gz

---
+++ packages/gopro-overlay/gopro-overlay-0.20.0.tar.gz/gopro-overlay-0.20.0/gopro_overlay/common.py
@@ -0,0 +1,16 @@
+import contextlib
+import sys
+
+
+@contextlib.contextmanager
+def smart_open(filename=None):
+    if filename and filename != '-':
+        fh = open(filename, 'w')
+    else:
+        fh = sys.stdout
+
+    try:
+        yield fh
+    finally:
+        if fh is not sys.stdout:
+            fh.close()
