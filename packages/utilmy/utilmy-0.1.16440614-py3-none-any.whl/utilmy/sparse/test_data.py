c03ad3888b76dbe5600c88a09e9fbc7ba791acf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:59 +0000

    Add package utilmy/0.1.16440614/utilmy-0.1.16440614-py3-none-any.whl

---
+++ packages/utilmy/utilmy-0.1.16440614-py3-none-any.whl/utilmy/sparse/test_data.py
@@ -0,0 +1,55 @@
+
+##### Input Data
+
+
+def pandas_to_csr(Xdf,  ydf, hashsize=5000):
+    """
+        Dataframes
+        Xdf:
+        userid :  Int64
+        genre_list1  :  string:    "4343/4343/4545, 4343/4343/4545, 4343/4343/4545, 4343/4343/4545,4343/4343/4545"
+        genre_list2  :  string :   "4343/4343/4545, 4343/4343/4545, 4343/4343/4545, 4343/4343/4545,4343/4343/4545"
+
+
+        Ydf:
+        genre_list3  :  string:    "4343/4343/4545, 4343/4343/4545, 4343/4343/4545, 4343/4343/4545,4343/4343/4545"
+
+
+        To transform into :
+
+        X sparse of dimension:
+              ncol: 5000 (hash size) *   (nlist1 + nlist2) 
+              nrows:  n_userid
+
+
+        y sparse of dimension:
+              ncol: 5000 (hash size) *   (nlist3) 
+              nrows:  n_userid
+
+
+        ll = "4343/4343/4545".split("/")
+        for t in ll :
+           colid = hash(t) % 5000 ---> into 0---5000 bucket (ie one hot).
+    
+    
+    """
+    
+    
+    
+    
+    
+    return X, y
+
+
+
+import scipy, numpy as np
+X=  scipy.sparse.coo_matrix(( np.random.random(120), (np.random.randint(0,100,120), np.random.randint(0,100,120) )), dtype='float32')
+X = X.tocsr()
+n1 = X.shape[1]
+
+
+y=  scipy.sparse.coo_matrix(( np.random.randint(0,2, 120), (np.random.randint(0,100, 120), np.random.randint(0,100, 120) )), dtype='float32')
+Y =y.tocsr()
+
+
+
