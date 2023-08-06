d69c7958ca5f0078beeb23b3ad9f75627b456a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:41 +0000

    Add package sub-package2/1.0/sub_package2-1.0-py3-none-any.whl

---
+++ packages/sub-package2/sub_package2-1.0-py3-none-any.whl/sub_package2/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  5 20:36:41 2022
+
+@author: ALEXANDRA
+"""
+
+
+def mod3():
+    print ("mod3")
+    
+def mod4():
+    print ("mod4")
+    
+
+    
+
