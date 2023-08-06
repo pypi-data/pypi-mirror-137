48e0ded67c3c3f69b2e85264e8e1079ab809483f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:10 +0000

    Add package formsg-python-sdk/0.1.4/formsg_python_sdk-0.1.4-py3-none-any.whl

---
+++ packages/formsg-python-sdk/formsg_python_sdk-0.1.4-py3-none-any.whl/formsg/schemas/crypto.py
@@ -0,0 +1,57 @@
+from typing import Any, List, Mapping, Optional, Union
+
+from typing_extensions import Literal, TypedDict
+
+FieldType = Union[
+    Literal["section"],
+    Literal["radiobutton"],
+    Literal["dropdown"],
+    Literal["checkbox"],
+    Literal["nric"],
+    Literal["email"],
+    Literal["table"],
+    Literal["number"],
+    Literal["rating"],
+    Literal["yes_no"],
+    Literal["decimal"],
+    Literal["textfield"],  # short text
+    Literal["textarea"],  # long text
+    Literal["attachment"],
+    Literal["date"],
+    Literal["mobile"],
+    Literal["homeno"],
+]
+FormFieldSignature = TypedDict(
+    "FormFieldSignature",
+    {
+        "answer": Optional[str],
+        "answer_array": Optional[Union[List[str], List[List[str]]]],
+    },
+)
+FormField = TypedDict(
+    "FormField",
+    {
+        "_id": str,
+        "question": str,
+        "field_type": FieldType,
+        "is_header": bool,
+        "signature": Optional[FormFieldSignature],
+    },
+)
+DecryptedContent = TypedDict(
+    "DecryptedContent",
+    {"responses": List[FormField], "verified": Optional[Mapping[str, Any]]},
+)
+
+
+EncryptedAttachmentRecords = Mapping[str, str]
+
+DecryptParams = TypedDict(
+    "DecryptParams",
+    {
+        "encryptedContent": str,
+        "version": str,
+        "verifiedContent": Optional[str],
+        "attachmentDownloadUrls": Optional[EncryptedAttachmentRecords],
+    },
+)
