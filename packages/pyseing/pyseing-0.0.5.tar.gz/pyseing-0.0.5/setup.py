49dedda3fe36712b18ea77630f7225a46cda45e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:40 +0000

    Add package pyseing/0.0.5/pyseing-0.0.5.tar.gz

---
+++ packages/pyseing/pyseing-0.0.5.tar.gz/pyseing-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.5'
+DESCRIPTION = 'test1'
+LONG_DESCRIPTION = 'test1 long description'
+
+# Setting up
+setup(
+    name="pyseing",
+    version=VERSION,
+    author="dummy",
+    author_email="<aomankit2002@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=[],
+    package_dir={'': '.'},
+    package_data={'': ['pyseing.cpython-38-x86_64-linux-gnu.so']},
+    install_requires=[],
+    keywords=[],
+    classifiers=[]
+)
