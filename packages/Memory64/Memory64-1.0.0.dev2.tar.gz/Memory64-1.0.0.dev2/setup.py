160e7a0e85ae9b7d8319e7ea2266b297fe0fc003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:31 +0000

    Add package Memory64/1.0.0.dev2/Memory64-1.0.0.dev2.tar.gz

---
+++ packages/Memory64/Memory64-1.0.0.dev2.tar.gz/Memory64-1.0.0.dev2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='Memory64',
+      version='1.0.0dev2',
+      description='一个内存读写模块, 今后会更加的完善, 在此非常感谢大家的支持 !',
+      url='https://space.bilibili.com/84500837',
+      author='忆梦',
+      author_email='2872930558@qq.com',
+      license='MIT',
+      packages=find_packages()
+)
