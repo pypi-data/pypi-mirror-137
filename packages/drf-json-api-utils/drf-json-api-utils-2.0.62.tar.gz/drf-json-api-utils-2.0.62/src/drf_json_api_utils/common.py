df14b072781d5be2188e000f4b9a83bd46deac92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:43 +0000

    Add package drf-json-api-utils/2.0.62/drf-json-api-utils-2.0.62.tar.gz

---
+++ packages/drf-json-api-utils/drf-json-api-utils-2.0.62.tar.gz/drf-json-api-utils-2.0.62/src/drf_json_api_utils/common.py
@@ -0,0 +1,51 @@
+import logging
+from functools import wraps
+from rest_framework.response import Response
+from rest_framework.filters import SearchFilter
+from rest_framework.status import HTTP_500_INTERNAL_SERVER_ERROR
+from rest_framework_json_api.pagination import JsonApiPageNumberPagination
+
+DEFAULT_PAGE_SIZE = 50
+
+
+class LimitedJsonApiPageNumberPagination(JsonApiPageNumberPagination):
+    page_size = DEFAULT_PAGE_SIZE
+
+
+class JsonApiSearchFilter(SearchFilter):
+    search_param = 'filter[search]'
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class JsonApiGlobalSettings(metaclass=Singleton):
+    pass
+
+
+def exception_handler(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as exc:
+            import traceback
+            traceback.print_exc()
+
+            settings = JsonApiGlobalSettings()
+            exception_response_handler = getattr(settings, 'exception_response_handler', None)
+            if exception_response_handler:
+                return exception_response_handler(exc)
+
+            return Response(data={'attributes': {'message': str(exc)}},
+                            status=getattr(exc, 'http_status', HTTP_500_INTERNAL_SERVER_ERROR))
+    return wrapper
+
+
+LOGGER = logging.getLogger(__name__)
