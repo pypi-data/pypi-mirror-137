5c77b592657a9931ef926ee3ccedef72aab1b9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:54 +0000

    Add package napari-plot/0.1.3/napari_plot-0.1.3.tar.gz

---
+++ packages/napari-plot/napari_plot-0.1.3.tar.gz/napari_plot-0.1.3/napari_plot/_vispy/utils/visual.py
@@ -0,0 +1,44 @@
+"""Utilities."""
+from napari._vispy.layers.base import VispyBaseLayer
+from napari._vispy.layers.points import VispyPointsLayer
+from napari._vispy.layers.shapes import VispyShapesLayer
+from napari.layers import Points, Shapes
+
+from ...layers import Centroids, InfLine, Line, MultiLine, Region, Scatter
+from ..layers.centroids import VispyCentroidsLayer
+from ..layers.infline import VispyInfLineLayer
+from ..layers.line import VispyLineLayer
+from ..layers.multiline import VispyMultiLineLayer
+from ..layers.region import VispyRegionLayer
+from ..layers.scatter import VispyScatterLayer
+
+layer_to_visual = {
+    Line: VispyLineLayer,
+    Centroids: VispyCentroidsLayer,
+    Scatter: VispyScatterLayer,
+    Shapes: VispyShapesLayer,
+    Points: VispyPointsLayer,
+    Region: VispyRegionLayer,
+    InfLine: VispyInfLineLayer,
+    MultiLine: VispyMultiLineLayer,
+}
+
+
+def create_vispy_visual(layer) -> VispyBaseLayer:
+    """Create vispy visual for a layer based on its layer type.
+
+    Parameters
+    ----------
+    layer : napari.layers._base_layer.Layer
+        Layer that needs its property widget created.
+
+    Returns
+    -------
+    visual : vispy.scene.visuals.VisualNode
+        Vispy visual node
+    """
+    for layer_type, visual_class in layer_to_visual.items():
+        if isinstance(layer, layer_type):
+            return visual_class(layer)
+
+    raise TypeError(f"Could not find VispyLayer for layer of type {type(layer)}")
---
+++ packages/napari-plot/napari_plot-0.1.3.tar.gz/napari_plot-0.1.3/napari_plot/components/_viewer_mouse_bindings.py
@@ -0,0 +1,80 @@
+"""Mouse bindings to the viewer"""
+from functools import partial
+
+from .tools import Shape
+
+ACTIVE_COLOR = (1.0, 0.0, 0.0, 1.0)
+
+
+def boxzoom(viewer, event):
+    """Enable box zoom."""
+
+    def _get_shape():
+        if "Control" in event.modifiers:
+            return Shape.VERTICAL
+        elif "Shift" in event.modifiers:
+            return Shape.HORIZONTAL
+        return Shape.BOX
+
+    # make sure box is visible
+    if not viewer.drag_tool.tool.visible:
+        viewer.drag_tool.tool.visible = True
+
+    # on press
+    color = viewer.drag_tool.tool.color
+    viewer.drag_tool.tool.shape = _get_shape()
+    yield
+
+    # on mouse move
+    while event.type == "mouse_move":
+        viewer.drag_tool.tool.shape = _get_shape()
+        yield
+
+    # on release
+    viewer.drag_tool.tool.color = color
+    position = viewer.drag_tool.tool.position
+    viewer.drag_tool.tool.visible = False
+    viewer.drag_tool.tool.position = (0, 0, 0, 0)
+    viewer.events.span(position=position)
+
+
+def boxzoom_shape(shape: Shape, viewer, event):
+    """Enable box zoom."""
+
+    def _set_event_range():
+        if "Control" in event.modifiers:
+            viewer.drag_tool.ctrl = position
+        elif "Shift" in event.modifiers:
+            viewer.drag_tool.shift = position
+        elif "Alt" in event.modifiers:
+            viewer.drag_tool.alt = position
+
+    # make sure box is visible
+    if not viewer.drag_tool.tool.visible:
+        viewer.drag_tool.tool.visible = True
+
+    # on press
+    color = viewer.drag_tool.tool.color
+    viewer.drag_tool.tool.shape = shape
+    yield
+
+    # on mouse move
+    while event.type == "mouse_move":
+        if event.modifiers:
+            viewer.drag_tool.tool.color = ACTIVE_COLOR
+        else:
+            viewer.drag_tool.tool.color = color
+        yield
+
+    # on release
+    viewer.drag_tool.tool.color = color
+    position = viewer.drag_tool.tool.position
+    _set_event_range()
+    viewer.drag_tool.tool.visible = False
+    viewer.drag_tool.tool.position = (0, 0, 0, 0)
+    viewer.events.span(position=position)
+
+
+boxzoom_vertical = partial(boxzoom_shape, Shape.VERTICAL)
+boxzoom_horizontal = partial(boxzoom_shape, Shape.HORIZONTAL)
+boxzoom_box = partial(boxzoom_shape, Shape.BOX)
