d70a8420fcc682a15fed064a03fdb515f3e52f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:24 +0000

    Add package qiskit-superstaq/0.1.8/qiskit_superstaq-0.1.8-py3-none-any.whl

---
+++ packages/qiskit-superstaq/qiskit_superstaq-0.1.8-py3-none-any.whl/qiskit_superstaq/superstaq_backend.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+from typing import Any, List, Union
+
+import qiskit
+import requests
+
+import qiskit_superstaq as qss
+
+
+class SuperstaQBackend(qiskit.providers.BackendV1):
+    def __init__(
+        self, provider: "qss.superstaq_provider.SuperstaQProvider", remote_host: str, backend: str
+    ) -> None:
+        self.remote_host = remote_host
+        self._provider = provider
+        self.configuration_dict = {
+            "backend_name": backend,
+            "backend_version": "n/a",
+            "n_qubits": -1,
+            "basis_gates": None,
+            "gates": [],
+            "local": False,
+            "simulator": False,
+            "conditional": False,
+            "open_pulse": False,
+            "memory": False,
+            "max_shots": -1,
+            "coupling_map": None,
+        }
+        super().__init__(
+            configuration=qiskit.providers.models.BackendConfiguration.from_dict(
+                self.configuration_dict
+            ),
+            provider=provider,
+        )
+
+    @classmethod
+    def _default_options(cls) -> qiskit.providers.Options:
+        return qiskit.providers.Options(shots=1000)
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, qss.superstaq_backend.SuperstaQBackend):
+            return False
+
+        return (
+            self._provider == other._provider
+            and self.configuration_dict == other.configuration_dict
+        )
+
+    def run(
+        self, circuits: Union[qiskit.QuantumCircuit, List[qiskit.QuantumCircuit]], **kwargs: int
+    ) -> "qss.superstaq_job.SuperstaQJob":
+
+        if isinstance(circuits, qiskit.QuantumCircuit):
+            circuits = [circuits]
+
+        superstaq_json = {
+            "qiskit_circuits": qss.serialization.serialize_circuits(circuits),
+            "backend": self.name(),
+            "shots": kwargs.get("shots"),
+            "ibmq_token": kwargs.get("ibmq_token"),
+            "ibmq_hub": kwargs.get("ibmq_hub"),
+            "ibmq_group": kwargs.get("ibmq_group"),
+            "ibmq_project": kwargs.get("ibmq_project"),
+            "ibmq_pulse": kwargs.get("ibmq_pulse"),
+        }
+
+        res = requests.post(
+            f"{self.remote_host}/{qss.API_VERSION}/jobs",
+            json=superstaq_json,
+            headers=self._provider._http_headers(),
+            verify=(self.remote_host == qss.API_URL),
+        )
+
+        res.raise_for_status()
+        response = res.json()
+        if "job_ids" not in response:
+            raise Exception
+
+        #  we make a virtual job_id that aggregates all of the individual jobs
+        # into a single one, that comma-separates the individual jobs:
+        job_id = ",".join(response["job_ids"])
+        job = qss.superstaq_job.SuperstaQJob(self, job_id)
+
+        return job
---
+++ packages/qiskit-superstaq/qiskit_superstaq-0.1.8-py3-none-any.whl/qiskit_superstaq/superstaq_backend_test.py
@@ -0,0 +1,108 @@
+import json
+from typing import Any, Dict, List
+
+import pytest
+import qiskit
+import requests
+
+import qiskit_superstaq as qss
+
+
+def test_default_options() -> None:
+    ss_provider = qss.superstaq_provider.SuperstaQProvider(api_key="MY_TOKEN")
+    device = qss.superstaq_backend.SuperstaQBackend(
+        provider=ss_provider,
+        remote_host=qss.API_URL,
+        backend="ibmq_qasm_simulator",
+    )
+
+    assert qiskit.providers.Options(shots=1000) == device._default_options()
+
+
+class MockResponse:
+    def __init__(self, job_ids: List[str]) -> None:
+        self.content = json.dumps({"job_ids": job_ids})
+
+    def json(self) -> Dict:
+        return json.loads(self.content)
+
+    def raise_for_status(self) -> None:
+        pass
+
+
+class MockBadResponse:
+    def __init__(self) -> None:
+        self.content = json.dumps({})
+
+    def json(self) -> Dict:
+        return {}
+
+    def raise_for_status(self) -> None:
+        pass
+
+
+class MockProvider(qss.superstaq_provider.SuperstaQProvider):
+    def __init__(self) -> None:
+        self.api_key = "super.tech"
+
+
+class MockDevice(qss.superstaq_backend.SuperstaQBackend):
+    def __init__(self) -> None:
+        super().__init__(MockProvider(), "super.tech", "mock_backend")
+        self._provider = MockProvider()
+
+
+def test_run(monkeypatch: Any) -> None:
+    qc = qiskit.QuantumCircuit(2, 2)
+    qc.h(0)
+    qc.cx(0, 1)
+    qc.measure([0, 0], [1, 1])
+    device = MockDevice()
+
+    monkeypatch.setattr(requests, "post", lambda *_, **__: MockResponse(["123abc"]))
+    answer = device.run(circuits=qc)
+    expected = qss.superstaq_job.SuperstaQJob(device, "123abc")
+    assert answer == expected
+
+    monkeypatch.setattr(requests, "post", lambda *_, **__: MockBadResponse())
+    with pytest.raises(Exception):
+        device.run(circuits=qc)
+
+
+def test_multi_circuit_run(monkeypatch: Any) -> None:
+    device = MockDevice()
+
+    qc1 = qiskit.QuantumCircuit(1, 1)
+    qc1.h(0)
+    qc1.measure(0, 0)
+
+    qc2 = qiskit.QuantumCircuit(2, 2)
+    qc2.h(0)
+    qc2.cx(0, 1)
+    qc2.measure([0, 1], [0, 1])
+
+    monkeypatch.setattr(requests, "post", lambda *_, **__: MockResponse(["123abc", "456efg"]))
+    answer = device.run(circuits=[qc1, qc2])
+    expected = qss.superstaq_job.SuperstaQJob(device, "123abc,456efg")
+
+    assert answer == expected
+
+
+def test_eq() -> None:
+
+    assert MockDevice() != 3
+
+    provider = qss.superstaq_provider.SuperstaQProvider(api_key="123")
+
+    backend1 = qss.superstaq_backend.SuperstaQBackend(
+        provider=provider, backend="ibmq_qasm_simulator", remote_host=qss.API_URL
+    )
+    backend2 = qss.superstaq_backend.SuperstaQBackend(
+        provider=provider, backend="ibmq_athens", remote_host=qss.API_URL
+    )
+    assert backend1 != backend2
+
+    backend3 = qss.superstaq_backend.SuperstaQBackend(
+        provider=provider, backend="ibmq_qasm_simulator", remote_host=qss.API_URL
+    )
+    assert backend1 == backend3
