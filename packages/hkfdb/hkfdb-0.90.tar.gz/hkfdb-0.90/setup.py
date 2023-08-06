30a46883ccbfbc8fc267be3dc3a33f51e32eab8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:04 +0000

    Add package hkfdb/0.90/hkfdb-0.90.tar.gz

---
+++ packages/hkfdb/hkfdb-0.90.tar.gz/hkfdb-0.90/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hkfdb",
+    version="0.90",
+    author="Hong Kong Finance Database Team",
+    author_email="info@hkfdb.net",
+    description="Hong Kong Finance Database.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.hkfdb.net",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
