7b482e2fa0d64e97cd473f3a51afeb4a0a361499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:06 +0000

    Add package valeeew/0.0.10/valeeew-0.0.10.tar.gz

---
+++ packages/valeeew/valeeew-0.0.10.tar.gz/valeeew-0.0.10/src/valeeew/cleaning.py
@@ -0,0 +1,40 @@
+from valeeew import __init__
+#function to make a quick overview of your DATAframe
+def watch(data):
+  x = __init__.var_name(data)
+  s = " "
+
+  dupli = data.duplicated().sum()
+  nul = (data.isna().mean()).sum()
+  reslt = 0
+  if dupli + nul == 0:
+    a = "No duplicate and no null DATA"
+  elif dupli == 0 and nul != 0:
+    a = "Null DATA and nothing duplicate"
+  elif dupli != 0 and nul == 0:
+    a = "Duplicate DATA and nothing null"
+  else:
+    a = "Duplicate and null DATA"
+  
+  if a == "Duplicate DATA and nothing null" or "Duplicate and null DATA":
+    reslt = data[(data.duplicated())== True]
+  
+  print(x)
+  print(s)
+  print(a)
+  print(s)
+  print(data.isna().sum())
+  print(s)
+  print(reslt)
+  print("______________")
+  print(s)
+
+# function to filter the values we want to keep
+def keep(data, col, value):
+    reslt = data[(data[col] == value) == True]
+    return reslt
+
+# function to filter the values we want to delete
+def kick(data, col, value):
+    reslt = data[(data[col] == value) == False]
+    return reslt
