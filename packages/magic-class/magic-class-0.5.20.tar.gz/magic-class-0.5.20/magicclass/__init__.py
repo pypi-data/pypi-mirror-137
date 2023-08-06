361820a6adbbb0bfd4d417390357650841ad3191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:31 +0000

    Add package magic-class/0.5.20/magic-class-0.5.20.tar.gz

---
+++ packages/magic-class/magic-class-0.5.20.tar.gz/magic-class-0.5.20/magicclass/__init__.py
@@ -0,0 +1,29 @@
+__version__ = "0.5.20"
+
+from .core import (
+    magicclass,
+    magicmenu,
+    magiccontext,
+    magictoolbar, 
+    Parameters,
+    Bound,
+    build_help
+    )
+
+from .wrappers import (
+    set_options,
+    click,
+    set_design,
+    do_not_record,
+    bind_key,
+    confirm,
+    nogui,
+    )
+
+from .fields import field, vfield
+from .gui._base import wraps, defaults, MagicTemplate, PopUpMode
+from ._typing import WidgetType
+from .gui.keybinding import Key
+from . import widgets, utils
+
+from magicgui import *
