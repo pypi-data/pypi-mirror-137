7d78ae5bf29d726f036de51ee4614618a2c29b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:07 +0000

    Add package Mopidy-Iris/3.61.1/Mopidy_Iris-3.61.1-py3-none-any.whl

---
+++ packages/Mopidy-Iris/Mopidy_Iris-3.61.1-py3-none-any.whl/mopidy_iris/frontend.py
@@ -0,0 +1,41 @@
+import pykka
+import logging
+import functools
+from mopidy.core import CoreListener
+from .mem import iris
+
+# import logger
+logger = logging.getLogger(__name__)
+
+
+class IrisFrontend(pykka.ThreadingActor, CoreListener):
+    def __init__(self, config, core):
+        super().__init__()
+
+        # Pass our Mopidy config and core to the IrisCore instance
+        iris.config = config
+        iris.core = core
+
+    def on_start(self):
+        iris.start()
+
+    def on_stop(self):
+        iris.stop()
+
+    def track_playback_ended(self, tl_track, time_position):
+        if iris.ioloop and iris.config["spotify"]["enabled"]:
+            iris.ioloop.add_callback(
+                functools.partial(iris.check_for_radio_update)
+            )
+
+    def tracklist_changed(self):
+        if iris.ioloop:
+            iris.ioloop.add_callback(
+                functools.partial(iris.clean_queue_metadata)
+            )
+
+    def track_playback_started(self, tl_track):
+        if iris.ioloop and iris.config["iris"]["snapcast_enabled"]:
+            iris.ioloop.add_callback(
+                functools.partial(iris.update_snapcast_meta)
+            )
