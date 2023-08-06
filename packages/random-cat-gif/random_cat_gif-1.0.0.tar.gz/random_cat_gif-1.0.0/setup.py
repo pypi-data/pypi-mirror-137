dc8770c6f2445412134869fc7b061a81bccf3f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:17 +0000

    Add package random-cat-gif/1.0.0/random_cat_gif-1.0.0.tar.gz

---
+++ packages/random-cat-gif/random_cat_gif-1.0.0.tar.gz/random_cat_gif-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='random_cat_gif',
+    version='1.0.0',
+    description='Who doesnt like a funny cat gif?',
+    author='applenana',
+    author_email='2806922724@qq.com',
+    url='https://github.com/applenana/random_cat_gif',
+    packages=['random_cat_gif']
+)
