2a8c89115dd6c57aef7aee373d269a0e531fd8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:25 +0000

    Add package terminal-img/0.0.3/terminal-img-0.0.3.tar.gz

---
+++ packages/terminal-img/terminal-img-0.0.3.tar.gz/terminal-img-0.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Programming Language :: Python :: 3",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Utilities",
+]
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setup(
+    name="terminal-img",
+    version="0.0.3",
+    author="Pranav Baburaj",
+    author_email="i.am.pranav.baburaj@gmail.com",
+    url="https://github.com/pranavbaburaj/img",
+    py_modules=["image", "cli"],
+    description="Display images in the terminal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=classifiers,
+    python_requires=">=3.6",
+    install_requires=["pillow", "requests"],
+    entry_points={
+        "console_scripts": ["img=cli:main"],
+    },
+)
