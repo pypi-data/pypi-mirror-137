2a77427d5f9ac770a8680c9c9e0e3006c7023073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:40 +0000

    Add package beersmith-direct/1.2.1/beersmith_direct-1.2.1.tar.gz

---
+++ packages/beersmith-direct/beersmith_direct-1.2.1.tar.gz/beersmith_direct-1.2.1/beersmith_direct/__init__.py
@@ -0,0 +1,9 @@
+"""A set of functions to retrieve and save Beersmith data into MongoDB.
+"""
+
+# import main modules here, e.g., 
+from beersmith_direct.i_beersmith import BeersmithInterface
+from beersmith_direct.recipes import Recipes
+
+
+__version__ = '1.2.1'
