f5fa846d7f9108b7152ccbc53e63b94b42839dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:20 +0000

    Add package ess-cloud-utils/0.4/ess-cloud-utils-0.4.tar.gz

---
+++ packages/ess-cloud-utils/ess-cloud-utils-0.4.tar.gz/ess-cloud-utils-0.4/ess_cloud_utils/apis_client.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import requests
+from .eureka import Eureka
+
+
+class ApisClient(object):
+    @staticmethod
+    def get(service_name, endpoint):
+        url = Eureka.get_application_address(service_name) + endpoint
+        response = requests.get(url).json()
+        return response
+
+    @staticmethod
+    def post(service_name, endpoint, body=None):
+        url = Eureka.get_application_address(service_name) + endpoint
+        response = requests.post(url, data=body)
+        return response
+
+    @staticmethod
+    def put(service_name, endpoint, body=None):
+        url = Eureka.get_application_address(service_name) + endpoint
+        response = requests.put(url, data=body)
+        return response
+
+    @staticmethod
+    def delete(service_name, endpoint, body=None):
+        url = Eureka.get_application_address(service_name) + endpoint
+        response = requests.delete(url, data=body)
+        return response
+
