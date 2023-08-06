5db932d51ec21b5013cb2992c001a3272cab6ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:22 +0000

    Add package toolcli/0.1.0/toolcli-0.1.0.tar.gz

---
+++ packages/toolcli/toolcli-0.1.0.tar.gz/toolcli-0.1.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name='toolcli',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'ipdb',
+        'rich',
+    ],
+)
+
