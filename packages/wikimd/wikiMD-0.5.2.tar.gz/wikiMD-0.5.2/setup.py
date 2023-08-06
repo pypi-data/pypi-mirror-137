9b02c88113333147f670be97377b8c45a1c937ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:54 +0000

    Add package wikimd/0.5.2/wikiMD-0.5.2.tar.gz

---
+++ packages/wikimd/wikiMD-0.5.2.tar.gz/wikiMD-0.5.2/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['wikiMD']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Jinja2>=3.0.3,<4.0.0', 'wikipedia>=1.4.0,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['wikimd = wikiMD.script:run']}
+
+setup_kwargs = {
+    'name': 'wikimd',
+    'version': '0.5.2',
+    'description': 'Turn Wikipedia in markmap friendly markdown',
+    'long_description': None,
+    'author': 'John Capobianco',
+    'author_email': 'ptcapo@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
