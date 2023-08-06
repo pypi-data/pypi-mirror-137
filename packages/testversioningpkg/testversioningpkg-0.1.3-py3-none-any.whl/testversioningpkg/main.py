ee39211c13f328c2aff1967c7ca559a7b2d9bf72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:46 +0000

    Add package testversioningpkg/0.1.3/testversioningpkg-0.1.3-py3-none-any.whl

---
+++ packages/testversioningpkg/testversioningpkg-0.1.3-py3-none-any.whl/testversioningpkg/main.py
@@ -0,0 +1,62 @@
+import importlib
+
+def check_module_version(version=1, file_name="", event_name=None):
+    version_func = None
+    try:
+        if version < 0:
+            return 0, "Version 0 reached, no matched function"
+        if event_name != None:
+            version_func = importlib.import_module(f'testversioningpkg.customs.{event_name.lower()}.{file_name}')
+        else:
+            version_func = importlib.import_module('testversioningpkg.versions.v{}.{}'.format(version, file_name))
+    except ModuleNotFoundError as e:
+        version_func, version = check_module_version(version=version-1, file_name=file_name)
+    return version_func, version
+
+def get_floorplans(version=1, event_name=None):
+    # Check if module is valid by its version
+    valid_module, version = check_module_version(version,"floorplan", event_name)
+    # If look up process is done and ended up with no match result
+    if not valid_module:
+        result = "No module match with requested version"
+    else:
+        try:
+            # Get the data by its function
+            result = valid_module.get_floorplans()
+        except AttributeError:
+            # Check another version if the function is not exist in module
+            result = get_floorplans(version=version-1)
+    return result
+
+def get_routes(version=1, event_name=None):
+    valid_module, version = check_module_version(version,"floorplan", event_name)
+    if not valid_module:
+        result = "No module match with requested version"
+    else:
+        try:
+            result = valid_module.get_routes()
+        except AttributeError:
+            result = get_routes(version=version-1)
+    return result
+
+def get_agendas(version=1, event_name=None):
+    valid_module, version = check_module_version(version,"agenda", event_name)
+    if not valid_module:
+        result = "No module match with requested version"
+    else:
+        try:
+            result = valid_module.get_agenda_list_view()
+        except AttributeError:
+            result = get_agendas(version=version-1)
+    return result
+
+def get_session(version=1, event_name=None):
+    valid_module, version = check_module_version(version,"agenda", event_name)
+    if not valid_module:
+        result = "No module match with requested version"
+    else:
+        try:
+            result = valid_module.get_agenda_session()
+        except AttributeError:
+            result = get_session(version=version-1)
+    return result
