907e2079c1079ecb439a9221ced5f8965344f10c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:25 +0000

    Add package mongodb-connector/0.1.2/mongodb_connector-0.1.2.tar.gz

---
+++ packages/mongodb-connector/mongodb_connector-0.1.2.tar.gz/mongodb_connector-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mongodb_connector']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['lark-parser>=0.12.0', 'pymongo>=4.0.1']
+
+setup_kwargs = {
+    'name': 'mongodb-connector',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7',
+}
+
+
+setup(**setup_kwargs)
