b07158bf3a899a581cd2a4dc2fc777f4ddc88e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:20 +0000

    Add package awpy/1.0/awpy-1.0.tar.gz

---
+++ packages/awpy/awpy-1.0.tar.gz/awpy-1.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="awpy",
+    version="1.0",
+    packages=find_packages(),
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=[
+        "pandas>=0.25.3",
+        "numpy>=1.18.1",
+        "scipy>=1.4.1",
+        "matplotlib>=3.1.2",
+        "networkx>=2.6.3",
+        "textdistance>=4.2.0",
+        "imageio>=2.9.0",
+        "tqdm>=4.55.2",
+    ],
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        "": [
+            "*.go",
+            "data/map/*.png",
+            "data/map/*.json",
+            "data/nav/*.txt",
+            "data/nav/*.csv",
+            "*.mod",
+            "*.sum",
+        ]
+    },
+    # metadata to display on PyPI
+    author="Peter Xenopoulos",
+    author_email="xenopoulos@nyu.edu",
+    description="Counter-Strike: Global Offensive data parsing, analysis and visualization functions",
+    keywords="esports sports-analytics csgo counter-strike",
+    url="https://github.com/pnxenopoulos/awpy",
+    project_urls={
+        "Issues": "https://github.com/pnxenopoulos/awpy/issues",
+        "Documentation": "https://awpy.readthedocs.io/en/latest/?badge=latest",
+        "GitHub": "https://github.com/pnxenopoulos/awpy/",
+    },
+    classifiers=["License :: OSI Approved :: MIT License"],
+)
