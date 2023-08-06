16c23004fad1dfea79308cb334ce01189e8cbcfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:27:42 +0000

    Add package twarc-timeline-archive/0.1.1/twarc-timeline-archive-0.1.1.tar.gz

---
+++ packages/twarc-timeline-archive/twarc-timeline-archive-0.1.1.tar.gz/twarc-timeline-archive-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='twarc-timeline-archive',
+    version='0.1.1',
+    url='https://github.com/docnow/twarc-timeline-archive',
+    author='Ed Summers',
+    author_email='ehs@pobox.com',
+    py_modules=['twarc_timelines'],
+    description='A twarc plugin to collect the timelines of a list of users',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.3',
+    install_requires=['twarc>=2.0.8'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    entry_points='''
+        [twarc.plugins]
+        timeline_archive=twarc_timeline_archive:timeline_archive
+    '''
+)
