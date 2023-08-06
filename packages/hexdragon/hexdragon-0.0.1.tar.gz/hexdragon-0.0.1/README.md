46ff09fc65ac7a4b1fac121976833c8f66b0db62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:33 +0000

    Add package hexdragon/0.0.1/hexdragon-0.0.1.tar.gz

---
+++ packages/hexdragon/hexdragon-0.0.1.tar.gz/hexdragon-0.0.1/README.md
@@ -0,0 +1,11 @@
+# Hexdragon
+
+This is an art project. 
+
+Print a file as coloured hexadecimal values to console.
+
+```bash
+> python3 -m hexagon <file>
+```
+
+![console](./docs/console.png)
---
+++ packages/hexdragon/hexdragon-0.0.1.tar.gz/hexdragon-0.0.1/hexdragon/__main__.py
@@ -0,0 +1,44 @@
+import argparse, sys
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Print a file as coloured hexadecimal values to console"
+    )
+    parser.add_argument("inputfile", action="store", help="Input file")
+    parser.add_argument("--fill", action="store", help="byte to fill [default: 0xff]")
+    args = parser.parse_args()
+
+    with open(args.inputfile, "rb") as in_file:
+        data_array = []
+        data = in_file.read(1)
+        data_array.append(data)
+        while data:
+            data = in_file.read(1)
+            data_array.append(data)
+
+        in_file.close()
+
+        hex_array = [hex(int.from_bytes(byte, "big")) for byte in data_array]
+        length = len(hex_array)
+
+        n = length
+        while n % n ** 0.5 != 0:
+            n += 1
+        sqrt_n = int(n ** 0.5)
+
+        padding = n - length
+        for i in range(padding):
+            hex_array.append(args.fill if args.fill is not None else hex(0xff))
+
+        chunks = [hex_array[i : i + sqrt_n] for i in range(0, len(hex_array), sqrt_n)]
+
+        for chunk in chunks:
+            for i in chunk:
+                hexa = i if len(i) != 3 else i[0:2] + "0" + i[2]
+                sys.stdout.write(f"\033[38;5;{int(i, 16)}m{hexa}\u001b[0m")
+            sys.stdout.write("\n")
+
+
+if __name__ == "__main__":
+    main()
