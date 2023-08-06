f4f6763502291b4aa9335cc5584dc9131d1568b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:47 +0000

    Add package sw360/1.2.0/sw360-1.2.0.tar.gz

---
+++ packages/sw360/sw360-1.2.0.tar.gz/sw360-1.2.0/sw360/__init__.py
@@ -0,0 +1,12 @@
+# -------------------------------------------------------------------------------
+# (c) 2019-2022 Siemens AG
+# All Rights Reserved.
+# Author: thomas.graf@siemens.com
+#
+# Licensed under the MIT license.
+# SPDX-License-Identifier: MIT
+# -------------------------------------------------------------------------------
+
+__version__ = (1, 2, 0)
+
+from .sw360_api import SW360, SW360Error  # noqa: F401
