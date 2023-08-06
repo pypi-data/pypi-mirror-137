6734d2e544f013a22c8a410a5c7b3282feb6185c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:56 +0000

    Add package relevanceai/0.32.1/RelevanceAI-0.32.1.tar.gz

---
+++ packages/relevanceai/RelevanceAI-0.32.1.tar.gz/RelevanceAI-0.32.1/relevanceai/api/client.py
@@ -0,0 +1,20 @@
+"""Batch client to allow for batch insertions/retrieval and encoding
+"""
+from typing import Callable
+
+from relevanceai.api.batch.batch_insert import BatchInsertClient
+from relevanceai.api.batch.batch_retrieve import BatchRetrieveClient
+from relevanceai.api.endpoints.client import APIClient
+from relevanceai.api.batch.chunk import Chunker
+from relevanceai.utils import Utils
+
+
+class BatchAPIClient(BatchInsertClient, Utils, BatchRetrieveClient, APIClient):
+    """Batch API client"""
+
+    def batch_insert(self):
+        raise NotImplemented
+
+    def batch_get_and_edit(self, dataset_id: str, chunk_size: int, bulk_edit: Callable):
+        """Batch get the documents and return the documents"""
+        raise NotImplemented
