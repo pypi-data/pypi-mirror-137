2e0f7996261d6e514864e7c5fd67fbcc095cecb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:56 +0000

    Add package hestia-earth-models/0.22.3/hestia-earth-models-0.22.3.tar.gz

---
+++ packages/hestia-earth-models/hestia-earth-models-0.22.3.tar.gz/hestia-earth-models-0.22.3/hestia_earth/models/impact_assessment/pre_checks/__init__.py
@@ -0,0 +1,15 @@
+from os.path import dirname, abspath
+import sys
+
+from hestia_earth.models.utils import _run_in_serie
+from hestia_earth.models.impact_assessment.pre_checks import cycle
+
+CURRENT_DIR = dirname(abspath(__file__)) + '/'
+sys.path.append(CURRENT_DIR)
+
+MODELS = [
+    cycle.run
+]
+
+
+def run(data: dict): return _run_in_serie(data, MODELS)
