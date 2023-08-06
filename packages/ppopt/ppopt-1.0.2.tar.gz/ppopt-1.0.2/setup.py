814969b4b9f5397441d3d43c22ecd5bb1b1648f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:03 +0000

    Add package ppopt/1.0.2/ppopt-1.0.2.tar.gz

---
+++ packages/ppopt/ppopt-1.0.2.tar.gz/ppopt-1.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+__version__ = "1.0.2"
+
+short_desc = (
+    "Extensible Multiparametric Solver in Python"
+)
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='ppopt',
+    version=__version__,
+    author='Dustin R. Kenefake',
+    author_email='Dustin.Kenefake@tamu.edu',
+    description=short_desc,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url='https://github.com/TAMUparametric/PPOPT',
+    install_requires=["numpy",
+                      "matplotlib",
+                      "scipy",
+                      "numba",
+                      "gurobipy",
+                      "pytest",
+                      "setuptools",
+                      "psutil",
+                      "pathos",
+                      "plotly"],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'}
+)
