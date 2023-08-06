824a1b1a455449c95c55d69116faafb992b95379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:47 +0000

    Add package testversioningpkg/0.1.3/testversioningpkg-0.1.3.tar.gz

---
+++ packages/testversioningpkg/testversioningpkg-0.1.3.tar.gz/testversioningpkg-0.1.3/testversioningpkg/customs/event_b/agenda.py
@@ -0,0 +1,22 @@
+def get_agenda_list_view():
+    list_view = {"loaderData":{
+        "peopleData":[{
+            "date":"22 Dec",
+            "desc":"EVENT B Bill Gates, the main speaker will be sharing on the progress of his foundations work in nuclear energy",
+            "favorited":None,
+            "has_livestream":0,
+            "id_object":4545,
+            "loc":"Yamaha Hall",
+            "local_date":"22 Dec",
+            "local_time":"07:00 AM",
+            "name":"Fireside chat with Bill Gates",
+            "speaker_photos":["https://buzz-images.jublia.com/agenda/475/8281_0.png","",""],
+            "tags":[],
+            "time":"08:00 AM",
+            "timestamp":"2020-12-22 08:00:00",
+            "track":"Yamaha Hall",
+            "type":"session"
+        }],
+        "resultCount":1}
+    }
+    return list_view
---
+++ packages/testversioningpkg/testversioningpkg-0.1.3.tar.gz/testversioningpkg-0.1.3/testversioningpkg/versions/v4/floorplan.py
@@ -0,0 +1,17 @@
+def get_routes():
+    routes = {
+        'distance': 2925.17,
+        'found': 1,
+        'path': [
+            {'floorplan': 251, 'x': 1869, 'y': 1840},
+            {'floorplan': 251, 'x': 1869, 'y': 1911},
+            {'floorplan': 251, 'x': 2117, 'y': 1911},
+            {'floorplan': 251, 'x': 2117, 'y': 2052},
+            {'floorplan': 251, 'x': 2117, 'y': 3228},
+            {'floorplan': 251, 'x': 2294, 'y': 3228},
+            {'floorplan': 255, 'x': 2711, 'y': 2220},
+            {'floorplan': 255, 'x': 2920, 'y': 2475},
+            {'floorplan': 255, 'x': 2612, 'y': 2813}
+        ]
+    }
+    return routes
