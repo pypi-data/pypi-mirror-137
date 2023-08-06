eed186fe61fb7568e5449a93d529a7e6986f6773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:52 +0000

    Add package torchrec-nightly-cpu/2022.2.7/torchrec_nightly_cpu-2022.2.7-py39-none-any.whl

---
+++ packages/torchrec-nightly-cpu/torchrec_nightly_cpu-2022.2.7-py39-none-any.whl/torchrec/datasets/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torchrec.datasets.criteo  # noqa
+import torchrec.datasets.movielens  # noqa
+import torchrec.datasets.random  # noqa
+import torchrec.datasets.utils  # noqa
