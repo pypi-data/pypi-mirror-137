d166ee30e4aef0efd77c6dd418860cf03e93e5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:55 +0000

    Add package nowcasting-forecast/0.0.49/nowcasting_forecast-0.0.49-py3-none-any.whl

---
+++ packages/nowcasting-forecast/nowcasting_forecast-0.0.49-py3-none-any.whl/nowcasting_forecast/batch.py
@@ -0,0 +1,37 @@
+""" Using ManagerLive to make batches """
+import logging
+from datetime import datetime
+
+from nowcasting_dataset.manager.manager_live import ManagerLive
+
+from nowcasting_forecast.utils import floor_30_minutes_dt
+
+logger = logging.getLogger(__name__)
+
+
+def make_batches(
+    config_filename: str = "nowcasting_forecast/config/mvp_v0.yaml",
+    t0_datetime_utc: datetime = None,
+):
+    """Make batches from config file"""
+
+    logger.info(f"Making batches using configuration file: {config_filename}")
+
+    if t0_datetime_utc is None:
+        t0_datetime_utc = floor_30_minutes_dt(datetime.utcnow())  # add timezone
+
+    # load config
+    manager = ManagerLive()
+    manager.load_yaml_configuration(config_filename, set_git=False)
+
+    # make location file
+    manager.initialize_data_sources(names_of_selected_data_sources=["gsp", "nwp"])
+    manager.create_files_specifying_spatial_and_temporal_locations_of_each_example(
+        t0_datetime=t0_datetime_utc
+    )
+
+    # remove gsp as a datasource
+    manager.data_sources.pop("gsp")
+
+    # make batches
+    manager.create_batches()
