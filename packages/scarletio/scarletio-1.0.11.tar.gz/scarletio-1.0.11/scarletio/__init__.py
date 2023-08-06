6ee09aacfdc2f618056e94e4c655892ff8292576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:05 +0000

    Add package scarletio/1.0.11/scarletio-1.0.11.tar.gz

---
+++ packages/scarletio/scarletio-1.0.11.tar.gz/scarletio-1.0.11/scarletio/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '1.0.11'
+
+from .core import *
+from .utils import *
+
+__all__ = (
+    *core.__all__,
+    *utils.__all__,
+)
+
+from .utils.export_include import check_satisfaction
+check_satisfaction()
