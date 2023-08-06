a7e871ab2499bbebc4f5037a5b5c4cf760956d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:37 +0000

    Add package murefi/5.1.0/murefi-5.1.0-py3-none-any.whl

---
+++ packages/murefi/murefi-5.1.0-py3-none-any.whl/murefi/__init__.py
@@ -0,0 +1,7 @@
+from . core import ParameterMapping
+from . datastructures import ShapeError, DtypeError, Timeseries, Replicate, Dataset, save_dataset, load_dataset
+from . import objectives
+from . ode import BaseODEModel
+
+
+__version__ = "5.1.0"
