c3528fa30d9e72e0f1ca336e253aa562d6f8deca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:58 +0000

    Add package walax/0.1.6/walax-0.1.6.tar.gz

---
+++ packages/walax/walax-0.1.6.tar.gz/walax-0.1.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="walax",
+    version="0.1.6",
+    author="Matt Barry",
+    author_email="matt@hazelmollusk.org",
+    description="Walax Django API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hazelmollusk/django-walax",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
