66b3942820ca9fd2291d240f7fe68776a280b953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:35:54 +0000

    Add package st-card-component/0.0.8/st_card_component-0.0.8.tar.gz

---
+++ packages/st-card-component/st_card_component-0.0.8.tar.gz/st_card_component-0.0.8/st_card_component/frontend/build/precache-manifest.001cb2f57278730657c6acb7b3e0f26e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2c681cb97830528c9a286ef33c37b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be5cb160731a41a76b92",
+    "url": "./static/js/2.2e142390.chunk.js"
+  },
+  {
+    "revision": "fdc04156fe2caf5da02c72a4c7efb329",
+    "url": "./static/js/2.2e142390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8cc31d3ed30b21667af",
+    "url": "./static/js/main.a0c8435a.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
