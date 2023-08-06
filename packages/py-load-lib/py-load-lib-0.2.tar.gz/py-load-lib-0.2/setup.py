547bdf66bae44e335bbbd8d9aa5909b1be7cb791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:03 +0000

    Add package py-load-lib/0.2/py-load-lib-0.2.tar.gz

---
+++ packages/py-load-lib/py-load-lib-0.2.tar.gz/py-load-lib-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='py-load-lib',
+    version='0.2',
+    license='MIT',
+    author="Talwat",
+    author_email='talwat321@gmail.com',
+    py_modules=["py_load"],
+    python_requires='>=3.6, <4',
+    keywords='loading, bar, loadingbar, loading bar',
+    url='https://github.com/talwat/py-load',
+)
