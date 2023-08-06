c4ab449ebdeb065cc62c6e21d484fa4188e4d696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:04 +0000

    Add package mallgroup-python-lib/0.0.5/mallgroup_python_lib-0.0.5.tar.gz

---
+++ packages/mallgroup-python-lib/mallgroup_python_lib-0.0.5.tar.gz/mallgroup_python_lib-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mallgroup_python_lib",
+    version="0.0.5",
+    description="Official Mallgroup Library",
+    author="Data Engineering Mallgroup",
+    author_email="dataengineering@mallgroup.com",
+    py_modules=["mallgroup_python_lib"],
+    packages=find_packages(include=['mallgroup_python_lib', 'mallgroup_python_lib.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[],
+    extras_require={"dev": ["pytest"]},
+)
