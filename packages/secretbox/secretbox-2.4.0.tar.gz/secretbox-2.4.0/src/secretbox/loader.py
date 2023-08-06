a26a4a94f93e6a3a65bf1aae721ffd9cddb10698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:36 +0000

    Add package secretbox/2.4.0/secretbox-2.4.0.tar.gz

---
+++ packages/secretbox/secretbox-2.4.0.tar.gz/secretbox-2.4.0/src/secretbox/loader.py
@@ -0,0 +1,15 @@
+from abc import ABC
+from typing import Any
+from typing import Dict
+
+
+class Loader(ABC):
+    """Abstract Base Class for all loaders"""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.loaded_values: Dict[str, str] = {}
+
+    def load_values(self, **kwargs: Any) -> bool:
+        """Override with loading optionation, store within self.loaded_values"""
+        raise NotImplementedError()  # pragma: no cover
