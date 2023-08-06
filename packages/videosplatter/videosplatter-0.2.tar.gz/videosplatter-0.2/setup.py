5bb9193f9f831194980d51a3357e7235e370fcc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:46 +0000

    Add package videosplatter/0.2/videosplatter-0.2.tar.gz

---
+++ packages/videosplatter/videosplatter-0.2.tar.gz/videosplatter-0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='videosplatter',
+    version='0.2',
+    description='Splat videos',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/ryanfox/videosplatter',
+    author='Ryan Fox',
+    author_email='ryan@foxrow.com',
+    license='ACSL',
+    packages=['videosplatter'],
+    install_requires=[
+        'imageio-ffmpeg==0.4.5',
+    ],
+    entry_points= {
+        'console_scripts': ['videosplatter=videosplatter.splat:main'],
+    },
+    zip_safe=False,
+    python_requires='>=3.6',
+)
