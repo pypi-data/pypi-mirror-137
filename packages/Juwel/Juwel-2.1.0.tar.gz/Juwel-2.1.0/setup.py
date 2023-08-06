d64447dc37aafbc50938ae2874556e392fbde3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:58 +0000

    Add package Juwel/2.1.0/Juwel-2.1.0.tar.gz

---
+++ packages/Juwel/Juwel-2.1.0.tar.gz/Juwel-2.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='Juwel',
+   license='GPL',
+   version='2.1.0',
+   description='Sidecar file generator',
+   author='Gefan Qian',
+   author_email='qian@cbs.mpg.de',
+   packages=find_packages(),
+   install_requires=['tk', 'tkcalendar'],
+   python_requires='>=3.6',
+
+   include_package_data=True,
+   package_data={'Juwel': []},
+
+   entry_points={
+        'console_scripts': [
+            'juwel=Juwel.Main:main',
+        ],
+    },
+)
