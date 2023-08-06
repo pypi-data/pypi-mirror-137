8c9274ebe90a944ebd9c91b7eed3032faa0a17eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:46 +0000

    Add package baizhanMath20002/1.0/baizhanMath20002-1.0.tar.gz

---
+++ packages/baizhanMath20002/baizhanMath20002-1.0.tar.gz/baizhanMath20002-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+name='baizhanMath20002', # 对外我们模块的名字
+version='1.0', # 版本号
+description='这是第一个对外发布的模块，测试哦', #描述
+author='jj', # 作者
+author_email='gaoqi110@163.com',#个人的邮箱
+py_modules=['baizhansupeq3.demo1','baizhansupeq3.demo2'] # 要发布的模块
+)
