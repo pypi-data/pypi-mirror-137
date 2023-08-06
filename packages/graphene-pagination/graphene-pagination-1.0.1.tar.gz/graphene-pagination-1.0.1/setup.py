3bffaa6421d96d5ebfabcac8349c4fa7502ecc47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:54 +0000

    Add package graphene-pagination/1.0.1/graphene-pagination-1.0.1.tar.gz

---
+++ packages/graphene-pagination/graphene-pagination-1.0.1.tar.gz/graphene-pagination-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name="graphene-pagination",
+    version="1.0.1",
+    author="Dmitry Kalinin",
+    packages=["graphene_pagination"],
+    url="https://github.com/null-none/graphene-pagination",
+    keywods=[        
+        "graphene",
+        "django",
+        "pagination"
+        "graphene pagination"
+    ],
+    install_requires=[
+        "graphene-django",
+    ],
+)
