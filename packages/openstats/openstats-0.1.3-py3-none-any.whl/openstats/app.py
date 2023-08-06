3d767f0ca0eeb83b293c4a8d92892fddbb4e1310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:11 +0000

    Add package openstats/0.1.3/openstats-0.1.3-py3-none-any.whl

---
+++ packages/openstats/openstats-0.1.3-py3-none-any.whl/openstats/app.py
@@ -0,0 +1,32 @@
+"""
+Main file to run the streamlit app
+"""
+import streamlit as st
+from levy.config import Config
+
+from openstats.components import Builder
+from openstats.theme import write_theme
+
+
+def stats(cfg: Config):
+    """
+    Build the app
+    """
+    builder = Builder(cfg)
+
+    st.title(cfg.title)
+
+    builder.sidebar()
+    builder.stars_component()
+    st.markdown("---")
+    builder.good_first_issues_component()
+    st.markdown("---")
+    builder.contributors_component()
+    st.markdown("---")
+    builder.traffic_component()
+
+
+def run():
+    config = Config.read_file("openstats.yaml")
+    write_theme(config)
+    stats(config)
