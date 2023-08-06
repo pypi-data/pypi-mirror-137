b8db56dea3d7f1adf25f8b180b0ba804035aba04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:08 +0000

    Add package qiboconnection/0.0.2/qiboconnection-0.0.2-py3-none-any.whl

---
+++ packages/qiboconnection/qiboconnection-0.0.2-py3-none-any.whl/qiboconnection/typings/device.py
@@ -0,0 +1,50 @@
+import enum
+from typing import Literal, TypedDict, Union
+
+
+class DeviceStatus(enum.Enum):
+    available = "available"
+    busy = "busy"
+    offline = "offline"
+
+
+class DeviceType(enum.Enum):
+    QUANTUM = "quantum"
+    SIMULATOR = "simulator"
+
+
+class QuantumDeviceCharacteristicsInput(TypedDict):
+    type: Literal[DeviceType.QUANTUM, "quantum"]
+    description: str
+
+
+class SimulatorDeviceCharacteristicsInput(TypedDict):
+    type: Literal[DeviceType.SIMULATOR, "simulator"]
+    cpu: str
+    gpu: str
+    os: str
+    kernel: str
+    ram: str
+
+
+class DeviceInput(TypedDict):
+    device_id: int
+    device_name: str
+    status: Union[str, DeviceStatus]
+    channel_id: int
+
+
+class CalibrationDetailsInput(TypedDict, total=False):
+    elapsed_time: int
+    t1: int
+    frequency: int
+
+
+class SimulatorDeviceInput(DeviceInput, total=False):
+    characteristics: SimulatorDeviceCharacteristicsInput
+
+
+class QuantumDeviceInput(DeviceInput, total=False):
+    last_calibration_time: str
+    characteristics: QuantumDeviceCharacteristicsInput
+    calibration_details: CalibrationDetailsInput
