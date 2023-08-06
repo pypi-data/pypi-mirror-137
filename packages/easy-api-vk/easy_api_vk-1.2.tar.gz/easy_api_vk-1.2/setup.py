114ee314fb6deb3d116d26594a84b686b34d5b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:27 +0000

    Add package easy-api-vk/1.2/easy_api_vk-1.2.tar.gz

---
+++ packages/easy-api-vk/easy_api_vk-1.2.tar.gz/easy_api_vk-1.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='easy_api_vk',
+	version='1.2',
+	author='Dimouse',
+	author_email='brazhenk0@icloud.com',
+	description='easy-api-vk',
+	packages=['easy_api_vk'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
