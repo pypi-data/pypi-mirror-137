2bdd0985f303db56197747ff30de775b9f3b1bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:02 +0000

    Add package botocove/1.5.0/botocove-1.5.0-py3-none-any.whl

---
+++ packages/botocove/botocove-1.5.0-py3-none-any.whl/botocove/cove_types.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from typing import Any, Dict, Generic, List, Optional, TypedDict, TypeVar
+
+from mypy_boto3_organizations.literals import AccountStatusType
+
+R = TypeVar("R")
+
+
+@dataclass
+class CoveSessionInformation(Generic[R]):
+    Id: str
+    RoleName: str
+    AssumeRoleSuccess: bool = False
+    Arn: Optional[str] = None
+    Email: Optional[str] = None
+    Name: Optional[str] = None
+    Status: Optional[AccountStatusType] = None
+    RoleSessionName: Optional[str] = None
+    Policy: Optional[str] = None
+    PolicyArns: Optional[List[str]] = None
+    Result: Optional[R] = None
+    ExceptionDetails: Optional[Exception] = None
+
+
+CoveResults = List[CoveSessionInformation]
+
+
+class CoveFunctionOutput(TypedDict):
+    Results: CoveResults
+    Exceptions: CoveResults
+
+
+class CoveOutput(TypedDict):
+    Results: List[Dict[str, Any]]
+    Exceptions: List[Dict[str, Any]]
+    FailedAssumeRole: List[Dict[str, Any]]
