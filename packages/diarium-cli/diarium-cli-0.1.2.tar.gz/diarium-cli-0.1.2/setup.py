713c5f8e698ffccc377304dc42a0afccaa0a1331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:17 +0000

    Add package diarium-cli/0.1.2/diarium-cli-0.1.2.tar.gz

---
+++ packages/diarium-cli/diarium-cli-0.1.2.tar.gz/diarium-cli-0.1.2/setup.py
@@ -0,0 +1,19 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="diarium-cli",
+    version="0.1.2",
+    description="CLI tool for journaling app Diarium",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Yelov",
+    packages=find_packages(),
+    include_package_date=True,
+    install_requires=open("requirements.txt").read().splitlines(),
+    license="Apache License 2.0",
+    include_package_data=True,
+    entry_points="""
+        [console_scripts]
+        diarium-cli=src.main:cli
+    """,
+)
