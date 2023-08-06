adcaa3e9b144f373e4cb11344c75c396809099cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:42 +0000

    Add package PyAdder/0.0.4/PyAdder-0.0.4.tar.gz

---
+++ packages/PyAdder/PyAdder-0.0.4.tar.gz/PyAdder-0.0.4/adder/tests/test_one_number.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+import math
+from numpy import inf, isinf, nan, isnan
+import adder
+
+class TestOneNumber(TestCase):
+
+    def test_floats(self):
+        for num in [1617161771.7650001, math.pi, math.pi**100,
+                    math.pi**-100, 3.1]:
+            self.assertEqual(adder.run(num), num)
+
+    def test_ints(self):
+        for num in [-1,0,1]:
+            self.assertEqual(adder.run(num), num)
+
+    def test_deplorables(self):
+        self.assertTrue(isinf(adder.run(inf)))
+        self.assertTrue(isnan(adder.run(nan)))
+
+class TestTwoNumbers(TestCase):
+
+    def test_floats(self):
+        self.assertEqual(adder.run([2.0,3.0]), 5.0)
