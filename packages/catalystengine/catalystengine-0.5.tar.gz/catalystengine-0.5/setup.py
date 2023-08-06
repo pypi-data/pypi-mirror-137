649b797c96dd74825070d59cdbb4e7004fc0b6bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:58 +0000

    Add package catalystengine/0.5/catalystengine-0.5.tar.gz

---
+++ packages/catalystengine/catalystengine-0.5.tar.gz/catalystengine-0.5/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'catalystengine',
+  packages = ['catalystengine'],
+  version = '0.5',
+  license='MIT',
+  description = 'Terminal based game engine',
+  author = 'Azearia',
+  author_email = 'azearia5@gmail.com',
+  url = 'https://github.com/Azearia',
+  download_url = 'https://github.com/Azearia/catalystengine/archive/refs/tags/0.5.tar.gz',
+  keywords = ['GAMEENGINE', 'TERMINAL'],
+  install_requires=[
+          'audioplayer',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
