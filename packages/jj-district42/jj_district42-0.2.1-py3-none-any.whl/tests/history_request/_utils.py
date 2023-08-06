4da1ca82f5b3037a4f7c3614da143e57b5191f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:05 +0000

    Add package jj-district42/0.2.1/jj_district42-0.2.1-py3-none-any.whl

---
+++ packages/jj-district42/jj_district42-0.2.1-py3-none-any.whl/tests/history_request/_utils.py
@@ -0,0 +1,30 @@
+from typing import Dict, Optional
+
+from jj.mock import HistoryRequest
+from multidict import CIMultiDict, CIMultiDictProxy, MultiDict, MultiDictProxy
+
+__all__ = ("make_history_request",)
+
+
+def make_history_request(*,
+                         method: str = "GET",
+                         path: str = "/",
+                         segments: Optional[Dict[str, str]] = None,
+                         params: Optional[MultiDict] = None,
+                         headers: Optional[CIMultiDict] = None,
+                         body: bytes = b"") -> HistoryRequest:
+    if segments is None:
+        segments = {}
+    if params is None:
+        params = MultiDict()
+    if headers is None:
+        headers = CIMultiDict()
+
+    return HistoryRequest(
+        method=method,
+        path=path,
+        segments=segments,
+        params=MultiDictProxy(params),
+        headers=CIMultiDictProxy(headers),
+        body=body,
+    )
