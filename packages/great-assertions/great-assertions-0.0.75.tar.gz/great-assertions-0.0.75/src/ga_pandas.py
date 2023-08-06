3ad1fe8b5655b5e4f77faf3b31045b6aa0353784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:20 +0000

    Add package great-assertions/0.0.75/great-assertions-0.0.75.tar.gz

---
+++ packages/great-assertions/great-assertions-0.0.75.tar.gz/great-assertions-0.0.75/src/ga_pandas.py
@@ -0,0 +1,145 @@
+from src.ga_dataframe import GADataFrame
+
+
+class GAPandas(GADataFrame):
+    """Great Assertions."""
+
+    def __init__(self, df) -> None:
+        """Great Assertions."""
+        super().__init__(df.copy(deep=True))
+
+    @property
+    def row_count(self) -> int:
+        """
+        Calculate the row count.
+
+        :returns: The row count value
+
+        """
+        return len(self.df)
+
+    def column_mean(self, column: str) -> int:
+        """
+        Calculate the mean of a Column.
+
+        :returns: The mean value of the column provided
+        """
+
+        return self.df[column].mean()
+
+    def column_min(self, column: str) -> int:
+        """
+        Calculate the min of a Column.
+
+        :returns: The min value of the column provided
+        """
+
+        return self.df[column].min()
+
+    def column_max(self, column: str) -> int:
+        """
+        Calculate the max of a Column.
+
+        :returns: The max value of the column provided
+        """
+
+        return self.df[column].max()
+
+    def check_regex(self, column: str, regex: str):
+        """
+        Filter column values which DO NOT conform to a regex
+
+        :returns: The dataframe of results
+        """
+
+        filtered_dataframe = self.df[
+            self.df[column].astype(str).str.match(regex).eq(False)
+        ]
+        return GAPandas(filtered_dataframe)
+
+    def first(self, column: str) -> str:
+        """
+        The first value found in the column
+
+        :returns: The string of the value
+        """
+
+        return str(self.df[column].values[0])
+
+    def is_in_set(self, column: str, value_set: set, ignore_case: bool):
+        """
+        Checks to see if the value_set is in the provided column.
+
+        :returns: The dataframe of results
+        """
+
+        if ignore_case:
+            value_set_lower = set(map(lambda x: x.lower(), value_set))
+            filtered_dataframe = self.df[
+                ~self.df[column].str.lower().isin(value_set_lower)
+            ].eq(False)
+        else:
+            filtered_dataframe = self.df[~self.df[column].isin(value_set)].eq(False)
+
+        return GAPandas(filtered_dataframe)
+
+    def unique_list(self, column: str) -> list:
+        """
+        Gets the unique values from a provided column.
+
+        :returns: The unique values in a list
+        """
+
+        return self.df[column].unique().tolist()
+
+    def drop_duplicates(self):
+        """
+        Drops any duplicates in the DataFrame.
+
+        :returns: The unique values in a list
+        """
+
+        self.df = self.df.drop_duplicates()
+        return self
+
+    def expect_frames_equal(self, right, check_dtype=True, check_index=True):
+        """Compares two DataFrames."""
+        from pandas.testing import assert_frame_equal
+
+        if check_index is False:
+            assert_frame_equal(
+                self.df.reset_index(drop=True), right.reset_index(drop=True)
+            )
+        else:
+            assert_frame_equal(
+                self.df,
+                right,
+                check_dtype=check_dtype,
+            )
+
+    def filter_out(self, column: str, value: object):
+        """Filters out if criteria not met."""
+        self.df = self.df[self.df[column] != value]
+
+        return self
+
+    def filter_none(self, column: str):
+        self.df = self.df[self.df[column].notnull()]
+
+        return self
+
+    def not_greater(self, column: str, value: object):
+        """Filters out if criteria is not greater."""
+        self.df = self.df[self.df[column] <= value]
+
+        return self
+
+    def where(self, column: str, value: object):
+        """Returns a new data set based on where criteria."""
+        self.df = self.df[self.df[column] == value]
+
+        return self
+
+    def get_column(self, column):
+        """Returns Dataframe with only column."""
+        return GAPandas(self.df[column])
---
+++ packages/great-assertions/great-assertions-0.0.75.tar.gz/great-assertions-0.0.75/tests/test_great_assertions_reporting.py
@@ -0,0 +1,167 @@
+import unittest
+import sys
+from great_assertions import GreatAssertionResult, GreatAssertions, NoValueFoundError
+from pyspark.sql import SparkSession
+from pandas.testing import assert_frame_equal
+import pandas as pd
+import pytest
+
+spark = SparkSession.builder.getOrCreate()
+
+
+class SaveTest(GreatAssertions):
+
+    __test__ = False
+
+    @classmethod
+    def setUpClass(cls):
+        import os
+        import shutil
+
+        dirpath = os.path.join("spark-warehouse")
+        shutil.rmtree(dirpath, ignore_errors=True)
+
+    def test_fail1(self):
+        df = spark.createDataFrame([{"col_1": 100}])
+        self.expect_table_row_count_to_equal(df, 3)
+
+    def test_fail2(self):
+        df = spark.createDataFrame([{"col_1": 100}, {"col_1": 300}])
+        self.expect_column_values_to_be_between(df, "col_1", 101, 301)
+
+    def test_fail3(self):
+        self.assertFalse(True)
+
+    def test_pass1(self):
+        df = spark.createDataFrame([{"col_1": 100}])
+        self.expect_table_row_count_to_equal(df, 1)
+
+    def test_pass2(self):
+        df = spark.createDataFrame([{"col_1": ""}])
+        self.expect_date_range_to_be_more_than(df, "col_1", "1899-12-31")
+
+    @unittest.skip("demonstrating skipping")
+    def test_skip(self):
+        pass
+
+    def test_error(self):
+        self.no_method_here()
+
+    def test_catch_error(self):
+        df = pd.DataFrame({"col_1": [None, None, None, None]})
+
+        try:
+            self.expect_column_values_to_be_between(
+                df, "col_1", min_value=99, max_value=301
+            )
+        except (NoValueFoundError):
+            pass
+
+
+def _run_tests(test_class):
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_class)
+    testRunner = unittest.runner.TextTestRunner(resultclass=GreatAssertionResult)
+    return testRunner.run(suite)
+
+
+class GreatAssertionSaveTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        if sys.platform.startswith("win"):
+            pytest.skip("Skipping test for windows", allow_module_level=True)
+
+        _run_tests(SaveTest).save("databricks", spark=spark)
+        cls.df = spark.table("ga_result")
+
+    def test_status(self):
+        self.assertEqual(self.df.count(), 8)
+        self.assertEqual(self.df.filter(self.df.status == "Fail").count(), 3)
+        self.assertEqual(self.df.filter(self.df.status == "Pass").count(), 3)
+        self.assertEqual(self.df.filter(self.df.status == "Skip").count(), 1)
+        self.assertEqual(self.df.filter(self.df.status == "Error").count(), 1)
+
+    def test_extended_fail(self):
+        df = self.df.filter((self.df.status == "Fail") & (self.df.test_id == 7))
+
+        col = ["test_id", "status", "extended"]
+        data = [
+            [
+                7,
+                "Fail",
+                '{"id": 7, "name": "expect_column_values_to_be_between", "values": {"column": "col_1", "exp_min_value": 101, "exp_max_value": 301, "act_min_value": 100.0}}',
+            ]
+        ]
+        expected = pd.DataFrame(data, columns=col)
+        actual = df.toPandas()
+
+        assert_frame_equal(expected, actual[col])
+
+    def test_extended_pass(self):
+        df = self.df.filter((self.df.status == "Pass")).sort(self.df.test_id.desc())
+
+        col = ["test_id", "status", "extended"]
+        data = [
+            [
+                14,
+                "Pass",
+                '{"id": 14, "name": "expect_date_range_to_be_more_than", "values": {"expected_min_date": "1899-12-31"}}',
+            ],
+            [
+                1,
+                "Pass",
+                '{"id": 1, "name": "expect_table_row_count_to_equal", "values": {"exp_count": 1, "act_count": 1, "tolerance": 0}}',
+            ],
+            [-1, "Pass", "{}"],
+        ]
+        expected = pd.DataFrame(data, columns=col)
+        actual = df.toPandas()
+
+        assert_frame_equal(expected, actual[col])
+
+    def test_extended_skip(self):
+        df = self.df.filter((self.df.status == "Skip"))
+
+        col = ["test_id", "status", "extended"]
+        data = [
+            [
+                -1,
+                "Skip",
+                "{}",
+            ]
+        ]
+        expected = pd.DataFrame(data, columns=col)
+        actual = df.toPandas()
+
+        assert_frame_equal(expected, actual[col])
+
+    def test_extended_error(self):
+        df = self.df.filter((self.df.status == "Error"))
+
+        col = ["test_id", "status", "extended"]
+        data = [
+            [
+                -1,
+                "Error",
+                "{}",
+            ]
+        ]
+        expected = pd.DataFrame(data, columns=col)
+        actual = df.toPandas()
+
+        assert_frame_equal(expected, actual[col])
+
+    def test_run_id(self):
+        cnt = self.df.groupBy("run_id").count().select("count").collect()[0][0]
+        self.assertEqual(cnt, 8)
+
+    def test_created_at(self):
+        cnt = self.df.groupBy("created_at").count().select("count").collect()[0][0]
+        self.assertEqual(cnt, 8)
+
+    def test_information(self):
+        df = self.df.filter(self.df.information.contains("Traceback"))
+        self.assertEqual(df.count(), 4)
+
+    def test_method(self):
+        df = self.df.filter(self.df.method.contains("test_fail"))
+        self.assertEqual(df.count(), 3)
