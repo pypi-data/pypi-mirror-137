ae9542566bdcfe9a1c4b8bc66677d4fa58eb9680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:23 +0000

    Add package fastybird-modbus-connector/0.3.0/fastybird_modbus_connector-0.3.0-py3-none-any.whl

---
+++ packages/fastybird-modbus-connector/fastybird_modbus_connector-0.3.0-py3-none-any.whl/fastybird_modbus_connector/entities.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python3
+
+#     Copyright 2022. FastyBird s.r.o.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+"""
+Modbus connector entities module
+"""
+
+# Library dependencies
+from typing import Dict, List, Optional, Union
+
+from fastybird_devices_module.entities.connector import ConnectorEntity
+from fastybird_devices_module.entities.device import DeviceEntity
+
+# Library libs
+from fastybird_modbus_connector.types import CONNECTOR_NAME, DEVICE_NAME
+
+
+class ModbusConnectorEntity(ConnectorEntity):
+    """
+    Modbus connector entity
+
+    @package        FastyBird:ModbusConnector!
+    @module         entities
+
+    @author         Adam Kadlec <adam.kadlec@fastybird.com>
+    """
+
+    __mapper_args__ = {"polymorphic_identity": "fastybird_modbus_connector"}
+
+    # -----------------------------------------------------------------------------
+
+    @property
+    def type(self) -> str:
+        """Connector type"""
+        return CONNECTOR_NAME
+
+    # -----------------------------------------------------------------------------
+
+    @property
+    def serial_interface(self) -> Optional[str]:
+        """Connector serial interface"""
+        return (
+            str(self.params.get("serial_interface", None))
+            if self.params is not None and self.params.get("serial_interface") is not None
+            else None
+        )
+
+    # -----------------------------------------------------------------------------
+
+    @serial_interface.setter
+    def serial_interface(self, serial_interface: Optional[str]) -> None:
+        """Connector serial interface setter"""
+        if self.params is not None and bool(self.params) is True:
+            self.params["serial_interface"] = serial_interface
+
+        else:
+            self.params = {"serial_interface": serial_interface}
+
+    # -----------------------------------------------------------------------------
+
+    @property
+    def baud_rate(self) -> Optional[int]:
+        """Connector communication baud rate"""
+        return (
+            int(str(self.params.get("baud_rate", 9600)))
+            if self.params is not None and self.params.get("baud_rate", 9600) is not None
+            else None
+        )
+
+    # -----------------------------------------------------------------------------
+
+    @baud_rate.setter
+    def baud_rate(self, baud_rate: Optional[int]) -> None:
+        """Connector communication baud rate setter"""
+        if self.params is not None and bool(self.params) is True:
+            self.params["baud_rate"] = baud_rate
+
+        else:
+            self.params = {"baud_rate": baud_rate}
+
+    # -----------------------------------------------------------------------------
+
+    def to_dict(self) -> Dict[str, Union[str, int, bool, List[str], None]]:
+        """Transform entity to dictionary"""
+        return {
+            **super().to_dict(),
+            **{
+                "serial_interface": self.serial_interface,
+                "baud_rate": self.baud_rate,
+            },
+        }
+
+
+class ModbusDeviceEntity(DeviceEntity):  # pylint: disable=too-few-public-methods
+    """
+    Modbus device entity
+
+    @package        FastyBird:ModbusConnector!
+    @module         entities
+
+    @author         Adam Kadlec <adam.kadlec@fastybird.com>
+    """
+
+    __mapper_args__ = {"polymorphic_identity": DEVICE_NAME}
+
+    # -----------------------------------------------------------------------------
+
+    @property
+    def type(self) -> str:
+        """Device type"""
+        return DEVICE_NAME
