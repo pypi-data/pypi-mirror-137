a1c94f6ef17ce78f83ebf7e20ea769a9274a6b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:42 +0000

    Add package ethpm-types/0.1.0b6/ethpm-types-0.1.0b6.tar.gz

---
+++ packages/ethpm-types/ethpm-types-0.1.0b6.tar.gz/ethpm-types-0.1.0b6/tests/test_examples.py
@@ -0,0 +1,35 @@
+import os
+
+import github
+import pytest  # type: ignore
+import requests  # type: ignore
+from pydantic import ValidationError
+
+from ethpm_types import PackageManifest
+
+ETHPM_SPEC_REPO = github.Github(os.environ.get("GITHUB_ACCESS_TOKEN", None)).get_repo(
+    "ethpm/ethpm-spec"
+)
+
+EXAMPLES_RAW_URL = "https://raw.githubusercontent.com/ethpm/ethpm-spec/master/examples"
+
+
+@pytest.mark.parametrize(
+    "example_name",
+    [f.name for f in ETHPM_SPEC_REPO.get_contents("examples")],  # type: ignore
+)
+def test_examples(example_name):
+    example = requests.get(f"{EXAMPLES_RAW_URL}/{example_name}/v3.json")
+    example_str = example.text.strip()  # NOTE: Some examples have extra newline
+    example_json = example.json()
+
+    if "invalid" not in example_name:
+        package = PackageManifest.parse_obj(example_json)
+        assert package.dict() == example_json
+
+        # NOTE: Also make sure that the encoding is exactly the same (per EIP-2678)
+        assert package.json() == example_str
+
+    else:
+        with pytest.raises((ValidationError, ValueError)):
+            PackageManifest.parse_obj(example_json).dict()
