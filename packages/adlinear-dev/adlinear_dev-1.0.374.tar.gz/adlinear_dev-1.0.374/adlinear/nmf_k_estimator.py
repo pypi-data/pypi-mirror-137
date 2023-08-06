bae8939eb7ec6152fce68ade0ee1e8c37325c757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:35:20 +0000

    Add package adlinear-dev/1.0.374/adlinear_dev-1.0.374.tar.gz

---
+++ packages/adlinear-dev/adlinear_dev-1.0.374.tar.gz/adlinear_dev-1.0.374/adlinear/nmf_k_estimator.py
@@ -0,0 +1,34 @@
+import math
+from . import nmfmodel
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+# from .utilities import *
+from . import utilities as utl
+from typing import Union, Tuple
+from adinference import admlp
+
+
+class NCompEstimator:
+
+    def __init__(self):
+        pass
+
+    def estimate_ncomp(mat: Union[pd.DataFrame, np.ndarray], inference_model: admlp.AdMlp,
+                       ncmin: int = 2, ncmax: int = 35):
+        df_screeplot = nmfmodel.generate_scree_plot(mat, ncmin=ncmin, ncmax=ncmax)
+        df_mini_scree_plots = pd.DataFrame(index=[], columns=[])
+        df_mini_scree_plots = nmfmodel.collect_windows_from_scree_plot(df_collected_windows=df_mini_scree_plots,
+                                                                       df_scree_plot=df_screeplot,
+                                                                       window_size=6)
+        data_for_inference = df_mini_scree_plots.drop([col for col in df_mini_scree_plots.columns if
+                                                       col.find("_ncomp") >= 0 or col.find("_entropy") >= 0
+                                                       or col.find("Position") >= 0],
+                                                      axis="columns", inplace=False).to_numpy(dtype=np.float64)
+
+        preds = inference_model.predict(data_for_inference)
+        return preds
+
+    pass
