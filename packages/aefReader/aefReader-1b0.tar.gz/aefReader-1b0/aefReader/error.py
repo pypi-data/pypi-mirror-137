c051f0cbb743432f64bda7dab7763816c9cd2873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:07 +0000

    Add package aefReader/1b0/aefReader-1b0.tar.gz

---
+++ packages/aefReader/aefReader-1b0.tar.gz/aefReader-1b0/aefReader/error.py
@@ -0,0 +1,32 @@
+class _error(Exception):
+    pass
+
+
+class Location(_error):
+    def __str__(self):
+        return "such a file does not exist or an incorrect location is specified"
+
+
+#class Already(_error):
+#    def __str__(self):
+#        return "such a file already exists"
+
+
+class Access(_error):
+    def __str__(self):
+        return "the system refused access"
+
+
+class FileType(_error):
+    def __str__(self):
+        return "invalid file type"
+
+
+class NoneMeta(_error):
+    def __str__(self):
+        return "metadata is missing or ignored"
+
+
+class OutBlock(_error):
+    def __str__(self):
+        return "output blocked"
---
+++ packages/aefReader/aefReader-1b0.tar.gz/aefReader-1b0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='aefReader',
+      version='1b',
+      description='reading .aef files',
+      packages=['aefReader'],
+      author_email='4dcubeoff@gmail.com',
+      zip_safe=False,
+      author="Savtis")
