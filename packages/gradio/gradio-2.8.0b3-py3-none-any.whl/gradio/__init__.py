be488d503c05e14e6d34105430180d7c8e0ba405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:35 +0000

    Add package gradio/2.8.0b3/gradio-2.8.0b3-py3-none-any.whl

---
+++ packages/gradio/gradio-2.8.0b3-py3-none-any.whl/gradio/__init__.py
@@ -0,0 +1,9 @@
+import pkg_resources
+
+from gradio.routes import get_state, set_state
+from gradio.flagging import *
+from gradio.interface import * 
+from gradio.mix import *
+
+current_pkg_version = pkg_resources.require("gradio")[0].version
+__version__ = current_pkg_version
