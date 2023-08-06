6c051a1f2e91a309432a7eef241d7e7136082245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:56 +0000

    Add package pacc/0.0.336/pacc-0.0.336-py3-none-any.whl

---
+++ packages/pacc/pacc-0.0.336-py3-none-any.whl/pacc/nox/noxConsole.py
@@ -0,0 +1,32 @@
+import os
+from ..tools import sleep
+
+
+class NoxConsole:
+
+    @classmethod
+    def quitAll(cls):
+        cmd = 'NoxConsole.exe quitall'
+        os.system(cmd)
+        print(cmd)
+        sleep(13)
+        if os.popen('tasklist | findstr "Nox.exe"').read():
+            cmd = 'taskkill /IM Nox.exe'
+            print(cmd)
+            os.system(cmd)
+            sleep(15)
+
+    def __init__(self, noxIndex):
+        self.noxIndex = noxIndex
+
+    def quit(self):
+        cmd = 'NoxConsole.exe quit -index:%d' % self.noxIndex
+        print(cmd)
+        os.popen(cmd)
+        sleep(5, False, False)
+
+    def runApp(self, packagename):
+        cmd = 'NoxConsole.exe runapp -index:%d -packagename:%s' % (self.noxIndex, packagename)
+        print(cmd)
+        os.popen(cmd)
+        sleep(5, False, False)
