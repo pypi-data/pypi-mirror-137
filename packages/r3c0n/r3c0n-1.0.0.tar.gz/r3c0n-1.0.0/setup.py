4f96b19703d75b31403b513e7368749192f57157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:06 +0000

    Add package r3c0n/1.0.0/r3c0n-1.0.0.tar.gz

---
+++ packages/r3c0n/r3c0n-1.0.0.tar.gz/r3c0n-1.0.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+VERSION="1.0.0"
+DESCRIPTION="A tool for performing reconnaissance on web targets in Python."
+LONG_DESCRIPTION="A tool for finding subdomain and directory information for various web targets."
+
+setup(
+    name='r3c0n',
+    version=VERSION,
+    license='MIT',
+    license_files=["LICENSE"],
+    author='Gacoka Mbui',
+    author_email='<markgacoka@gmail.com>',
+    description=DESCRIPTION,
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    url="https://github.com/markgacoka/r3c0n",
+    download_url="https://github.com/markgacoka/r3c0n/releases",
+    packages=find_packages('r3c0n'),
+    install_requires=required,
+    py_modules=['r3c0n'],
+    keywords='cybersecurity, reconnaissance, scanning, automation',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Security"
+    ]
+)
