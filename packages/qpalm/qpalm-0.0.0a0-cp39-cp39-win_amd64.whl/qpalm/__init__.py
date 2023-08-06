68e0b57ec389ccb6df2f8c207f952f025ee86757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:34:14 +0000

    Add package qpalm/0.0.0a0/qpalm-0.0.0a0-cp39-cp39-win_amd64.whl

---
+++ packages/qpalm/qpalm-0.0.0a0-cp39-cp39-win_amd64.whl/qpalm/__init__.py
@@ -0,0 +1,11 @@
+"""Proximal Augmented Lagrangian method for Quadratic Programs"""
+
+__version__ = '0.0.0a0'
+
+try:
+    from qpalm._qpalm import *
+    from qpalm._qpalm import __version__ as c_version
+    assert __version__ == c_version
+except ImportError as e:
+    import warnings
+    warnings.warn("Failed to import the QPALM C++ extension")
