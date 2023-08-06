0ffccf01fc5c72ef8cde7fc795f0d87f42cfd285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:15 +0000

    Add package srsparser/1.2.12/srsparser-1.2.12.tar.gz

---
+++ packages/srsparser/srsparser-1.2.12.tar.gz/srsparser-1.2.12/srsparser/configs.py
@@ -0,0 +1,39 @@
+from nltk import download, corpus
+
+# regular expression for detection numbering elements
+NUMBERING_PATTERN = '^([а-я0-9][.) ])+'
+
+# minimal strings similarity ratio
+MIN_SIMILARITY_RATIO = 0.5
+
+download('stopwords', quiet=True)
+STOPWORDS_RU = corpus.stopwords.words('russian')
+
+SPECIAL_CHARS = "!#$%&'()*+,/;<=>?@[\]^_`{|}~—\"\-."
+EXCESS_CHARS = f'[A-Za-z0-9{SPECIAL_CHARS}]+'
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'default_formatter': {
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+        },
+    },
+
+    'handlers': {
+        'stream_handler': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default_formatter',
+        },
+    },
+
+    'loggers': {
+        'my_logger': {
+            'handlers': ['stream_handler'],
+            'level': 'INFO',
+            'propagate': False
+        }
+    }
+}
