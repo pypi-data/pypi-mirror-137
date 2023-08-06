6a4ea79e2d7e080f4b768cc2633594b9582608cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:07 +0000

    Add package mapa/0.1.4/mapa-0.1.4-py3-none-any.whl

---
+++ packages/mapa/mapa-0.1.4-py3-none-any.whl/mapa/utils.py
@@ -0,0 +1,51 @@
+import tempfile
+from functools import wraps
+from pathlib import Path
+from time import time
+
+import click
+import rasterio as rio
+import requests
+from rasterio.plot import show
+
+
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        # start_msg = f"{f.__name__} ..."
+        # click.echo(f"{start_msg:<40s}", nl=False)
+        result = f(*args, **kw)
+        te = time()
+        end_msg = f"✅ ({round(te - ts, 1)}s)"
+        click.echo(f"{end_msg:<20s}")
+        return result
+
+    return wrap
+
+
+def download_file(url: str, local_file: Path) -> Path:
+    data = requests.get(url, stream=True)
+    with open(local_file, "wb") as file:
+        file.write(data.content)
+    return local_file
+
+
+def TMPDIR() -> Path:
+    tmpdir = Path(tempfile.gettempdir()) / "map2stl"
+    if not tmpdir.is_dir():
+        tmpdir.mkdir()
+    return tmpdir
+
+
+def _path_to_merged_tiff(bbox_hash: str) -> Path:
+    return TMPDIR() / f"merged_{bbox_hash}.tiff"
+
+
+def _path_to_clipped_tiff(bbox_hash: str) -> Path:
+    return TMPDIR() / f"clipped_{bbox_hash}.tiff"
+
+
+def show_tiff(path: Path) -> None:
+    tiff = rio.open(path)
+    show((tiff, 1), cmap="terrain")
