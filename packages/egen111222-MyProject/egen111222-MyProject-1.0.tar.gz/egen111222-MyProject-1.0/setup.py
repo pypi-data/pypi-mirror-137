693ca9026f770fe8662f3a14702f02efca0c426a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:13 +0000

    Add package egen111222-MyProject/1.0/egen111222-MyProject-1.0.tar.gz

---
+++ packages/egen111222-MyProject/egen111222-MyProject-1.0.tar.gz/egen111222-MyProject-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='egen111222-MyProject',
+      version='1.0',
+      description='Tmp project',
+      author='Kirill',
+      author_email='egen13@ukr.net',
+      url='https://www.google.com/',
+      packages=['module1', 'module2'],
+     )
