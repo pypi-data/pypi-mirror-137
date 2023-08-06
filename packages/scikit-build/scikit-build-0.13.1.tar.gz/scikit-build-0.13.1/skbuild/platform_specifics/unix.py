a15026bec1e57cd10f4e4908729cbee8dbfde118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:11 +0000

    Add package scikit-build/0.13.1/scikit-build-0.13.1.tar.gz

---
+++ packages/scikit-build/scikit-build-0.13.1.tar.gz/scikit-build-0.13.1/skbuild/platform_specifics/unix.py
@@ -0,0 +1,26 @@
+"""This module defines object specific to Unix platform."""
+
+import os
+
+from .abstract import CMakeGenerator
+
+from . import abstract
+
+
+# pylint:disable=abstract-method
+class UnixPlatform(abstract.CMakePlatform):
+    """Unix implementation of :class:`.abstract.CMakePlatform`."""
+
+    def __init__(self):
+        super(UnixPlatform, self).__init__()
+        try:
+            import ninja
+            ninja_executable_path = os.path.join(ninja.BIN_DIR, "ninja")
+            ninja_args = ['-DCMAKE_MAKE_PROGRAM:FILEPATH=' + ninja_executable_path]
+        except ImportError:
+            ninja_args = []
+
+        self.default_generators = [
+            CMakeGenerator("Ninja", args=ninja_args),
+            CMakeGenerator("Unix Makefiles")
+        ]
