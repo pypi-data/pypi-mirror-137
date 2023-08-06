ae4af57f421aa6fdcba89ab45df4c16d67576999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:25 +0000

    Add package qord/0.1.0/qord-0.1.0-py3-none-any.whl

---
+++ packages/qord/qord-0.1.0-py3-none-any.whl/qord/__init__.py
@@ -0,0 +1,15 @@
+"""
+Qord
+====
+
+A high level library for building Discord bots.
+"""
+
+from qord import events
+from qord.enums import *
+from qord.exceptions import *
+from qord.project_info import *
+from qord.core.client import *
+from qord.core.shard import *
+from qord.flags.base import *
+from qord.flags.intents import *
---
+++ packages/qord/qord-0.1.0-py3-none-any.whl/qord/enums.py
@@ -0,0 +1,34 @@
+# MIT License
+
+# Copyright (c) 2022 Izhar Ahmad
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import annotations
+
+
+class GatewayEvent:
+    r"""An enumeration that details names of various events sent over gateway.
+
+    These events names are commonly passed in :class:`Client.event` decorator for
+    registering a listener for relevant event.
+    """
+
+    GATEWAY_DISPATCH = "gateway_dispatch"
+    r"""Called whenever gateway sends a dispatch event. See :class:`events.GatewayDispatch`."""
