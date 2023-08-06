6b8eef8c11292316b9a5650df7f12911753ccea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:46 +0000

    Add package larq-zoo/2.3.0/larq-zoo-2.3.0.tar.gz

---
+++ packages/larq-zoo/larq-zoo-2.3.0.tar.gz/larq-zoo-2.3.0/setup.py
@@ -0,0 +1,63 @@
+from setuptools import find_packages, setup
+
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
+setup(
+    name="larq-zoo",
+    version="2.3.0",
+    author="Plumerai",
+    author_email="opensource@plumerai.com",
+    description="Reference implementations of popular Binarized Neural Networks",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/plumerai/larq-zoo",
+    packages=find_packages(),
+    license="Apache 2.0",
+    install_requires=[
+        "numpy>=1.15.0,<1.23.0",
+        "larq>=0.9.2,<0.13.0",
+        "zookeeper>=1.0.0,<1.4.0",
+        "importlib-metadata ~= 2.0 ; python_version<'3.8'",
+    ],
+    extras_require={
+        "tensorflow": ["tensorflow>=1.15.0"],
+        "tensorflow_gpu": ["tensorflow-gpu>=1.15.0"],
+        "test": [
+            "black==22.1.0",
+            "flake8==4.0.1",
+            "isort==5.10.1",
+            "pytype==2021.10.25",
+            "pytest==6.2.5",
+            "pytest-cov==3.0.0",
+            "pytest-mock>=3.6.1,<3.8.0",
+            "pytest-xdist==2.5.0",
+            "Pillow>=8.4.0,<10.0.0",
+            "tensorflow_datasets>=3.1.0,!=4.3.0",
+        ],
+    },
+    entry_points="""
+        [console_scripts]
+        lqz=larq_zoo.training.main:cli
+    """,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+    ],
+)
