40a38e7593b63e8de41dcfbbbb9146e7c7ea0824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:55 +0000

    Add package dash-flask-login/0.0.7/dash_flask_login-0.0.7.tar.gz

---
+++ packages/dash-flask-login/dash_flask_login-0.0.7.tar.gz/dash_flask_login-0.0.7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+# Metadata goes in setuf.cfg
+setup(
+    name="dash_flask_login",
+    install_requires=[
+        "dash >= 2.0.0",
+        "flask-login >=0.5.0",
+    ],
+)
