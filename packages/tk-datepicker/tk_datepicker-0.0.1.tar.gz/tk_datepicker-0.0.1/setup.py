0f84e2a69fbead6725e150a98951ae1447812a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:10 +0000

    Add package tk-datepicker/0.0.1/tk_datepicker-0.0.1.tar.gz

---
+++ packages/tk-datepicker/tk_datepicker-0.0.1.tar.gz/tk_datepicker-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+
+setup(
+    name="tk_datepicker",
+    version=VERSION,
+    author="QuantumaStelata",
+    author_email="<quantumastelata@gmail.com>",
+    description="Calendar for Tkinter module",
+    packages=find_packages(),
+    keywords=["python", "tk", "tkinter", "calendar", "tk_datepicker", "pytk"],
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
---
+++ packages/tk-datepicker/tk_datepicker-0.0.1.tar.gz/tk_datepicker-0.0.1/tk_datepicker/__init__.py
@@ -0,0 +1,1 @@
+from .tk_datepicker import Datepicker
