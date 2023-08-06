4c480d74da59327f0862bf021ec7b5b7ee918073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:47 +0000

    Add package rh-ocp-build-data-validator/0.2.5/rh_ocp_build_data_validator-0.2.5-py2.py3-none-any.whl

---
+++ packages/rh-ocp-build-data-validator/rh_ocp_build_data_validator-0.2.5-py2.py3-none-any.whl/validator/schema/streams_schema.py
@@ -0,0 +1,22 @@
+from schema import Schema, Optional, And, SchemaError
+
+STREAMS_SCHEMA = {
+        And(str, len): {
+            'image': And(str, len),
+            Optional('upstream_image_base'): And(str, len),  # won't need if we do not transform
+            'upstream_image': And(str, len),
+            Optional('mirror'): bool,
+            Optional('transform'): And(str, len),
+        },
+    }
+
+
+def streams_schema():
+    return Schema(STREAMS_SCHEMA)
+
+
+def validate(_, data):
+    try:
+        streams_schema().validate(data)
+    except SchemaError as err:
+        return '{}'.format(err)
