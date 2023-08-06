b50aa13ec60f53f786a6c523f9c1b55494523ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:58 +0000

    Add package banidb/0.5.0/banidb-0.5.0.tar.gz

---
+++ packages/banidb/banidb-0.5.0.tar.gz/banidb-0.5.0/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    README = f.read()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='banidb',
+    version='0.5.0',
+    license='Open Software License 3.0 (OSL-3.0)',
+    description='Python Package for Sikh Gurbani - BaniDB API',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Khalis Foundation',
+    url='https://github.com/KhalisFoundation/banidb-api-python',
+    keywords=[
+        'BaniDB',
+        'SikhiToTheMax',
+        'Gurbani',
+        'KhalisFoundation',
+        'Sikh',
+        'Guru Granth Sahib',
+        'API',
+        'Amrit Keertan',
+        'Sundar Gutka'
+    ],
+    install_requires=required,
+    author_email='python@khalis.info',
+    download_url='https://github.com/KhalisFoundation/banidb-api-python/archive/refs/tags/v_0.2.tar.gz',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Religion',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: Open Software License 3.0 (OSL-3.0)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    packages=['banidb'],
+    include_package_data=True,
+    project_urls={
+        'Source': 'https://github.com/KhalisFoundation/banidb-api-python',
+        'Issues': 'https://git.io/JG4II',
+        'Documentation':'https://banidbpy.readthedocs.io/'
+    },
+)
