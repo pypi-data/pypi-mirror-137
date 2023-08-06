6258ff00fe745e2b1cc48f11cc9c42b448af9965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:37:12 +0000

    Add package MixacLib/2.1.0/MixacLib-2.1.0.tar.gz

---
+++ packages/MixacLib/MixacLib-2.1.0.tar.gz/MixacLib-2.1.0/src/MixacLib/mc/anticheat.py
@@ -0,0 +1,37 @@
+"""
+MixacLib反作弊程序 - Minecraft
+当前已支持检测：
+水影：LiquidBounce, FDPClient, 141Sense, VictimClient
+端：PowerX
+"""
+
+import os
+
+# 检测水影
+__LIQUID__ = ['LiquidBounce-1.8', 'LiquidBounce-1.12',
+              'FDPClient-1.8',
+              '141Sense-1.8', 'VictimConfig', 'Victim']
+
+__CLIENT__ = ['Power']
+
+
+# 水影检测器
+def liquid(path: str = r'\.minecraft'):
+    for i in __LIQUID__:
+        if os.path.exists(rf'{path}\{i}'):
+            return True
+        else:
+            return False
+
+
+# 端检测器
+def client(path: str = r'\.minecraft'):
+    for i in __CLIENT__:
+        if os.path.exists(rf'{path}\{i}'):
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    print(str(client('.minecraft')))
