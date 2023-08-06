2cc8459a2d641c04a0a7c3ea5c4ce577d327858a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:41 +0000

    Add package hacklebox/0.0.0/hacklebox-0.0.0.tar.gz

---
+++ packages/hacklebox/hacklebox-0.0.0.tar.gz/hacklebox-0.0.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='hacklebox',
+    version='0.0.0',
+    description='An assortment of utilities',
+    # long_description='Check out the README on [GitLab](https://gitlab.com/thomasjlsn/qargs)!',
+    # long_description_content_type='text/markdown',
+    # author='Thomas Ellison',
+    # author_email='thomasjlsn@gmail.com',
+    # url='https://gitlab.com/thomasjlsn/qargs',
+    packages=['hb'],
+    # license_files=['LICENSE'],
+    # classifiers=[
+        # 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        # 'Programming Language :: Python :: 3',
+        # 'Development Status :: 3 - Alpha',
+        # 'Environment :: Console',
+    # ]
+)
