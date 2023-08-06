5c017bc07a644a67da5ed5f46f1f081a8b38b40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:54 +0000

    Add package napari-plot/0.1.3/napari_plot-0.1.3-py3-none-any.whl

---
+++ packages/napari-plot/napari_plot-0.1.3-py3-none-any.whl/napari_plot/layers/centroids/_centroids_utils.py
@@ -0,0 +1,15 @@
+"""Various utilities."""
+import numpy as np
+
+
+def preprocess_centroids(data: np.ndarray):
+    """Preprocess centroids data. Ensure that user-specified data matches expected dimensions."""
+    data = np.asarray(data)
+
+    # check if data includes the upper and lower boundaries - if so, move on
+    if data.shape[1] == 3:
+        return data
+    if data.shape[1] == 2:
+        data = np.insert(data, 1, np.zeros(data.shape[0]), axis=1)
+        return data
+    raise ValueError("Provided centroid data was incorrect.")
