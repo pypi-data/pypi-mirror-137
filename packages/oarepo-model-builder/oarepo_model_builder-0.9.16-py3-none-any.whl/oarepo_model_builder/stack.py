e1f6be3c4152e4dc53c4e9fcb057c6e588ee25b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:06 +0000

    Add package oarepo-model-builder/0.9.16/oarepo_model_builder-0.9.16-py3-none-any.whl

---
+++ packages/oarepo-model-builder/oarepo_model_builder-0.9.16-py3-none-any.whl/oarepo_model_builder/stack.py
@@ -0,0 +1,72 @@
+from functools import cached_property
+from typing import Generator, List
+
+from deepdiff import DeepDiff
+
+
+class ReplaceElement(Exception):
+    def __init__(self, data):
+        super().__init__()
+        self.data = data
+
+
+class ModelBuilderStackEntry:
+    def __init__(self, key=None, data=None):
+        self.key = key
+        self.data = data
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def __eq__(self, other):
+        return self.key == other.key and not DeepDiff(self.key, other.key)
+
+    def __str__(self):
+        return f"{self.key} - {self.data}"
+
+
+class ModelBuilderStack:
+    DICT = "dict"
+    LIST = "list"
+    PRIMITIVE = "primitive"
+    SKIP = "skip"
+
+    def __init__(self):
+        self.stack = []
+
+    def __getitem__(self, item):
+        return self.stack[item]
+
+    def push(self, key, el):
+        self._clear_path()
+        self.stack.append(ModelBuilderStackEntry(key, el))
+
+    def pop(self):
+        self._clear_path()
+        self.stack.pop()
+
+    @property
+    def top(self):
+        return self.stack[-1]
+
+    @property
+    def level(self):
+        return len(self.stack)
+
+    @property
+    def top_type(self):
+        match self.top.data:
+            case dict():
+                return self.DICT
+            case list():
+                return self.LIST
+            case _:
+                return self.PRIMITIVE
+
+    @cached_property
+    def path(self):
+        return "/" + "/".join(x.key for x in self.stack if x.key)
+
+    def _clear_path(self):
+        if "path" in self.__dict__:
+            del self.__dict__["path"]
