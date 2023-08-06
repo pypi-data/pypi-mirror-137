92a5041ea59b4ec83a2f3ca61dc9909f6ba58b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:22 +0000

    Add package dewloosh.math/0.0.1a1/dewloosh.math-0.0.1a1-py3-none-any.whl

---
+++ packages/dewloosh.math/dewloosh.math-0.0.1a1-py3-none-any.whl/dewloosh/math/function/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from dewloosh.math.function.function import *
+from dewloosh.math.function.relation import *
+from dewloosh.math.function.functions import *
---
+++ packages/dewloosh.math/dewloosh.math-0.0.1a1-py3-none-any.whl/dewloosh/math/optimize/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  2 18:31:06 2018
+
+@author: Bence
+"""
+
+from dewloosh.math.optimize.genetic import BinaryGeneticAlgorithm
+#from dewloosh.math.optimize.newton import Newton
+#from dewloosh.math.optimize.genetic import BinaryGeneticAlgorithm
+#from dewloosh.math.optimize.steepestdescent import SteepestDescent
