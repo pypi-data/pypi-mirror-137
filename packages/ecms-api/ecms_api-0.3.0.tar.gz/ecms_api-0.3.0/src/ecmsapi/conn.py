31e079aca3220a13bd88c84adcb0649ee40d4caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:17 +0000

    Add package ecms-api/0.3.0/ecms_api-0.3.0.tar.gz

---
+++ packages/ecms-api/ecms_api-0.3.0.tar.gz/ecms_api-0.3.0/src/ecmsapi/conn.py
@@ -0,0 +1,35 @@
+import os
+import pyodbc
+
+
+class Conn:
+
+    """
+    Connection to ecms 
+    """
+
+    host = os.getenv('ECMS_HOST')
+    uid = os.getenv('ECMS_UID')
+    pwd = os.getenv('ECMS_PWD')
+    connection_string = f'DSN={host}; UID={uid}; PWD={pwd}'
+
+    def execute(self, command):
+        """
+        Base execution method
+        """
+        try:
+            conn = pyodbc.connect(self.connection_string)
+            cur = conn.cursor()
+            print(command)
+
+            cur.execute(command)
+            if 'SELECT' in command:
+                resp = cur.fetchall()
+                return list(resp)
+            if 'UPDATE' in command:
+                conn.commit()
+            if 'INSERT' in command:
+                return True
+                conn.commit()
+        except Exception as e:
+            print(e)
