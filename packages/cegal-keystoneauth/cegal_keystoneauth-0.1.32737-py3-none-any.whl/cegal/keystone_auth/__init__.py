475a15177d4189376c2254338acc5d85cef798cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:55 +0000

    Add package cegal-keystoneauth/0.1.32737/cegal_keystoneauth-0.1.32737-py3-none-any.whl

---
+++ packages/cegal-keystoneauth/cegal_keystoneauth-0.1.32737-py3-none-any.whl/cegal/keystone_auth/__init__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Cegal AS
+# All rights reserved
+
+import logging
+from os import getenv
+
+
+logger = logging.getLogger(__name__)
+verify_tls = True
+if getenv("PYVAR_ALLOW_NON_HTTPS") == "true":
+    verify_tls = False
+
+
+from .client import OidcClient
+from .options import OidcOptions, OidcFlow
