bcf8cf0f4d51c73927ebde7225acd080b9a4354e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:35 +0000

    Add package playsoundsimple.py/0.1/playsoundsimple.py-0.1.tar.gz

---
+++ packages/playsoundsimple.py/playsoundsimple.py-0.1.tar.gz/playsoundsimple.py-0.1/setup.py
@@ -0,0 +1,21 @@
+import os
+import setuptools
+
+setuptools.setup(
+    name='playsoundsimple.py',
+    version='0.1',
+    description='A simple library for playing sound files.',
+    keywords='playsoundsimple',
+    packages=setuptools.find_packages(),
+    author_email='semina054@gmail.com',
+    url="https://github.com/romanin-rf/playsoundsimple.py",
+    zip_safe=False,
+    long_description=open(
+        os.path.join(
+            os.path.dirname(__file__),
+            'README.rst'
+        )
+    ).read(),
+    author='ProgrammerFromParlament',
+    license='MIT'
+)
