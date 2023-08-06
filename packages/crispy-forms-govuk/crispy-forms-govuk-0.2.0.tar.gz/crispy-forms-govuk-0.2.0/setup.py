3b4f13fc6e29a83faef9a17b2bfb802a25a47490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:39:31 +0000

    Add package crispy-forms-govuk/0.2.0/crispy-forms-govuk-0.2.0.tar.gz

---
+++ packages/crispy-forms-govuk/crispy-forms-govuk-0.2.0.tar.gz/crispy-forms-govuk-0.2.0/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['crispy_forms_govuk',
+ 'crispy_forms_govuk.layout',
+ 'crispy_forms_govuk.templatetags']
+
+package_data = \
+{'': ['*'],
+ 'crispy_forms_govuk': ['static/js/*',
+                        'templates/govuk/*',
+                        'templates/govuk/layout/*',
+                        'templates/govuk/snippets/*']}
+
+install_requires = \
+['django-crispy-forms>=1.9.0,<1.10.0', 'django>=3.1.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'crispy-forms-govuk',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
