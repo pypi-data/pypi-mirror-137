f396d8aac63160fe6b5fd083e75d5d9107443f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:51 +0000

    Add package cdk-organizations-list-accounts/0.0.0/cdk_organizations_list_accounts-0.0.0-py3-none-any.whl

---
+++ packages/cdk-organizations-list-accounts/cdk_organizations_list_accounts-0.0.0-py3-none-any.whl/cdk_organizations_list_accounts/__init__.py
@@ -0,0 +1,69 @@
+'''
+# cdk-organizations-list-accounts
+
+Want to keep an up-to-date list of your AWS accounts?
+
+cdk-organizations-list-accounts is an AWS CDK building library that outputs a list of AWS organization accounts in CSV format.
+
+## Overview
+
+Amazon EventBridge detects the account creation event and starts a Lambda function.
+An accounts list, including the organization structure, will be output to S3 bucket in CSV format.
+
+Output Example:
+
+```
+Id,Name,Email,Status,Joined Method,Joined Timestamp,OU Id,1st Level OU,2nd Level OU,3rd Level OU,4th Level OU,5th Level OU
+000000000000,account-mgmt,account+mgmt@example.com,ACTIVE,INVITED,2022-01-31 07:19:57,r-xxxx
+111111111111,account-0001,account+0001@example.com,ACTIVE,INVITED,2022-01-31 07:25:38,ou-xxxx-yyyyyyyy,Suspended
+222222222222,account-0002,account+0002@example.com,ACTIVE,CREATED,2022-01-31 07:31:28,ou-xxxx-zzzzzzzz,Sample System,Additional,Workloads,Prod
+333333333333,account-0003,account+0003@example.com,ACTIVE,CREATED,2022-01-31 08:15:49,ou-xxxx-zzzzzzzz,Sample System,Additional,Workloads,SDLC
+444444444444,account-0004,account+0004@example.com,ACTIVE,CREATED,2022-01-31 09:18:50,ou-xxxx-zzzzzzzz,Sample System,Foundational,Security,Prod
+555555555555,account-0005,account+0005@example.com,ACTIVE,CREATED,2022-01-31 10:21:30,ou-xxxx-zzzzzzzz,Sample System,Foundational,Infrastructure,Prod
+666666666666,account-0006,account+0006@example.com,ACTIVE,CREATED,2022-01-31 11:21:05,ou-xxxx-zzzzzzzz,Sample System,Foundational,Infrastructure,SDLC
+```
+
+## Limitations at present
+
+* Must deploy to AWS Organization's management account
+* Events other than CreateAccount are not supported
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from ._jsii import *
+
+import constructs
+
+
+class OrganizationsListAccounts(
+    constructs.Construct,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="cdk-organizations-list-accounts.OrganizationsListAccounts",
+):
+    '''
+    :stability: experimental
+    '''
+
+    def __init__(self, scope: constructs.Construct, id: builtins.str) -> None:
+        '''
+        :param scope: -
+        :param id: -
+
+        :stability: experimental
+        '''
+        jsii.create(self.__class__, self, [scope, id])
+
+
+__all__ = [
+    "OrganizationsListAccounts",
+]
+
+publication.publish()
