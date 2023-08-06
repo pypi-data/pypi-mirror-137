ffb52424bc1d0429fc8b02043b6bfa152987ab23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:23 +0000

    Add package mc-converter/1.0.3.1/mc-converter-1.0.3.1.tar.gz

---
+++ packages/mc-converter/mc-converter-1.0.3.1.tar.gz/mc-converter-1.0.3.1/README.md
@@ -0,0 +1,61 @@
+# modpack-converter
+Convert's one minecraft modpack format to another.
+
+Maybe I need to think of a better name...
+
+# Features
+
+- Auto detect input modpack format
+- Support convertions to and from:
+    - MultiMC (parsing only)
+    - CurseForge
+    - Modrinth
+    - packwiz
+- Detects downloadable resourcepacks and shaders (MultiMC only)
+- User friendly toml config
+- Multiple output formats at once
+
+# How to Use
+
+```
+mc-converter [-h] [-c CONFIG] -i INPUT -f FORMAT [-o OUTPUT]
+```
+
+## Explanation:
+
+```
+-h --help: prints help
+-i --input: specifies input file (mostly zip file)
+-c --config: specifies config file, used for fill the gaps like description or files not in modrinth on curseforge example can be found in this repository.
+-f --format: soecifies formats to convert, must be separated by space.
+-o --output: specifies output directory, where converted zip files will be stored. By default current working directory will be used.
+```
+
+Avaliable formats:     - `curseforge, modrinth, packwiz, intermediate`
+
+`intermediate` must be used only for debuging, can contain sensetive information
+
+Example: 
+```
+mc-converter -i MyLovelyMultiMcModpack.zip -c config.toml -f curseforge modrinth -o converted_modpacks
+```
+
+# How to Install
+
+## From PyPI
+```
+pip install mc-converter
+```
+## From `.whl` file
+Go to Release page, download latest `.whl` file \
+ And install it via the following comand:
+ ```
+ pip install mc_converter_{version}.whl
+ ```
+ 
+ # Credits
+
+murmurhash2 - Murmur Hash 2 libray - https://pypi.org/project/murmurhash2 \
+aiohttp - Async web interface - https://github.com/aio-libs/aiohttp \
+tomli - Fast pure python toml parser - https://github.com/hukkin/tomli \
+pytoml - The only one toml writer that can handle weird packwiz files - https://github.com/avakar/pytoml
---
+++ packages/mc-converter/mc-converter-1.0.3.1.tar.gz/mc-converter-1.0.3.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mc_converter', 'mc_converter.Helpers']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.7.4,<4.0.0',
+ 'murmurhash2>=0.2.9,<0.3.0',
+ 'pytoml>=0.1.21,<0.2.0',
+ 'tenacity>=8.0.1,<9.0.0',
+ 'tomli>=1.2.1,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['mc-converter = mc_converter:main']}
+
+setup_kwargs = {
+    'name': 'mc-converter',
+    'version': '1.0.3.1',
+    'description': '',
+    'long_description': "# modpack-converter\nConvert's one minecraft modpack format to another.\n\nMaybe I need to think of a better name...\n\n# Features\n\n- Auto detect input modpack format\n- Support convertions to and from:\n    - MultiMC (parsing only)\n    - CurseForge\n    - Modrinth\n    - packwiz\n- Detects downloadable resourcepacks and shaders (MultiMC only)\n- User friendly toml config\n- Multiple output formats at once\n\n# How to Use\n\n```\nmc-converter [-h] [-c CONFIG] -i INPUT -f FORMAT [-o OUTPUT]\n```\n\n## Explanation:\n\n```\n-h --help: prints help\n-i --input: specifies input file (mostly zip file)\n-c --config: specifies config file, used for fill the gaps like description or files not in modrinth on curseforge example can be found in this repository.\n-f --format: soecifies formats to convert, must be separated by space.\n-o --output: specifies output directory, where converted zip files will be stored. By default current working directory will be used.\n```\n\nAvaliable formats:     - `curseforge, modrinth, packwiz, intermediate`\n\n`intermediate` must be used only for debuging, can contain sensetive information\n\nExample: \n```\nmc-converter -i MyLovelyMultiMcModpack.zip -c config.toml -f curseforge modrinth -o converted_modpacks\n```\n\n# How to Install\n\n## From PyPI\n```\npip install mc-converter\n```\n## From `.whl` file\nGo to Release page, download latest `.whl` file \\\n And install it via the following comand:\n ```\n pip install mc_converter_{version}.whl\n ```\n \n # Credits\n\nmurmurhash2 - Murmur Hash 2 libray - https://pypi.org/project/murmurhash2 \\\naiohttp - Async web interface - https://github.com/aio-libs/aiohttp \\\ntomli - Fast pure python toml parser - https://github.com/hukkin/tomli \\\npytoml - The only one toml writer that can handle weird packwiz files - https://github.com/avakar/pytoml",
+    'author': 'RozeFound',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/RozeFound/modpack-converter',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
