491eb01971eb0cb27994b0d3e4a1fd065bb8193e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:45 +0000

    Add package radiojavanapi/0.3.0/radiojavanapi-0.3.0.tar.gz

---
+++ packages/radiojavanapi/radiojavanapi-0.3.0.tar.gz/radiojavanapi-0.3.0/radiojavanapi/constants.py
@@ -0,0 +1,11 @@
+API_DOMAIN = "https://d3m6do6hyfiftd.cloudfront.net/api2/{}"
+
+BASE_HEADERS = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Host': 'd3m6do6hyfiftd.cloudfront.net',
+    'Connection': 'keep-alive',
+    'x-rj-user-agent': 'Radio Javan/4.0.2 (Windows 10 64-bit 13.0.1) com.radioJavan.rj.desktop',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) RadioJavan/4.0.2 Chrome/91.0.4472.69 Electron/13.0.1 Safari/537.36',
+}
