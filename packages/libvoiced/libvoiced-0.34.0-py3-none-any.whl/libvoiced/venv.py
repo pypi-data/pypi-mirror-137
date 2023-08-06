423d4d313c003701b0418a99066dd9af4f847f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:22 +0000

    Add package libvoiced/0.34.0/libvoiced-0.34.0-py3-none-any.whl

---
+++ packages/libvoiced/libvoiced-0.34.0-py3-none-any.whl/libvoiced/venv.py
@@ -0,0 +1,30 @@
+import logging
+import os
+import subprocess
+
+
+def create_virtualenv(project_path):
+    os.chdir(project_path)
+    cmd = [
+        "python3",
+        "-mvenv",
+        ".venv",
+    ]
+    s1 = " ".join(cmd)
+    logging.debug(s1)
+
+    proc = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    try:
+        outs, errs = proc.communicate(timeout=15)
+        logging.debug(outs.decode())
+    except subprocess.TimeoutExpired:
+        proc.kill()
+        outs, errs = proc.communicate()
+        logging.warning(errs.decode())
+
+    logging.debug(errs.decode())
