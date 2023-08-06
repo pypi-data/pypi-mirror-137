7aad4e6361856e4be929d6bfbb993adfd7745c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:26 +0000

    Add package pydbro/0.1.2/pydbro-0.1.2.tar.gz

---
+++ packages/pydbro/pydbro-0.1.2.tar.gz/pydbro-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name="pydbro",
+    version="0.1.2",
+    description="Python Console Database Browser",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mtatton/pydbro",
+    author="Michael Tatton",
+    license="",
+    packages=["pydbro"],
+    install_requires=[
+        "",
+    ],
+    entry_points={
+        "console_scripts": [
+          "pydbro = pydbro.pydbro:cli",
+          "coned = pydbro.coned:cli",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: User Interfaces",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
