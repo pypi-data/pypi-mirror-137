61f98c8b5f4b8d4d4b658d7aab8a9e8c2485d20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:41 +0000

    Add package PyStrong/0.0.5/PyStrong-0.0.5-py3-none-any.whl

---
+++ packages/PyStrong/PyStrong-0.0.5-py3-none-any.whl/pystrong/exceptions.py
@@ -0,0 +1,22 @@
+class InitError(Exception):
+    pass
+
+
+class BadTypeError(Exception):
+    pass
+
+
+class EnforcedTypeError(Exception):
+    pass
+
+
+class AttributeTypeNotSet(Exception):
+    pass
+
+
+class EnforcedReturnTypeError(Exception):
+    pass
+
+
+class EnforcedArgTypeError(Exception):
+    pass
