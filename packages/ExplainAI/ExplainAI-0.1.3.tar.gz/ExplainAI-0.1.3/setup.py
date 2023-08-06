9f014a0c8c0b5447713ebbb2edbcb34754c8d75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:26 +0000

    Add package ExplainAI/0.1.3/ExplainAI-0.1.3.tar.gz

---
+++ packages/ExplainAI/ExplainAI-0.1.3.tar.gz/ExplainAI-0.1.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: Feini Huang
+# Mail: 386899557@qq.com
+# Created Time:  2022-2-4 19:17:34
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "ExplainAI",
+    version = "0.1.3",
+    keywords = (""),
+    description = "explain AI tool",
+    long_description = "",
+    license = "MIT Licence",
+
+    url = "https://github.com/HuangFeini/ExplainAI/tree/master",
+    author = "Feini Huang",
+    author_email = "386899557@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
