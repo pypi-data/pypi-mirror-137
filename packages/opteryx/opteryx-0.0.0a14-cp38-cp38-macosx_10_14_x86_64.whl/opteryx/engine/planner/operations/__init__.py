974ae3b12f86dcd369e12f89efdf4d60bda5cdf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:07 +0000

    Add package opteryx/0.0.0a14/opteryx-0.0.0a14-cp38-cp38-macosx_10_14_x86_64.whl

---
+++ packages/opteryx/opteryx-0.0.0a14-cp38-cp38-macosx_10_14_x86_64.whl/opteryx/engine/planner/operations/__init__.py
@@ -0,0 +1,17 @@
+from .base_plan_node import BasePlanNode
+
+from .aggregate_node import AggregateNode  # aggregate data
+
+# - Evaluate ()
+from .distinct_node import DistinctNode  # remove duplicate records
+from .dataset_reader_node import DatasetReaderNode  # read datasets
+
+# - JoinNode (currently only INNER JOIN)
+from .limit_node import LimitNode  # select the first N records
+
+# - Merge - append sets to each other
+from .offset_node import OffsetNode  # skip a number of records
+from .projection_node import ProjectionNode  # remove unwanted columns including renames
+from .selection_node import SelectionNode  # filter unwanted rows
+
+# - SortNode (order a relation by given keys)
