22f1d59e89274a6f89eb910599001b05f7e1acd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:49 +0000

    Add package noodles/0.3.4/noodles-0.3.4-py3-none-any.whl

---
+++ packages/noodles/noodles-0.3.4-py3-none-any.whl/noodles/serial/__init__.py
@@ -0,0 +1,15 @@
+try:
+    from .numpy import registry as numpy
+except ImportError:
+    numpy = None
+
+from .pickle import registry as pickle
+from .base import registry as base
+from .as_dict import AsDict
+from .namedtuple import registry as namedtuple
+from .registry import (Registry, Serialiser, RefObject)
+from .reasonable import (Reasonable)
+from .path import SerPath
+
+__all__ = ['pickle', 'base', 'Registry', 'Serialiser', 'SerPath',
+           'RefObject', 'AsDict', 'namedtuple', 'Reasonable', 'numpy']
