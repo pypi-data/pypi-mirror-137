f67d702dffc12fa97cfa865e26587c9facca5bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:55 +0000

    Add package ilens-kafka-publisher/0.3.1/ilens-kafka-publisher-0.3.1.tar.gz

---
+++ packages/ilens-kafka-publisher/ilens-kafka-publisher-0.3.1.tar.gz/ilens-kafka-publisher-0.3.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='ilens-kafka-publisher',
+    version='0.3.1',
+    packages=['ilens_kafka_publisher'],
+    url='https://unifytwin.com',
+    license='Restricted',
+    author='Charan',
+    author_email='charankumar@knowledgelens.com',
+    description='Utility for Publishing messages to Kafka in iLens',
+    install_requires=[
+          "kafka-python~=2.0.2"
+      ],
+)
